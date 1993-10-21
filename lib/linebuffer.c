30d5e647aa317989d2470ce7eda0d85371241959
Jim Meyering <jim@meyering.net>
1993-10-21 21:58:56 +0000

    GNU text utilities

--- lib/linebuffer.c
+++ lib/linebuffer.c
@@ -17,6 +17,17 @@
 
 /* Written by Richard Stallman. */
 
+#ifdef HAVE_CONFIG_H
+#if defined (CONFIG_BROKETS)
+/* We use <config.h> instead of "config.h" so that a compilation
+   using -I. -I$srcdir will use ./config.h rather than $srcdir/config.h
+   (which it would do because it found this file in $srcdir).  */
+#include <config.h>
+#else
+#include "config.h"
+#endif
+#endif
+
 #include <stdio.h>
 #include "linebuffer.h"
 
