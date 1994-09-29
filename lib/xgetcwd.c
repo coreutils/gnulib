c46cc31d4194e82ac103fd4fb25e5720dca5f303
Jim Meyering <jim@meyering.net>
1994-09-29 17:33:34 +0000

    GNU shell utilities

--- lib/xgetcwd.c
+++ lib/xgetcwd.c
@@ -15,7 +15,7 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 
-/* Written by David MacKenzie, djm@gnu.ai.mit.edu. */
+/* Written by David MacKenzie <djm@gnu.ai.mit.edu>.  */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
