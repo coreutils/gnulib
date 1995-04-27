7071dba4e1694be8eea266db067b6e2969f6d75f
Jim Meyering <jim@meyering.net>
1995-04-27 05:38:06 +0000

    GNU text utilities

--- lib/error.h
+++ lib/error.h
@@ -1,12 +1,32 @@
+/* error.h -- declaration for error-reporting function
+   Copyright (C) 1995 Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
 #ifndef _error_h_
 #define _error_h_
 
-#ifdef __GNUC__
-void error (int, int, const char *, ...)
-#if __GNUC__ > 1
-    __attribute__ ((format (printf, 3, 4)))
+#ifndef __attribute__
+# if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 5) || __STRICT_ANSI__
+#  define __attribute__(Spec) /* empty */
+# endif
 #endif
-    ;
+
+#if __STDC__
+void error (int, int, const char *, ...) \
+  __attribute__ ((__format__ (__printf__, 3, 4)));
 #else
 void error ();
 #endif
