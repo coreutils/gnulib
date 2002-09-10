3f53294c6ad284644ff21fa9fe52173551b9965b
Karl Berry <karl@freefriends.org>
2002-09-10 23:41:12 +0000

    sync and scripts to check syncness

---
+++ config/srclistvars.sh
@@ -0,0 +1,10 @@
+# $Id: srclistvars.sh,v 1.1 2002-09-10 23:41:12 karl Exp $
+# Variables for srclist-update and srclist.txt.
+
+: ${EMACSSRC=$HOME/gnu/src/emacs}
+: ${GETTEXT=/usr/local/gnu/share/gettext}
+: ${GNUBIN=/usr/local/gnu/bin}
+: ${GNUCONFIG=$HOME/gnu/src/ftp.gnu.org/pub/gnu/config}
+: ${GNULIBSRC=$HOME/gnu/src/gnulib}
+: ${TEXINFO=/u/texinfo/src}
+: ${TEXMF=/usr/local/texmf/texmf}
