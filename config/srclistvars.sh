48b1f4609d94a9b6d9132841a7d5a196fb22e7a3
Karl Berry <karl@freefriends.org>
2002-09-12 20:29:26 +0000

    INSTALL from autoconf

--- config/srclistvars.sh
+++ config/srclistvars.sh
@@ -1,10 +1,13 @@
-# $Id: srclistvars.sh,v 1.1 2002-09-10 23:41:12 karl Exp $
+# $Id: srclistvars.sh,v 1.2 2002-09-12 20:29:26 karl Exp $
 # Variables for srclist-update and srclist.txt.
+# Definitely will change for each user.
 
+: ${AUTOCONF=/usr/local/texmf/texmf}
 : ${EMACSSRC=$HOME/gnu/src/emacs}
 : ${GETTEXT=/usr/local/gnu/share/gettext}
 : ${GNUBIN=/usr/local/gnu/bin}
 : ${GNUCONFIG=$HOME/gnu/src/ftp.gnu.org/pub/gnu/config}
 : ${GNULIBSRC=$HOME/gnu/src/gnulib}
-: ${TEXINFO=/u/texinfo/src}
+: ${LIBCSRC=$HOME/gnu/src/libc}
+: ${TEXINFOSRC=/u/texinfo/src}
 : ${TEXMF=/usr/local/texmf/texmf}
