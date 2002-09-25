9ec289d2919eb794ca337f270605c4a1a39b8871
Karl Berry <karl@freefriends.org>
2002-09-25 14:18:57 +0000

    update from automake 1.6

--- config/srclistvars.sh
+++ config/srclistvars.sh
@@ -1,14 +1,16 @@
-# $Id: srclistvars.sh,v 1.4 2002-09-24 15:40:12 karl Exp $
+# $Id: srclistvars.sh,v 1.5 2002-09-25 14:18:57 karl Exp $
 # Variables for srclist-update and srclist.txt.
-# Definitely will change for each user.
+# Will change for each user.
 
 : ${ACLOCAL=/usr/local/gnu/share/aclocal}
 : ${AUTOCONF=/usr/local/gnu/share/autoconf}
+: ${AUTOMAKE=/usr/local/gnu/share/automake-1.6}
 : ${EMACSSRC=$HOME/gnu/src/emacs}
 : ${GETTEXT=/usr/local/gnu/share/gettext}
 : ${GNUBIN=/usr/local/gnu/bin}
 : ${GNUCONFIG=$HOME/gnu/src/ftp.gnu.org/pub/gnu/config}
 : ${GNULIBSRC=$HOME/gnu/src/gnulib}
+: ${GNUORG=$HOME/gnu/gnuorg}
 : ${LIBCSRC=$HOME/gnu/src/libc}
 : ${TEXINFOSRC=/u/texinfo/src}
 : ${TEXMF=/usr/local/texmf/texmf}
