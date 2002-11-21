146a08ca25be72049c5385e6484291f08012d78b
Jim Meyering <jim@meyering.net>
2002-11-21 09:44:27 +0000

    (make_path): Restore umask *before* creating the final component.

--- lib/makepath.c
+++ lib/makepath.c
@@ -347,6 +347,9 @@ make_path (const char *argpath,
       if (!do_chdir)
 	basename_dir = dirpath;
 
+      /* Done creating leading directories.  Restore original umask.  */
+      umask (oldmask);
+
       /* We're done making leading directories.
 	 Create the final component of the path.  */
 
@@ -355,9 +358,6 @@ make_path (const char *argpath,
 	  CLEANUP;
 	  return 1;
 	}
-
-      /* Done creating directories.  Restore original umask.  */
-      umask (oldmask);
 
       if (verbose_fmt_string != NULL)
 	error (0, 0, verbose_fmt_string, quote (dirpath));
