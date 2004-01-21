d95c60093b25e22a7bc736a3e61b08d489027ffc
Jim Meyering <jim@meyering.net>
2004-01-21 22:10:43 +0000

    (__mktime_internal) [!_LIBC]: Define to mktime_internal so as not to conflict with a different-sized __mktime_internal function in GNU libc.

--- lib/timegm.c
+++ lib/timegm.c
@@ -1,6 +1,6 @@
 /* Convert UTC calendar time to simple time.  Like mktime but assumes UTC.
 
-   Copyright (C) 1994, 1997, 2003 Free Software Foundation, Inc.
+   Copyright (C) 1994, 1997, 2003, 2004 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    This program is free software; you can redistribute it and/or modify
@@ -28,6 +28,7 @@
 # include <time_r.h>
 # undef __gmtime_r
 # define __gmtime_r gmtime_r
+# define __mktime_internal mktime_internal
 time_t __mktime_internal (struct tm *,
 			  struct tm * (*) (time_t const *, struct tm *),
 			  time_t *);
