9a99a7c3d49a245f5ea41481012f65da34abc2ae
Paul Eggert <eggert@cs.ucla.edu>
2004-09-28 23:58:39 +0000

    Change copyright notice to FSF, and change license to GPL.

--- lib/dummy.c
+++ lib/dummy.c
@@ -1,18 +1,18 @@
 /* A dummy file, to prevent empty libraries from breaking builds.
-   Copyright (C) 2004 Simon Josefsson
+   Copyright (C) 2004 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU Library General Public License as published
-   by the Free Software Foundation; either version 2, or (at your option)
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
-   License along with this program; if not, write to the Free Software
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
    USA.  */
 
