c60d1bf2a758b6496f8647d28db766d78f6ada02
Paul Eggert <eggert@cs.ucla.edu>
2004-10-04 04:18:46 +0000

    Fix years in copyright notices.

--- lib/dirfd.h
+++ lib/dirfd.h
@@ -1,5 +1,5 @@
 /* Declare dirfd, if necessary.
-   Copyright (C) 2004 Free Software Foundation, Inc.
+   Copyright (C) 2001, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
