ec03ca352064740b271b27e0be2b562b687b4554
Bruno Haible <bruno@clisp.org>
2004-10-04 11:25:57 +0000

    New module 'memmem', from Simon Josefsson.

---
+++ lib/memmem.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2004 Free Software Foundation
+ * Written by Simon Josefsson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.  */
+
+#ifndef MEMMEM_H
+# define MEMMEM_H
+
+/* Get memmem, if available. */
+# include <string.h>
+
+# if defined HAVE_DECL_MEMMEM && !HAVE_DECL_MEMMEM
+void *
+memmem (const void *haystack, size_t haystack_len,
+	const void *needle, size_t needle_len);
+# endif
+
+#endif /* MEMMEM_H */
---
+++ tests/test-memmem.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2004 Free Software Foundation
+ * Written by Simon Josefsson
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.  */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include "memmem.h"
+
+int
+main (int argc, char *argv[])
+{
+  const char *big = "foo-bar-baz";
+  char *p;
+
+  p = memmem (big, "foo", strlen (big));
+  if (p != big)
+    {
+      fprintf (stderr, "memmem FAILURE 1\n");
+      return 1;
+    }
+
+  p = memmem (big, "baz", strlen (big));
+  if (p != big + strlen (big) - 3)
+    {
+      fprintf (stderr, "memmem FAILURE 2\n");
+      return 1;
+
+  p = memmem (big, "-", strlen (big));
+  if (p != big + 3)
+    {
+      fprintf (stderr, "memmem FAILURE 3\n");
+      return 1;
+    }
+
+  p = memmem (big, "baz", strlen (big) - 1);
+  if (p != NULL)
+    {
+      fprintf (stderr, "memmem FAILURE 4\n");
+      return 1;
+    }
+
+  return 0;
+}
