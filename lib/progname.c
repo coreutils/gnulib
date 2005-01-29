e57dfac3355164cfbfcb5473a42130dab793fe9f
Bruno Haible <bruno@clisp.org>
2005-01-29 15:11:35 +0000

    Fix a link error on MacOS X.

--- lib/progname.c
+++ lib/progname.c
@@ -1,5 +1,5 @@
 /* Program name management.
-   Copyright (C) 2001-2003 Free Software Foundation, Inc.
+   Copyright (C) 2001-2003, 2005 Free Software Foundation, Inc.
    Written by Bruno Haible <haible@clisp.cons.org>, 2001.
 
    This program is free software; you can redistribute it and/or modify
@@ -31,7 +31,7 @@
 
 /* String containing name the program is called with.
    To be initialized by main().  */
-const char *program_name;
+const char *program_name = NULL;
 
 /* Set program_name, based on argv[0].  */
 void
