9df4453b386268b137f71b0e9548f67e03565dd4
Bruno Haible <bruno@clisp.org>
2005-08-11 11:45:05 +0000

    Change parameter name to match comment.

--- lib/strnlen.h
+++ lib/strnlen.h
@@ -26,7 +26,7 @@
 /* Find the length (number of bytes) of STRING, but scan at most
    MAXLEN bytes.  If no '\0' terminator is found in that many bytes,
    return MAXLEN.  */
-extern size_t strnlen(const char *s, size_t maxlen);
+extern size_t strnlen(const char *string, size_t maxlen);
 #endif
 
 #endif /* STRNLEN_H */
