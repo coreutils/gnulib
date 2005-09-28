dee579126faa0f688b4fd608b4d8cf8124bf8025
Bruno Haible <bruno@clisp.org>
2005-09-28 12:00:59 +0000

    Test verify_true instead of verify_expr.

--- tests/test-verify.c
+++ tests/test-verify.c
@@ -12,6 +12,11 @@ verify (c == 2);                  /* should be ok */
 verify (1 + 1 == 3);              /* should give ERROR */
 verify (1 == 1); verify (1 == 1); /* should be ok */
 
+enum
+{
+  item = verify_true (1 == 1) * 0 + 17 /* should be ok */
+};
+
 int function (int n)
 {
   verify (n >= 0);                  /* should give ERROR: non-constant expression */
@@ -20,7 +25,7 @@ int function (int n)
   verify (1 == 1); verify (1 == 1); /* should be ok */
 
   if (n)
-    return (verify_expr (1 == 1), verify_expr (1 == 1), 7); /* should be ok */
+    return (verify_true (1 == 1), verify_true (1 == 1), 7); /* should be ok */
   else
-    return (verify_expr (1 == 2), 5); /* should give ERROR */
+    return (verify_true (1 == 2), 5); /* should give ERROR */
 }
