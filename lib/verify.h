dddf08d8738ed46c097c018b89385205066c96bf
Paul Eggert <eggert@cs.ucla.edu>
2005-10-05 06:50:03 +0000

    * verify.h (verify_true): Provide alternative definition for C++.

--- lib/verify.h
+++ lib/verify.h
@@ -36,9 +36,16 @@
 /* Verify requirement R at compile-time, as an integer constant expression.
    Return true.  */
 
-# define verify_true(R) \
-    (!!sizeof \
-     (struct { unsigned int verify_error_if_negative_size__ : (R) ? 1 : -1; }))
+# ifdef __cplusplus
+template <int w>
+  struct verify_type__ { unsigned int verify_error_if_negative_size__: w; };
+#  define verify_true(R) \
+     (!!sizeof (verify_type__<(R) ? 1 : -1>))
+# else
+#  define verify_true(R) \
+     (!!sizeof \
+      (struct { unsigned int verify_error_if_negative_size__: (R) ? 1 : -1; }))
+# endif
 
 /* Verify requirement R at compile-time, as a declaration without a
    trailing ';'.  */
