62f66280328dc39f3c568281ecd46015fb84a344
Simon Josefsson <simon@josefsson.org>
2005-10-05 07:39:50 +0000

    Don't protect sys/types.h with HAVE_SYS_TYPES_H, we assume all systems have it, suggested by Jim Meyering <jim@meyering.net>.

--- lib/getaddrinfo.h
+++ lib/getaddrinfo.h
@@ -19,14 +19,12 @@
 #ifndef GETADDRINFO_H
 # define GETADDRINFO_H
 
-/* Get all getaddrinfo related declarations, if available.  */
-# ifdef HAVE_SYS_TYPES_H
 /* sys/socket.h in i386-unknown-freebsd4.10 and
    powerpc-apple-darwin5.5 require sys/types.h, so include it first.
    Then we'll also get 'socklen_t' and 'struct sockaddr' which are
    used below. */
-#  include <sys/types.h>
-# endif
+# include <sys/types.h>
+/* Get all getaddrinfo related declarations, if available.  */
 # ifdef HAVE_SYS_SOCKET_H
 #  include <sys/socket.h>
 # endif
