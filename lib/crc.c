cfc51ab90f2f33933de15dd164027c32ebb8b1ac
Simon Josefsson <simon@josefsson.org>
2005-10-21 12:39:10 +0000

    * crc.c (crc32_update): Actually use crc parameter, suggested by Bruno Haible <bruno@clisp.org>.

--- lib/crc.c
+++ lib/crc.c
@@ -97,7 +97,7 @@ crc32_no_xor (const char *buf, size_t len)
 uint32_t
 crc32_update (uint32_t crc, const char *buf, size_t len)
 {
-  return crc32_update_no_xor (0L ^ 0xffffffffL, buf, len) ^ 0xffffffffL;
+  return crc32_update_no_xor (crc ^ 0xffffffffL, buf, len) ^ 0xffffffffL;
 }
 
 uint32_t
