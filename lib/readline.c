04100c39ec27510503ef803a99d895c943ddac12
Simon Josefsson <simon@josefsson.org>
2005-11-10 02:19:29 +0000

    * readline.c: Remove EOL.

--- lib/readline.c
+++ lib/readline.c
@@ -33,6 +33,7 @@
 #include "readline.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <getline.h>
 
 char *
@@ -46,6 +47,11 @@ readline (const char *prompt)
 
   if (getline (&out, &size, stdin) < 0)
     return NULL;
+
+  if (out[strlen (out) - 1] == '\r')
+    out[strlen (out) - 1] = '\0';
+  if (out[strlen (out) - 1] == '\n')
+    out[strlen (out) - 1] = '\0';
 
   return out;
 }
