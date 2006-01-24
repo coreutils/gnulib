003302be5715894e3ef497d1ec5b22414b5cd276
Jim Meyering <jim@meyering.net>
2006-01-24 12:53:13 +0000

    Remove useless parentheses in uses of cpp `defined'.

--- lib/socket_.h
+++ lib/socket_.h
@@ -35,13 +35,13 @@
 #endif
 
 /* For shutdown(). */
-#if !defined(SHUT_RD) && defined (SD_RECEIVE)
+#if !defined SHUT_RD && defined SD_RECEIVE
 # define SHUT_RD SD_RECEIVE
 #endif
-#if !defined(SHUT_WR) && defined (SD_SEND)
+#if !defined SHUT_WR && defined SD_SEND
 # define SHUT_WR SD_SEND
 #endif
-#if !defined(SHUT_RDWR) && defined (SD_BOTH)
+#if !defined SHUT_RDWR && defined SD_BOTH
 # define SHUT_RDWR SD_BOTH
 #endif
 
