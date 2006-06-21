baad693b4dba18d24f649f4bd4bd24b033e0556a
Simon Josefsson <simon@josefsson.org>
2006-06-21 10:00:08 +0000

    Add inet_pton.

--- MODULES.html.sh
+++ MODULES.html.sh
@@ -1794,6 +1794,7 @@ func_all_modules ()
   func_module getsubopt
   func_module gettimeofday
   func_module inet_ntop
+  func_module inet_pton
   func_module mkdir
   func_module mkstemp
   func_module mkdtemp
