361c5154c63de94ec3571612225de4a6933189b7
Simon Josefsson <simon@josefsson.org>
2006-07-22 10:13:57 +0000

    2006-07-22  Yoann Vandoorselaere <yoann.v@prelude-ids.com> 	and Simon Josefsson <jas@extundo.com>
    	* getaddrinfo.h (AI_PASSIVE): Make sure it is defined.
    
    	* getaddrinfo.c (getaddrinfo): Support AI_PASSIVE.

--- lib/getaddrinfo.h
+++ lib/getaddrinfo.h
@@ -48,12 +48,16 @@ struct addrinfo
 #endif
 
 /* Possible values for `ai_flags' field in `addrinfo' structure.  */
+#ifndef AI_PASSIVE
+# define AI_PASSIVE	0x0001	/* Socket address is intended for `bind'.  */
+#endif
 #ifndef AI_CANONNAME
 # define AI_CANONNAME	0x0002	/* Request for canonical name.  */
 #endif
 #ifndef AI_NUMERICSERV
 # define AI_NUMERICSERV	0x0400	/* Don't use name resolution.  */
 #endif
+
 #if 0
 /* The commented out definitions below are not yet implemented in the
    GNULIB getaddrinfo() replacement, so are not yet needed and may, in fact,
@@ -61,7 +65,6 @@ struct addrinfo
    define them.
 
    If they are restored, be sure to protect the definitions with #ifndef.  */
-#define AI_PASSIVE	0x0001	/* Socket address is intended for `bind'.  */
 #define AI_NUMERICHOST	0x0004	/* Don't use name resolution.  */
 #define AI_V4MAPPED	0x0008	/* IPv4 mapped addresses are acceptable.  */
 #define AI_ALL		0x0010	/* Return IPv4 mapped and IPv6 addresses.  */
