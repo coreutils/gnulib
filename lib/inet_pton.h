cef62fd4295a59b748877c3928beb342aab758b6
Simon Josefsson <simon@josefsson.org>
2006-07-28 16:28:08 +0000

    2006-07-28  Bruno Haible <bruno@clisp.org>
    	* inet_ntop.h, inet_pton.h: Use #if HAVE* instead of #ifdef HAVE*.
    
    2006-07-28  Yoann Vandoorselaere <yoann.v@prelude-ids.com>
    
    	* inet_ntop.h, inet_pton.h: Need to include netinet/in.h before
    	arpa/inet.h.

--- lib/inet_pton.h
+++ lib/inet_pton.h
@@ -17,7 +17,10 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
-#ifdef HAVE_ARPA_INET_H
+#if HAVE_NETINET_IN_H
+# include <netinet/in.h>
+#endif
+#if HAVE_ARPA_INET_H
 # include <arpa/inet.h>
 #endif
 
