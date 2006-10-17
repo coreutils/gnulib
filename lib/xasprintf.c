bac726e11dba0e61427e84b0d34fd6fb3290a5c6
Eric Blake <ebb9@byu.net>
2006-10-17 12:16:04 +0000

    * lib/xvasprintf.c (includes): Assume config.h. * lib/xasprintf.c (includes): Likewise.

--- lib/xasprintf.c
+++ lib/xasprintf.c
@@ -1,5 +1,5 @@
 /* vasprintf and asprintf with out-of-memory checking.
-   Copyright (C) 1999, 2002-2004 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2002-2004, 2006 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -15,9 +15,7 @@
    with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
 
-#ifdef HAVE_CONFIG_H
-# include <config.h>
-#endif
+#include <config.h>
 
 /* Specification.  */
 #include "xvasprintf.h"
