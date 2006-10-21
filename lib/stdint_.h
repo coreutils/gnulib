8b794fd95e474cbe19ee9a63e87cb3378935539e
Paul Eggert <eggert@cs.ucla.edu>
2006-10-21 22:10:06 +0000

    b2006-10-21  Paul Eggert  <eggert@cs.ucla.edu>
    * lib/stdint_.h [defined _AIX]: Don't include <sys/types.h>.
    Problem reported by Perry Smith and Ville Laurikari.

--- lib/stdint_.h
+++ lib/stdint_.h
@@ -48,10 +48,11 @@
 
 /* <sys/types.h> defines some of the stdint.h types as well, on glibc,
    IRIX 6.5, and OpenBSD 3.8 (via <machine/types.h>).
+   AIX 5.2 <sys/types.h> isn't needed and causes troubles.
    MacOS X 10.4.6 <sys/types.h> includes <stdint.h> (which is us), but
    relies on the system <stdint.h> definitions, so include
    <sys/types.h> after @ABSOLUTE_STDINT_H@.  */
-#if @HAVE_SYS_TYPES_H@
+#if @HAVE_SYS_TYPES_H@ && ! defined _AIX
 # include <sys/types.h>
 #endif
 
