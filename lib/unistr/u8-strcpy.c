32010832553b80e02a45590314181456bc951aa5
Bruno Haible <bruno@clisp.org>
2007-01-09 14:09:43 +0000

    New modules 'unistr/u8-strcpy', 'unistr/u16-strcpy', 'unistr/u32-strcpy'.

---
+++ lib/unistr/u8-strcpy.c
@@ -0,0 +1,31 @@
+/* Copy UTF-8 string.
+   Copyright (C) 2002, 2006 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2002.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "unistr.h"
+
+#include <string.h>
+
+uint8_t *
+u8_strcpy (uint8_t *dest, const uint8_t *src)
+{
+  return (uint8_t *) strcpy ((char *) dest, (const char *) src);
+}
