1426f5a70d23f84b079e9ae052dc1de829f643e7
Bruno Haible <bruno@clisp.org>
2007-01-09 14:09:01 +0000

    New modules 'unistr/u8-mbtouc', 'unistr/u16-mbtouc', 'unistr/u32-mbtouc'.

---
+++ lib/unistr/u16-mbtouc.c
@@ -0,0 +1,62 @@
+/* Look at first character in UTF-16 string.
+   Copyright (C) 1999-2002, 2006-2007 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2001.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "unistr.h"
+
+#if !HAVE_INLINE
+
+int
+u16_mbtouc (ucs4_t *puc, const uint16_t *s, size_t n)
+{
+  uint16_t c = *s;
+
+  if (c < 0xd800 || c >= 0xe000)
+    {
+      *puc = c;
+      return 1;
+    }
+#if CONFIG_UNICODE_SAFETY
+  if (c < 0xdc00)
+#endif
+    {
+      if (n >= 2)
+	{
+#if CONFIG_UNICODE_SAFETY
+	  if (s[1] >= 0xdc00 && s[1] < 0xe000)
+#endif
+	    {
+	      *puc = 0x10000 + ((c - 0xd800) << 10) + (s[1] - 0xdc00);
+	      return 2;
+	    }
+	  /* invalid multibyte character */
+	}
+      else
+	{
+	  /* incomplete multibyte character */
+	}
+    }
+  /* invalid multibyte character */
+  *puc = 0xfffd;
+  return 1;
+}
+
+#endif
