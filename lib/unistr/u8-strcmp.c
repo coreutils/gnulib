3aa8af7b48c544b3c4c9b63103eb32670368874f
Bruno Haible <bruno@clisp.org>
2007-01-09 14:09:39 +0000

    New modules 'unistr/u8-strcmp', 'unistr/u16-strcmp', 'unistr/u32-strcmp'.

---
+++ lib/unistr/u8-strcmp.c
@@ -0,0 +1,32 @@
+/* Compare UTF-8 strings.
+   Copyright (C) 2002, 2006 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2002.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "unistr.h"
+
+#include <string.h>
+
+int
+u8_strcmp (const uint8_t *s1, const uint8_t *s2)
+{
+  /* Use the fact that the UTF-8 encoding preserves lexicographic order.  */
+  return strcmp ((const char *) s1, (const char *) s2);
+}
