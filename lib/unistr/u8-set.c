66a155e5a0ac7ecf4e31fa931242413682e54613
Bruno Haible <bruno@clisp.org>
2007-01-09 14:09:18 +0000

    New modules 'unistr/u8-set', 'unistr/u16-set', 'unistr/u32-set'.

---
+++ lib/unistr/u8-set.c
@@ -0,0 +1,46 @@
+/* Fill UTF-8 string.
+   Copyright (C) 2002, 2006 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2002.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "unistr.h"
+
+#define FUNC u8_set
+#define UNIT uint8_t
+#define IS_SINGLE_UNIT(uc) (uc < 0x80)
+
+#include <errno.h>
+#include <string.h>
+
+UNIT *
+FUNC (UNIT *s, ucs4_t uc, size_t n)
+{
+  if (n > 0)
+    {
+      if (IS_SINGLE_UNIT (uc))
+	memset ((char *) s, uc, n);
+      else
+	{
+	  errno = EILSEQ;
+	  return NULL;
+	}
+    }
+  return s;
+}
