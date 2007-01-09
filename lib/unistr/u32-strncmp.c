e7624ee96a0ab7bd6cb88811205b8c4a997fdb93
Bruno Haible <bruno@clisp.org>
2007-01-09 14:10:07 +0000

    New modules 'unistr/u8-strncmp', 'unistr/u16-strncmp', 'unistr/u32-strncmp'.

---
+++ lib/unistr/u32-strncmp.c
@@ -0,0 +1,42 @@
+/* Compare UTF-32 strings.
+   Copyright (C) 1999, 2002, 2006 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2002.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "unistr.h"
+
+int
+u32_strncmp (const uint32_t *s1, const uint32_t *s2, size_t n)
+{
+  for (; n > 0;)
+    {
+      uint32_t uc1 = *s1++;
+      uint32_t uc2 = *s2++;
+      if (uc1 != 0 && uc1 == uc2)
+	{
+	  n--;
+	  continue;
+	}
+      /* Note that uc1 and uc2 each have at most 31 bits. */
+      return (int)uc1 - (int)uc2;
+      /* > 0 if uc1 > uc2, < 0 if uc1 < uc2, = 0 if uc1 and uc2 are both 0.  */
+    }
+  return 0;
+}
