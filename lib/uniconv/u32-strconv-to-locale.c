8c2094a680d68b47cfdc8eb84938c1f3e59f367d
Bruno Haible <bruno@clisp.org>
2007-01-27 00:53:08 +0000

    New module 'uniconv/u32-strconv-to-locale'.

---
+++ lib/uniconv/u32-strconv-to-locale.c
@@ -0,0 +1,31 @@
+/* Conversion from UTF-32 to the locale encoding.
+   Copyright (C) 2002, 2006-2007 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "uniconv.h"
+
+char *
+u32_strconv_to_locale (const uint32_t *string)
+{
+  const char *encoding = locale_charset ();
+  return u32_strconv_to_encoding (string, encoding, iconveh_question_mark);
+}
