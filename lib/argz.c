5a7735c229ffc6b9c3224263ed710d2130cc1a8d
Ralf Wildenhues <Ralf.Wildenhues@gmx.de>
2007-01-27 16:51:39 +0000

    Fix Copyright years.

--- lib/argz.c
+++ lib/argz.c
@@ -1,5 +1,5 @@
 /* argz.c -- argz implementation for non-glibc systems
-   Copyright (C) 2004, 2006 Free Software Foundation, Inc.
+   Copyright (C) 2004, 2006, 2007 Free Software Foundation, Inc.
    Originally by Gary V. Vaughan  <gary@gnu.org>
 
    NOTE: The canonical source of this file is maintained with the
