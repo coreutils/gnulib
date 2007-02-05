ed081024933a7c415cb2872f847357102cf6595b
Paul Eggert <eggert@cs.ucla.edu>
2007-02-05 05:44:11 +0000

    * lib/getsubopt.c [!_LIBC]: Include config.h and getsubopt.h. * lib/memmem.c [!defined _LIBC]: Include config.h.

--- lib/getsubopt.c
+++ lib/getsubopt.c
@@ -17,6 +17,11 @@
    with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
 
+#if !_LIBC
+# include <config.h>
+# include "getsubopt.h"
+#endif
+
 #include <stdlib.h>
 #include <string.h>
 
--- lib/memmem.c
+++ lib/memmem.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991,92,93,94,96,97,98,2000,2004 Free Software Foundation, Inc.
+/* Copyright (C) 1991,92,93,94,96,97,98,2000,2004,2007 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    This program is free software; you can redistribute it and/or modify
@@ -14,6 +14,10 @@
    You should have received a copy of the GNU General Public License along
    with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+#ifndef _LIBC
+# include <config.h>
+#endif
 
 #include <stddef.h>
 #include <string.h>
