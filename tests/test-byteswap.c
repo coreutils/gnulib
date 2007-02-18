f2c3077010e6b60705ca5ab66f72ccc875081995
Bruno Haible <bruno@clisp.org>
2007-02-18 04:12:20 +0000

    Tests for module 'byteswap'.

---
+++ tests/test-byteswap.c
@@ -0,0 +1,35 @@
+/* Test of <byteswap.h> substitute.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2007.  */
+
+#include <config.h>
+
+#include <byteswap.h>
+
+#include <stdlib.h>
+
+#define ASSERT(condition) if (!(condition)) abort ()
+
+int
+main ()
+{
+  ASSERT (bswap_16 (0xABCD) == 0xCDAB);
+  ASSERT (bswap_32 (0xDEADBEEF) == 0xEFBEADDE);
+
+  return 0;
+}
