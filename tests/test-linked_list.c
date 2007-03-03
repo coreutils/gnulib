20af5d475a09dd19cb7be12d78f0599d7ba48e54
Bruno Haible <bruno@clisp.org>
2007-03-03 12:42:29 +0000

    Avoid link error due to program_name.

--- tests/test-linked_list.c
+++ tests/test-linked_list.c
@@ -1,5 +1,5 @@
 /* Test of sequential list data type implementation.
-   Copyright (C) 2006 Free Software Foundation, Inc.
+   Copyright (C) 2006-2007 Free Software Foundation, Inc.
    Written by Bruno Haible <bruno@clisp.org>, 2006.
 
    This program is free software; you can redistribute it and/or modify
@@ -20,10 +20,12 @@
 # include <config.h>
 #endif
 
+#include "gl_linked_list.h"
+
 #include <stdlib.h>
 
 #include "gl_array_list.h"
-#include "gl_linked_list.h"
+#include "progname.h"
 
 static const char *objects[15] =
   {
@@ -59,6 +61,8 @@ int
 main (int argc, char *argv[])
 {
   gl_list_t list1, list2, list3;
+
+  set_program_name (argv[0]);
 
   /* Allow the user to provide a non-default random seed on the command line.  */
   if (argc > 1)
