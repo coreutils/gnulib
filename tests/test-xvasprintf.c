c9f0732d2e84d603e7e88df3222505ab8a80005c
Bruno Haible <bruno@clisp.org>
2007-03-04 18:05:48 +0000

    Tests for module 'xvasprintf'.

---
+++ tests/test-xvasprintf.c
@@ -0,0 +1,85 @@
+/* Test of xvasprintf() and xasprintf() functions.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2007.  */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include "xvasprintf.h"
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "progname.h"
+
+#define ASSERT(expr) if (!(expr)) abort ();
+
+static char *
+my_xasprintf (const char *format, ...)
+{
+  va_list args;
+  char *ret;
+
+  va_start (args, format);
+  ret = xvasprintf (format, args);
+  va_end (args);
+  return ret;
+}
+
+static void
+test_xvasprintf ()
+{
+  char buf[8];
+  int repeat;
+
+  for (repeat = 0; repeat <= 8; repeat++)
+    {
+      char *result = my_xasprintf ("%d", 12345);
+      ASSERT (result != NULL);
+      ASSERT (strcmp (result, "12345") == 0);
+      free (result);
+    }
+}
+
+static void
+test_xasprintf ()
+{
+  char buf[8];
+  int repeat;
+
+  for (repeat = 0; repeat <= 8; repeat++)
+    {
+      char *result = xasprintf ("%d", 12345);
+      ASSERT (result != NULL);
+      ASSERT (strcmp (result, "12345") == 0);
+      free (result);
+    }
+}
+
+int
+main (int argc, char *argv[])
+{
+  set_program_name (argv[0]);
+
+  test_xvasprintf ();
+  test_xasprintf ();
+
+  return 0;
+}
