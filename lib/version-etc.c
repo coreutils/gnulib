b906b9b9297c86e229d02384f3d3a8552edf883a
Eric Blake <ebb9@byu.net>
2007-03-23 12:37:17 +0000

    * lib/version-etc.c (version_etc_va): Update license wording to be more concise.  Recommended by Richard Stallman.

--- lib/version-etc.c
+++ lib/version-etc.c
@@ -67,8 +67,8 @@ version_etc_va (FILE *stream,
 
   fputs (_("\
 \n\
-This is free software.  You may redistribute copies of it under the terms of\n\
-the GNU General Public License <http://www.gnu.org/licenses/gpl.html>.\n\
+License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl.html>\n\
+This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n\
 \n\
 "),
