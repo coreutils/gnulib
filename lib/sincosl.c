896763bc42eb7fb9a10e92f16837d11bbaf49d9c
Bruno Haible <bruno@clisp.org>
2007-03-25 17:58:06 +0000

    Avoid compiler warning.

--- lib/sincosl.c
+++ lib/sincosl.c
@@ -230,7 +230,7 @@ kernel_cosl (long double x, long double y)
    Computed using gmp.
  */
 
-const long double sincosl_table[] = {
+static const long double sincosl_table[] = {
 
 /* x =  1.48437500000000000000000000000000000e-01L 3ffc3000000000000000000000000000 */
 /* cos(x) = 0.fd2f5320e1b790209b4dda2f98f79caaa7b873aff1014b0fbc5243766d03cb006bc837c4358 */
