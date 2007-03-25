192bd848d1106fc68d24e24e61534d6d9d49a335
Bruno Haible <bruno@clisp.org>
2007-03-25 02:48:08 +0000

    Tests for module 'tsearch'.

---
+++ tests/test-tsearch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+tmpfiles=""
+trap 'rm -fr $tmpfiles' 1 2 3 15
+
+tmpfiles="$tmpfiles t-tsearch.out"
+./test-tsearch${EXEEXT} > t-tsearch.out 2>&1
+test $? = 0 || { cat t-tsearch.out 1>&2; rm -f $tmpfiles; exit 1; }
+
+rm -f $tmpfiles
+
+exit 0
