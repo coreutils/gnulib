6a595066f7444ae86941cabf229b12938622faa3
Bruno Haible <bruno@clisp.org>
2007-04-10 23:41:17 +0000

    New module 'locale'.

---
+++ lib/locale_.h
@@ -0,0 +1,29 @@
+/* A POSIX <locale.h>.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+#ifndef _GL_LOCALE_H
+#define _GL_LOCALE_H
+
+#include @ABSOLUTE_LOCALE_H@
+
+/* The LC_MESSAGES locale category is specified in POSIX, but not in ISO C.
+   On systems that don't define it, use the same value as GNU libintl.  */
+#if !defined LC_MESSAGES
+# define LC_MESSAGES 1729
+#endif
+
+#endif /* _GL_LOCALE_H */
---
+++ m4/locale_h.m4
@@ -0,0 +1,24 @@
+# locale_h.m4 serial 1
+dnl Copyright (C) 2007 Free Software Foundation, Inc.
+dnl This file is free software; the Free Software Foundation
+dnl gives unlimited permission to copy and/or distribute it,
+dnl with or without modifications, as long as this notice is preserved.
+
+AC_DEFUN([gl_LOCALE_H],
+[
+  AC_CACHE_CHECK([whether locale.h conforms to POSIX],
+    [gl_cv_header_working_locale_h],
+    [AC_TRY_COMPILE([#include <locale.h>
+int x = LC_MESSAGES;], [],
+       [gl_cv_header_working_locale_h=yes],
+       [gl_cv_header_working_locale_h=no])])
+  if test $gl_cv_header_working_locale_h = yes; then
+    LOCALE_H=
+  else
+    gl_ABSOLUTE_HEADER([locale.h])
+    ABSOLUTE_LOCALE_H=\"$gl_cv_absolute_locale_h\"
+    AC_SUBST([ABSOLUTE_LOCALE_H])
+    LOCALE_H=locale.h
+  fi
+  AC_SUBST([LOCALE_H])
+])
