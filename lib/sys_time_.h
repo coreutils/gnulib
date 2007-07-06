1b9bd2b9f83a84ede12622d6a040383b6bded37d
Paul Eggert <eggert@cs.ucla.edu>
2007-07-06 16:42:06 +0000

    * lib/sys_time_.h: Use a recursion-safe inclusion guard rather than a split double-inclusion guard. Needed for cygwin, where <sys/time.h> includes <cygwin/sys_time.h> which includes <sys/select.h> which include <sys/time.h>. Reported by Eric Blake.

--- lib/sys_time_.h
+++ lib/sys_time_.h
@@ -18,31 +18,35 @@
 
 /* Written by Paul Eggert.  */
 
-#ifndef _GL_SYS_TIME_H
+#if defined _GL_SYS_TIME_H
 
-/* The include_next requires a split double-inclusion guard.  */
-#if @HAVE_SYS_TIME_H@
-# @INCLUDE_NEXT@ @NEXT_SYS_TIME_H@
+/* Simply delegate to the system's header, without adding anything.  */
+# if @HAVE_SYS_TIME_H@
+#  @INCLUDE_NEXT@ @NEXT_SYS_TIME_H@
+# endif
+
 #else
-# include <time.h>
-#endif
 
-#ifndef _GL_SYS_TIME_H
-#define _GL_SYS_TIME_H
+# define _GL_SYS_TIME_H
 
-#if ! @HAVE_STRUCT_TIMEVAL@
+# if @HAVE_SYS_TIME_H@
+#  @INCLUDE_NEXT@ @NEXT_SYS_TIME_H@
+# else
+#  include <time.h>
+# endif
+
+# if ! @HAVE_STRUCT_TIMEVAL@
 struct timeval
 {
   time_t tv_sec;
   long int tv_usec;
 };
-#endif
+# endif
 
-#if @REPLACE_GETTIMEOFDAY@
-# undef gettimeofday
-# define gettimeofday rpl_gettimeofday
+# if @REPLACE_GETTIMEOFDAY@
+#  undef gettimeofday
+#  define gettimeofday rpl_gettimeofday
 int gettimeofday (struct timeval *restrict, void *restrict);
-#endif
+# endif
 
-#endif /* _GL_SYS_TIME_H */
 #endif /* _GL_SYS_TIME_H */
