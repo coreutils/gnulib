b5fc65bfbfc06bebb497c812d5586f83f7e96971
Bruno Haible <bruno@clisp.org>
2007-07-07 16:24:12 +0000

    New module 'uniwidth/u8-strwidth'.

---
+++ lib/uniwidth/u8-strwidth.c
@@ -0,0 +1,31 @@
+/* Determine display width of UTF-8 string.
+   Copyright (C) 2001-2002, 2006 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2002.
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Library General Public License as published
+   by the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+   USA.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "uniwidth.h"
+
+#include "unistr.h"
+
+int
+u8_strwidth (const uint8_t *s, const char *encoding)
+{
+  return u8_width (s, u8_strlen (s), encoding);
+}
