9ae286b3b3918e354fafeaafd8082d37b7c90460
Bruno Haible <bruno@clisp.org>
2007-10-05 03:44:19 +0200

    Test for module 'ceilf'.

---
+++ tests/test-ceilf.c
@@ -0,0 +1,84 @@
+/* Test of rounding towards positive infinity.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2007.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ASSERT(expr) \
+  do									     \
+    {									     \
+      if (!(expr))							     \
+        {								     \
+          fprintf (stderr, "%s:%d: assertion failed\n", __FILE__, __LINE__); \
+          abort ();							     \
+        }								     \
+    }									     \
+  while (0)
+
+/* The Compaq (ex-DEC) C 6.4 compiler chokes on the expression 0.0 / 0.0.  */
+#ifdef __DECC
+static float
+NaN ()
+{
+  static float zero = 0.0f;
+  return zero / zero;
+}
+#else
+# define NaN() (0.0f / 0.0f)
+#endif
+
+int
+main ()
+{
+  /* Zero.  */
+  ASSERT (ceilf (0.0f) == 0.0f);
+  ASSERT (ceilf (-0.0f) == 0.0f);
+  /* Positive numbers.  */
+  ASSERT (ceilf (0.3f) == 1.0f);
+  ASSERT (ceilf (0.7f) == 1.0f);
+  ASSERT (ceilf (1.0f) == 1.0f);
+  ASSERT (ceilf (1.001f) == 2.0f);
+  ASSERT (ceilf (1.5f) == 2.0f);
+  ASSERT (ceilf (1.999f) == 2.0f);
+  ASSERT (ceilf (2.0f) == 2.0f);
+  ASSERT (ceilf (65535.99f) == 65536.0f);
+  ASSERT (ceilf (65536.0f) == 65536.0f);
+  ASSERT (ceilf (2.341e31f) == 2.341e31f);
+  /* Negative numbers.  */
+  ASSERT (ceilf (-0.3f) == 0.0f);
+  ASSERT (ceilf (-0.7f) == 0.0f);
+  ASSERT (ceilf (-1.0f) == -1.0f);
+  ASSERT (ceilf (-1.5f) == -1.0f);
+  ASSERT (ceilf (-1.999f) == -1.0f);
+  ASSERT (ceilf (-2.0f) == -2.0f);
+  ASSERT (ceilf (-65535.99f) == -65535.0f);
+  ASSERT (ceilf (-65536.0f) == -65536.0f);
+  ASSERT (ceilf (-2.341e31f) == -2.341e31f);
+  /* Infinite numbers.  */
+  ASSERT (ceilf (1.0f / 0.0f) == 1.0f / 0.0f);
+  ASSERT (ceilf (-1.0f / 0.0f) == -1.0f / 0.0f);
+  /* NaNs.  */
+  ASSERT (isnan (ceilf (NaN ())));
+
+  return 0;
+}
