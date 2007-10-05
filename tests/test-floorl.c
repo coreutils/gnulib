3b63293b88852539e9df5eab6e4e056878cfb650
Bruno Haible <bruno@clisp.org>
2007-10-05 03:34:16 +0200

    Test for module 'floorl'.

---
+++ tests/test-floorl.c
@@ -0,0 +1,79 @@
+/* Test of rounding towards negative infinity.
+   Copyright (C) 2007 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2007.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fpucw.h"
+#include "isnanl-nolibm.h"
+
+#define ASSERT(expr) \
+  do									     \
+    {									     \
+      if (!(expr))							     \
+        {								     \
+          fprintf (stderr, "%s:%d: assertion failed\n", __FILE__, __LINE__); \
+          abort ();							     \
+        }								     \
+    }									     \
+  while (0)
+
+int
+main ()
+{
+  DECL_LONG_DOUBLE_ROUNDING
+
+  BEGIN_LONG_DOUBLE_ROUNDING ();
+
+  /* Zero.  */
+  ASSERT (floorl (0.0L) == 0.0L);
+  ASSERT (floorl (-0.0L) == 0.0L);
+  /* Positive numbers.  */
+  ASSERT (floorl (0.3L) == 0.0L);
+  ASSERT (floorl (0.7L) == 0.0L);
+  ASSERT (floorl (1.0L) == 1.0L);
+  ASSERT (floorl (1.5L) == 1.0L);
+  ASSERT (floorl (1.999L) == 1.0L);
+  ASSERT (floorl (2.0L) == 2.0L);
+  ASSERT (floorl (65535.999L) == 65535.0L);
+  ASSERT (floorl (65536.0L) == 65536.0L);
+  ASSERT (floorl (2.341e31L) == 2.341e31L);
+  /* Negative numbers.  */
+  ASSERT (floorl (-0.3L) == -1.0L);
+  ASSERT (floorl (-0.7L) == -1.0L);
+  ASSERT (floorl (-1.0L) == -1.0L);
+  ASSERT (floorl (-1.001L) == -2.0L);
+  ASSERT (floorl (-1.5L) == -2.0L);
+  ASSERT (floorl (-1.999L) == -2.0L);
+  ASSERT (floorl (-2.0L) == -2.0L);
+  ASSERT (floorl (-65535.999L) == -65536.0L);
+  ASSERT (floorl (-65536.0L) == -65536.0L);
+  ASSERT (floorl (-2.341e31L) == -2.341e31L);
+  /* Infinite numbers.  */
+  ASSERT (floorl (1.0L / 0.0L) == 1.0L / 0.0L);
+  ASSERT (floorl (-1.0L / 0.0L) == -1.0L / 0.0L);
+  /* NaNs.  */
+  ASSERT (isnanl (floorl (0.0L / 0.0L)));
+
+  return 0;
+}
