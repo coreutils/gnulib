075c5c89a67ef66d6ef96926fd68ba8bbe4d742c
Bruno Haible <bruno@clisp.org>
2007-12-26 16:10:15 +0100

    Add more comments about Knuth-Morris-Pratt algorithm.

--- lib/memmem.c
+++ lib/memmem.c
@@ -45,34 +45,67 @@ knuth_morris_pratt (const char *haystack, const char *last_haystack,
   /* Fill the table.
      For 0 < i < m:
        0 < table[i] <= i is defined such that
-       rhaystack[0..i-1] == needle[0..i-1] and rhaystack[i] != needle[i]
-       implies
-       forall 0 <= x < table[i]: rhaystack[x..x+m-1] != needle[0..m-1],
+       forall 0 < x < table[i]: needle[x..i-1] != needle[0..i-1-x],
        and table[i] is as large as possible with this property.
+     This implies:
+     1) For 0 < i < m:
+          If table[i] < i,
+          needle[table[i]..i-1] = needle[0..i-1-table[i]].
+     2) For 0 < i < m:
+          rhaystack[0..i-1] == needle[0..i-1]
+          and exists h, i <= h < m: rhaystack[h] != needle[h]
+          implies
+          forall 0 <= x < table[i]: rhaystack[x..x+m-1] != needle[0..m-1].
      table[0] remains uninitialized.  */
   {
     size_t i, j;
 
+    /* i = 1: Nothing to verify for x = 0.  */
     table[1] = 1;
     j = 0;
+
     for (i = 2; i < m; i++)
       {
+	/* Here: j = i-1 - table[i-1].
+	   The inequality needle[x..i-1] != needle[0..i-1-x] is known to hold
+	   for x < table[i-1], by induction.
+	   Furthermore, if j>0: needle[i-1-j..i-2] = needle[0..j-1].  */
 	unsigned char b = (unsigned char) needle[i - 1];
 
 	for (;;)
 	  {
+	    /* Invariants: The inequality needle[x..i-1] != needle[0..i-1-x]
+	       is known to hold for x < i-1-j.
+	       Furthermore, if j>0: needle[i-1-j..i-2] = needle[0..j-1].  */
 	    if (b == (unsigned char) needle[j])
 	      {
+		/* Set table[i] := i-1-j.  */
 		table[i] = i - ++j;
 		break;
 	      }
+	    /* The inequality needle[x..i-1] != needle[0..i-1-x] also holds
+	       for x = i-1-j, because
+	         needle[i-1] != needle[j] = needle[i-1-x].  */
 	    if (j == 0)
 	      {
+		/* The inequality holds for all possible x.  */
 		table[i] = i;
 		break;
 	      }
+	    /* The inequality needle[x..i-1] != needle[0..i-1-x] also holds
+	       for i-1-j < x < i-1-j+table[j], because for these x:
+		 needle[x..i-2]
+		 = needle[x-(i-1-j)..j-1]
+		 != needle[0..j-1-(x-(i-1-j))]  (by definition of table[j])
+		    = needle[0..i-2-x],
+	       hence needle[x..i-1] != needle[0..i-1-x].
+	       Furthermore
+		 needle[i-1-j+table[j]..i-2]
+		 = needle[table[j]..j-1]
+		 = needle[0..j-1-table[j]]  (by definition of table[j]).  */
 	    j = j - table[j];
 	  }
+	/* Here: j = i - table[i].  */
       }
   }
 
@@ -164,7 +197,7 @@ memmem (const void *haystack, size_t haystack_len,
     size_t comparison_count = 0;
 
     /* Speed up the following searches of needle by caching its first
-       character.  */
+       byte.  */
     char b = *Needle++;
 
     for (;; Haystack++)
@@ -195,7 +228,7 @@ memmem (const void *haystack, size_t haystack_len,
         outer_loop_count++;
         comparison_count++;
         if (*Haystack == b)
-          /* The first character matches.  */
+          /* The first byte matches.  */
           {
             const char *rhaystack = Haystack + 1;
             const char *rneedle = Needle;
