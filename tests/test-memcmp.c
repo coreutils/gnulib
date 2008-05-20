6731a9abc1604f940a7e12e81084ceb8fddccce2
Bruno Haible <bruno@clisp.org>
2008-05-20 12:55:35 +0200

    More memcmp tests.

--- tests/test-memcmp.c
+++ tests/test-memcmp.c
@@ -37,16 +37,44 @@
 int
 main (void)
 {
-  char foo[] = "foo";
-  char foobar[] = "foobar";
-  char bar[] = "bar";
-
+  /* Test equal / not equal distinction.  */
   ASSERT (memcmp (NULL, NULL, 0) == 0);
-  ASSERT (memcmp (foo, foobar, 2) == 0);
-  ASSERT (memcmp (foo, foobar, 3) == 0);
-  ASSERT (memcmp (foo, foobar, 4) != 0);
-  ASSERT (memcmp (foo, bar, 1) != 0);
-  ASSERT (memcmp (foo, bar, 3) != 0);
+  ASSERT (memcmp ("foo", "foobar", 2) == 0);
+  ASSERT (memcmp ("foo", "foobar", 3) == 0);
+  ASSERT (memcmp ("foo", "foobar", 4) != 0);
+  ASSERT (memcmp ("foo", "bar", 1) != 0);
+  ASSERT (memcmp ("foo", "bar", 3) != 0);
+
+  /* Test less / equal / greater distinction.  */
+  ASSERT (memcmp ("foo", "moo", 4) < 0);
+  ASSERT (memcmp ("moo", "foo", 4) > 0);
+  ASSERT (memcmp ("oomph", "oops", 3) < 0);
+  ASSERT (memcmp ("oops", "oomph", 3) > 0);
+  ASSERT (memcmp ("foo", "foobar", 4) < 0);
+  ASSERT (memcmp ("foobar", "foo", 4) > 0);
+
+  /* Some old versions of memcmp were not 8-bit clean.  */
+  ASSERT (memcmp ("\100", "\201", 1) < 0);
+  ASSERT (memcmp ("\201", "\100", 1) > 0);
+  ASSERT (memcmp ("\200", "\201", 1) < 0);
+  ASSERT (memcmp ("\201", "\200", 1) > 0);
+
+  /* The Next x86 OpenStep bug shows up only when comparing 16 bytes
+     or more and with at least one buffer not starting on a 4-byte boundary.
+     William Lewis provided this test program.   */
+  {
+    char foo[21];
+    char bar[21];
+    int i;
+    for (i = 0; i < 4; i++)
+      {
+	char *a = foo + i;
+	char *b = bar + i;
+	strcpy (a, "--------01111111");
+	strcpy (b, "--------10000000");
+	ASSERT (memcmp (a, b, 16) < 0);
+      }
+  }
 
   return 0;
 }
