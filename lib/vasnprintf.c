889c9cb84b2aff4a4f7ff9c35cacf2adbb0644c4
Jim Meyering <meyering@redhat.com>
2008-05-21 23:09:59 +0200

    avoid another warning from gcc
    * lib/vasnprintf.c (scale10_round_decimal_double):
    Use IF_LINT to avoid another "may be used uninitialized" warning.

--- lib/vasnprintf.c
+++ lib/vasnprintf.c
@@ -1230,7 +1230,7 @@ scale10_round_decimal_long_double (long double x, int n)
 static char *
 scale10_round_decimal_double (double x, int n)
 {
-  int e;
+  int e IF_LINT(= 0);
   mpn_t m;
   void *memory = decode_double (x, &e, &m);
   return scale10_round_decimal_decoded (e, m, memory, n);
