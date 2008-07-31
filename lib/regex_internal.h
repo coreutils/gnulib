d5ea91965354489ef1eb271a05c4235f5b043590
Bruno Haible <bruno@clisp.org>
2008-07-31 12:04:14 +0200

    Make determination of BITSET_WORD_MAX work also with old HP-UX cc compilers.

--- lib/regex_internal.h
+++ lib/regex_internal.h
@@ -161,9 +161,9 @@ typedef unsigned long int bitset_word_t;
    instead, deduce it directly from BITSET_WORD_MAX.  Avoid
    greater-than-32-bit integers and unconditional shifts by more than
    31 bits, as they're not portable.  */
-#if BITSET_WORD_MAX == 0xffffffff
+#if BITSET_WORD_MAX == 0xffffffffUL
 # define BITSET_WORD_BITS 32
-#elif BITSET_WORD_MAX >> 31 >> 5 == 1
+#elif BITSET_WORD_MAX >> 31 >> 4 == 1
 # define BITSET_WORD_BITS 36
 #elif BITSET_WORD_MAX >> 31 >> 16 == 1
 # define BITSET_WORD_BITS 48
