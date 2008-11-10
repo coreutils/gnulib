594e0a34a8054efcfb572e4bb2a3323e98e4667c
Bruno Haible <bruno@clisp.org>
2008-11-10 12:46:10 +0100

    Add note about Haiku.

--- lib/fpurge.c
+++ lib/fpurge.c
@@ -19,7 +19,7 @@
 /* Specification.  */
 #include "fpurge.h"
 
-#if HAVE___FPURGE                   /* glibc >= 2.2, Solaris >= 7 */
+#if HAVE___FPURGE                   /* glibc >= 2.2, Haiku, Solaris >= 7 */
 # include <stdio_ext.h>
 #endif
 #include <stdlib.h>
@@ -29,7 +29,7 @@
 int
 fpurge (FILE *fp)
 {
-#if HAVE___FPURGE                   /* glibc >= 2.2, Solaris >= 7 */
+#if HAVE___FPURGE                   /* glibc >= 2.2, Haiku, Solaris >= 7 */
 
   __fpurge (fp);
   /* The __fpurge function does not have a return value.  */
