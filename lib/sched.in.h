4dd30a3ea2e00bbad5091af335962c4ebe4bfcfa
Bruno Haible <bruno@clisp.org>
2008-11-15 13:06:41 +0100

    Make the 'sched' module work on platforms where <sched.h> is incomplete.

--- lib/sched.in.h
+++ lib/sched.in.h
@@ -15,11 +15,33 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #ifndef _GL_SCHED_H
+
+#if __GNUC__ >= 3
+@PRAGMA_SYSTEM_HEADER@
+#endif
+
+/* The include_next requires a split double-inclusion guard.  */
+#if @HAVE_SCHED_H@
+# @INCLUDE_NEXT@ @NEXT_SCHED_H@
+#endif
+
+#ifndef _GL_SCHED_H
 #define _GL_SCHED_H
+
+#if !@HAVE_STRUCT_SCHED_PARAM@
 
 struct sched_param
 {
   int sched_priority;
 };
 
+#endif
+
+#if !(defined SCHED_FIFO && defined SCHED_RR && defined SCHED_OTHER)
+# define SCHED_FIFO   1
+# define SCHED_RR     2
+# define SCHED_OTHER  0
+#endif
+
+#endif /* _GL_SCHED_H */
 #endif /* _GL_SCHED_H */
