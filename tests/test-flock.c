c571b02a40485aa46e146a68d6d47eca71a4b477
Bruno Haible <bruno@clisp.org>
2008-11-15 13:33:04 +0100

    Include config.h in all tests.

--- tests/test-flock.c
+++ tests/test-flock.c
@@ -14,6 +14,9 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include <config.h>
+
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
--- tests/test-stpncpy.c
+++ tests/test-stpncpy.c
@@ -1,5 +1,5 @@
 /* Test the system defined function stpncpy().
-   Copyright (C) 2003 Free Software Foundation, Inc.
+   Copyright (C) 2003, 2008 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -13,6 +13,8 @@
 
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <config.h>
 
 #include <string.h>
 #include <stdio.h>
