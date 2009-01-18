6534ab3144e593447404c90df8a0d5676f344dd5
Bruno Haible <bruno@clisp.org>
2009-01-18 02:50:29 +0100

    Tests for module 'vdprintf-posix'.

---
+++ tests/test-vdprintf-posix.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+tmpfiles=""
+trap 'rm -fr $tmpfiles' 1 2 3 15
+
+tmpfiles="$tmpfiles t-vdprintf-posix.tmp t-vdprintf-posix.out"
+./test-vdprintf-posix${EXEEXT} > t-vdprintf-posix.tmp || exit 1
+LC_ALL=C tr -d '\r' < t-vdprintf-posix.tmp > t-vdprintf-posix.out || exit 1
+
+: ${DIFF=diff}
+${DIFF} "${srcdir}/test-printf-posix.output" t-vdprintf-posix.out
+result=$?
+
+rm -fr $tmpfiles
+
+exit $result
