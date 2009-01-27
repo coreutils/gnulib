9b38b4319e6b5c6743138e5afd34633837fecbab
Eric Blake <ebb9@byu.net>
2009-01-27 10:00:51 -0700

    version-etc: use consistent URL formatting
    * lib/version-etc.c (emit_bug_reporting_address, version_etc_va):
    Improve formatting.  Use fputs for string without %.
    
    Signed-off-by: Eric Blake <ebb9@byu.net>

--- lib/version-etc.c
+++ lib/version-etc.c
@@ -66,7 +66,7 @@ version_etc_va (FILE *stream,
 
   fputs (_("\
 \n\
-License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n\
 \n\
@@ -179,7 +179,8 @@ emit_bug_reporting_address (void)
      "Report translation bugs to <...>\n" with the address for translation
      bugs (typically your translation team's web or email address).  */
   printf (_("\nReport bugs to <%s>.\n"), PACKAGE_BUGREPORT);
-  printf (_("%s home page: http://www.gnu.org/software/%s/\n"),
+  printf (_("%s home page: <http://www.gnu.org/software/%s/>.\n"),
 	  PACKAGE_NAME, PACKAGE);
-  printf (_("General help using GNU software: http://www.gnu.org/gethelp/\n"));
+  fputs (_("General help using GNU software: <http://www.gnu.org/gethelp/>.\n"),
+         stdout);
 }
