1bd08626c24664e17e9c8d77a3084adf5a119f3b
Bruno Haible <bruno@clisp.org>
2009-03-07 11:26:01 +0100

    Tests for module 'uninorm/decomposing-form'.

---
+++ tests/uninorm/test-decomposing-form.c
@@ -0,0 +1,47 @@
+/* Test of decomposing variant of a normalization form.
+   Copyright (C) 2009 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2009.  */
+
+#include <config.h>
+
+#include "uninorm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ASSERT(expr) \
+  do									     \
+    {									     \
+      if (!(expr))							     \
+        {								     \
+          fprintf (stderr, "%s:%d: assertion failed\n", __FILE__, __LINE__); \
+          fflush (stderr);						     \
+          abort ();							     \
+        }								     \
+    }									     \
+  while (0)
+
+int
+main ()
+{
+  ASSERT (uninorm_decomposing_form (UNINORM_NFC) == UNINORM_NFD);
+  ASSERT (uninorm_decomposing_form (UNINORM_NFD) == UNINORM_NFD);
+  ASSERT (uninorm_decomposing_form (UNINORM_NFKC) == UNINORM_NFKD);
+  ASSERT (uninorm_decomposing_form (UNINORM_NFKD) == UNINORM_NFKD);
+
+  return 0;
+}
