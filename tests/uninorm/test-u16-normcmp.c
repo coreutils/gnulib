2010f906dccfe20a201277eb556b797d9a797b20
Bruno Haible <bruno@clisp.org>
2009-03-07 16:59:15 +0100

    More tests for module uninorm/u16-normcmp.

--- tests/uninorm/test-u16-normcmp.c
+++ tests/uninorm/test-u16-normcmp.c
@@ -36,43 +36,11 @@
     }									     \
   while (0)
 
-int
-main ()
+#include "test-u16-normcmp.h"
+
+static void
+test_nonascii (int (*my_normcmp) (const uint16_t *, size_t, const uint16_t *, size_t, uninorm_t, int *))
 {
-  /* Empty string.  */
-  {
-    int cmp;
-
-    ASSERT (u16_normcmp (NULL, 0, NULL, 0, UNINORM_NFD, &cmp) == 0);
-    ASSERT (cmp == 0);
-  }
-  {
-    static const uint16_t input[] = { 'x', 'y' };
-    int cmp;
-
-    ASSERT (u16_normcmp (input, SIZEOF (input), NULL, 0, UNINORM_NFD, &cmp) == 0);
-    ASSERT (cmp == 1);
-
-    ASSERT (u16_normcmp (NULL, 0, input, SIZEOF (input), UNINORM_NFD, &cmp) == 0);
-    ASSERT (cmp == -1);
-
-    ASSERT (u16_normcmp (input, SIZEOF (input), input, SIZEOF (input), UNINORM_NFD, &cmp) == 0);
-    ASSERT (cmp == 0);
-  }
-
-  /* Shorter and longer strings.  */
-  {
-    static const uint16_t input1[] = { 'R', 'e', 'a', 'g', 'a', 'n' };
-    static const uint16_t input2[] = { 'R', 'e', 'a', 'g', 'a', 'n', 'o', 'm', 'i', 'c', 's' };
-    int cmp;
-
-    ASSERT (u16_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
-    ASSERT (cmp == -1);
-
-    ASSERT (u16_normcmp (input2, SIZEOF (input2), input1, SIZEOF (input1), UNINORM_NFD, &cmp) == 0);
-    ASSERT (cmp == 1);
-  }
-
   /* Normalization effects.  */
   {
     static const uint16_t input1[] = { 'H', 0x00F6, 'h', 'l', 'e' };
@@ -82,30 +50,81 @@ main ()
     static const uint16_t input5[] = { 'H', 'u', 'r', 'z' };
     int cmp;
 
-    ASSERT (u16_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == 0);
 
-    ASSERT (u16_normcmp (input2, SIZEOF (input2), input1, SIZEOF (input1), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input2, SIZEOF (input2), input1, SIZEOF (input1), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == 0);
 
-    ASSERT (u16_normcmp (input3, SIZEOF (input3), input4, SIZEOF (input4), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input3, SIZEOF (input3), input4, SIZEOF (input4), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == 0);
 
-    ASSERT (u16_normcmp (input4, SIZEOF (input4), input3, SIZEOF (input3), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input4, SIZEOF (input4), input3, SIZEOF (input3), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == 0);
 
-    ASSERT (u16_normcmp (input2, SIZEOF (input2), input3, SIZEOF (input3), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input2, SIZEOF (input2), input3, SIZEOF (input3), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == -1);
 
-    ASSERT (u16_normcmp (input1, SIZEOF (input1), input4, SIZEOF (input4), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input4, SIZEOF (input4), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == -1);
 
-    ASSERT (u16_normcmp (input1, SIZEOF (input1), input5, SIZEOF (input5), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input5, SIZEOF (input5), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == -1);
 
-    ASSERT (u16_normcmp (input2, SIZEOF (input2), input5, SIZEOF (input5), UNINORM_NFD, &cmp) == 0);
+    ASSERT (my_normcmp (input2, SIZEOF (input2), input5, SIZEOF (input5), UNINORM_NFD, &cmp) == 0);
     ASSERT (cmp == -1);
   }
+  { /* LATIN CAPITAL LETTER A WITH DIAERESIS */
+    static const uint16_t input1[] = { 0x00C4 };
+    static const uint16_t input2[] = { 0x0041, 0x0308 };
+    int cmp;
+
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
+    ASSERT (cmp == 0);
+  }
+  { /* LATIN CAPITAL LETTER A WITH DIAERESIS AND MACRON */
+    static const uint16_t input1[] = { 0x01DE };
+    static const uint16_t input2[] = { 0x0041, 0x0308, 0x0304 };
+    int cmp;
+
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
+    ASSERT (cmp == 0);
+  }
+  { /* GREEK DIALYTIKA AND PERISPOMENI */
+    static const uint16_t input1[] = { 0x1FC1 };
+    static const uint16_t input2[] = { 0x00A8, 0x0342 };
+    int cmp;
+
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
+    ASSERT (cmp == 0);
+  }
+  { /* HANGUL SYLLABLE GEUL */
+    static const uint16_t input1[] = { 0xAE00 };
+    static const uint16_t input2[] = { 0xADF8, 0x11AF };
+    static const uint16_t input3[] = { 0x1100, 0x1173, 0x11AF };
+    int cmp;
+
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
+    ASSERT (cmp == 0);
+
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input3, SIZEOF (input3), UNINORM_NFD, &cmp) == 0);
+    ASSERT (cmp == 0);
+  }
+  { /* HANGUL SYLLABLE GEU */
+    static const uint16_t input1[] = { 0xADF8 };
+    static const uint16_t input2[] = { 0x1100, 0x1173 };
+    int cmp;
+
+    ASSERT (my_normcmp (input1, SIZEOF (input1), input2, SIZEOF (input2), UNINORM_NFD, &cmp) == 0);
+    ASSERT (cmp == 0);
+  }
+}
+
+int
+main ()
+{
+  test_ascii (u16_normcmp, UNINORM_NFD);
+  test_nonascii (u16_normcmp);
 
   return 0;
 }
