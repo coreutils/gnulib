f2ed33f70b0e45621225e7f88e17724636d43035
Bruno Haible <bruno@clisp.org>
2009-03-07 17:01:42 +0100

    Tests for module 'uninorm/u8-normcoll'.

---
+++ tests/uninorm/test-u8-normcoll.c
@@ -0,0 +1,49 @@
+/* Test of locale dependent, normalization insensitive comparison of
+   UTF-8 strings.
+   Copyright (C) 2009 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2009.  */
+
+#include <config.h>
+
+#include "uninorm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZEOF(array) (sizeof (array) / sizeof (array[0]))
+#define ASSERT(expr) \
+  do									     \
+    {									     \
+      if (!(expr))							     \
+        {								     \
+          fprintf (stderr, "%s:%d: assertion failed\n", __FILE__, __LINE__); \
+          fflush (stderr);						     \
+          abort ();							     \
+        }								     \
+    }									     \
+  while (0)
+
+#include "test-u8-normcmp.h"
+int
+main ()
+{
+  /* In the "C" locale, strcoll is equivalent to strcmp, therefore u8_normcoll
+     on ASCII strings should behave like strcmp as well.  */
+  test_ascii (u8_normcoll, UNINORM_NFC);
+
+  return 0;
+}
