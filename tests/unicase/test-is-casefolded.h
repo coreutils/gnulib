1cf34fafe06c511eb2cb328fe6d68a09e9c72ead
Bruno Haible <bruno@clisp.org>
2009-03-08 17:11:54 +0100

    Tests for module 'unicase/u8-is-casefolded'.

---
+++ tests/unicase/test-is-casefolded.h
@@ -0,0 +1,43 @@
+/* Test of test whether a Unicode string is already case-folded.
+   Copyright (C) 2009 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2009.  */
+
+static void
+test_ascii (int (*my_is) (const UNIT *, size_t, const char *, bool *))
+{
+  {
+    static const UNIT input[] = { 'J', 'O', 'H', 'N', ' ', 'S', 'M', 'I', 'T', 'H' };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == false);
+  }
+  {
+    static const UNIT input[] = { 'J', 'o', 'h', 'n', ' ', 'S', 'm', 'i', 't', 'h' };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == false);
+  }
+  {
+    static const UNIT input[] = { 'j', 'o', 'h', 'n', ' ', 's', 'm', 'i', 't', 'h' };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == true);
+  }
+}
---
+++ tests/unicase/test-u8-is-casefolded.c
@@ -0,0 +1,65 @@
+/* Test of test whether an UTF-8 string is already case-folded.
+   Copyright (C) 2009 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2009.  */
+
+#include <config.h>
+
+#include "unicase.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unistr.h"
+
+#define SIZEOF(array) (sizeof (array) / sizeof (array[0]))
+#define ASSERT(expr) \
+  do									     \
+    {									     \
+      if (!(expr))							     \
+        {								     \
+          fprintf (stderr, "%s:%d: assertion failed\n", __FILE__, __LINE__); \
+          fflush (stderr);						     \
+          abort ();							     \
+        }								     \
+    }									     \
+  while (0)
+
+#define UNIT uint8_t
+#include "test-is-casefolded.h"
+#undef UNIT
+
+static void
+test_nonascii (int (*my_is) (const uint8_t *, size_t, const char *, bool *))
+{
+  /* Test cases from Unicode 5.1.0.  */
+  {
+    static const uint8_t input[] = { 0xC3, 0x9F };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == false);
+  }
+}
+
+int
+main ()
+{
+  test_ascii (u8_is_casefolded);
+  test_nonascii (u8_is_casefolded);
+
+  return 0;
+}
