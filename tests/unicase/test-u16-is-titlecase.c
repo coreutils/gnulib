2b0991c2adbebba8d9ff03a956b6d4f65ee164d0
Bruno Haible <bruno@clisp.org>
2009-03-08 17:06:07 +0100

    Tests for module 'unicase/u16-is-titlecase'.

---
+++ tests/unicase/test-u16-is-titlecase.c
@@ -0,0 +1,51 @@
+/* Test of test whether an UTF-16 string is entirely title case.
+   Copyright (C) 2009 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2009.  */
+
+#include <config.h>
+
+#include "unicase.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unistr.h"
+
+#define SIZEOF(array) (sizeof (array) / sizeof (array[0]))
+#define ASSERT(expr) \
+  do									     \
+    {									     \
+      if (!(expr))							     \
+        {								     \
+          fprintf (stderr, "%s:%d: assertion failed\n", __FILE__, __LINE__); \
+          fflush (stderr);						     \
+          abort ();							     \
+        }								     \
+    }									     \
+  while (0)
+
+#define UNIT uint16_t
+#include "test-is-titlecase.h"
+#undef UNIT
+
+int
+main ()
+{
+  test_ascii (u16_is_titlecase);
+
+  return 0;
+}
