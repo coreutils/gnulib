4f9d80b9d8004f0fcedf3525e268abaa24b8b4a9
Bruno Haible <bruno@clisp.org>
2009-03-08 16:54:47 +0100

    Tests for module 'unicase/u8-is-uppercase'.

---
+++ tests/unicase/test-u8-is-uppercase.c
@@ -0,0 +1,93 @@
+/* Test of test whether an UTF-8 string is entirely upper case.
+   Copyright (C) 2009 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2009.  */
+
+#include <config.h>
+
+#include "unicase.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unistr.h"
+
+#define SIZEOF(array) (sizeof (array) / sizeof (array[0]))
+#define ASSERT(expr) \
+  do									     \
+    {									     \
+      if (!(expr))							     \
+        {								     \
+          fprintf (stderr, "%s:%d: assertion failed\n", __FILE__, __LINE__); \
+          fflush (stderr);						     \
+          abort ();							     \
+        }								     \
+    }									     \
+  while (0)
+
+#define UNIT uint8_t
+#include "test-is-uppercase.h"
+#undef UNIT
+
+static void
+test_nonascii (int (*my_is) (const uint8_t *, size_t, const char *, bool *))
+{
+  /* Test cases from Unicode 5.1.0.  */
+  {
+    static const uint8_t input[] = { 0xE2, 0x93, 0x97 };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == false);
+  }
+  {
+    static const uint8_t input[] = { 0xE2, 0x92, 0xBD };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == true);
+  }
+  {
+    static const uint8_t input[] = { 0xCA, 0xB0 };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == true);
+  }
+  {
+    static const uint8_t input[] = { 0xE1, 0x80, 0xB4 };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == true);
+  }
+  {
+    static const uint8_t input[] = { 0xCA, 0xBD };
+    bool result;
+
+    ASSERT (my_is (input, SIZEOF (input), NULL, &result) == 0);
+    ASSERT (result == true);
+  }
+}
+
+int
+main ()
+{
+  test_ascii (u8_is_uppercase);
+  test_nonascii (u8_is_uppercase);
+
+  return 0;
+}
