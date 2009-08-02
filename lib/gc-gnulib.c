003bfc2d35217d361cd4b799ada862296310888a
Simon Josefsson <simon@josefsson.org>
2009-08-02 22:23:56 +0200

    gc-libgcrypt.c, gc-gnulib.c: Change copyright to FSF.

--- lib/gc-gnulib.c
+++ lib/gc-gnulib.c
@@ -1,5 +1,6 @@
 /* gc-gnulib.c --- Common gnulib internal crypto interface functions
- * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008  Simon Josefsson
+ * Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009 Free
+ * Software Foundation, Inc.
  *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
