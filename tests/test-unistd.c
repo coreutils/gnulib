b8521e81e705f88784e7a6709bcd6c5a3ea440e0
Bruno Haible <bruno@clisp.org>
2009-08-11 21:06:43 +0200

    Avoid compilation error on NetBSD 5.0.

--- tests/test-unistd.c
+++ tests/test-unistd.c
@@ -24,7 +24,7 @@
 
 /* Check that NULL can be passed through varargs as a pointer type,
    per POSIX 2008.  */
-verify (sizeof NULL == sizeof (void *));
+verify (sizeof (NULL) == sizeof (void *));
 
 /* Check that the various SEEK_* macros are defined.  */
 int sk[] = { SEEK_CUR, SEEK_END, SEEK_SET };
