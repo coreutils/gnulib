3e2faf235348ba68f0fd19bc34d466cb1cfc1597
Jim Meyering <meyering@redhat.com>
2009-09-19 18:55:20 +0200

    test-posixtm.c: correct a comment
    * tests/test-posixtm.c: Correct first-line comment.
    Spotted by Eric Blake.

--- tests/test-posixtm.c
+++ tests/test-posixtm.c
@@ -1,4 +1,4 @@
-/* Test that openat_safer leave standard fds alone.
+/* Test that posixtime works as required.
    Copyright (C) 2009 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
