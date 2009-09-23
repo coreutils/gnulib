b5eb8386645bf1bcfafa63c729ff9a86454def87
Eric Blake <ebb9@byu.net>
2009-09-23 14:51:29 -0600

    same-inode: make SAME_INODE tri-state, to port to mingw
    Mingw has the annoying habit (already documented in
    doc/posix-functions/*stat) that st_ino is always 0.
    This means that naive uses of SAME_INODE(a,b) would
    succeed, even on distinct files.
    
    Here's an analysis of all gnulib modules that used
    the macro before this commit:
    
    chdir-safer is safe - SAME_INODE protected by HAVE_READLINK
    cycle-check - mingw has no dir hard links and no symlinks,
     so no directory cycles can occur, and we should ignore -1
    fts - SAME_INODE protected by FTS_DEBUG
    hash-triple - using -1 gives more hash collisions, but
     the results are still correct
    openat-proc - SAME_INODE protected by stat("/proc/self")
    same - no dir cycles, so files are only same with identical name
    link-follow.m4 - configure test already correct on mingw
    test-canonicalize* - test already passes on mingw
    test-[l]stat - test already passes on mingw
    
    * NEWS: Mention this change.
    * lib/same-inode.h (same-inode.h): Recognize mingw limitation of
    st_ino always being 0.
    * lib/cycle-check.h (CYCLE_CHECK_REFLECT_CHDIR_UP): Update caller.
    * lib/cycle-check.c (cycle_check): Likewise.
    * lib/same.c (same_name): Likewise.
    
    Signed-off-by: Eric Blake <ebb9@byu.net>

--- lib/same-inode.h
+++ lib/same-inode.h
@@ -1,6 +1,6 @@
 /* Determine whether two stat buffers refer to the same file.
 
-   Copyright (C) 2006 Free Software Foundation, Inc.
+   Copyright (C) 2006, 2009 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -18,8 +18,18 @@
 #ifndef SAME_INODE_H
 # define SAME_INODE_H 1
 
+/* Perform a tri-state query on whether STAT_BUF_1 and STAT_BUF_2
+   represent the same file.  Return 1 for equal, 0 for distinct, and
+   -1 for indeterminate (the latter is generally possible only on
+   mingw).  Algorithms that use this macro must be prepared to handle
+   the indeterminate case without wrong results.  For example, if an
+   optimization is possible if two files are the same but unsafe if
+   distinct, use SAME_INODE()==1; whereas for an optimization that is
+   possible only for distinct files, use !SAME_INODE().  */
+
 # define SAME_INODE(Stat_buf_1, Stat_buf_2) \
-   ((Stat_buf_1).st_ino == (Stat_buf_2).st_ino \
-    && (Stat_buf_1).st_dev == (Stat_buf_2).st_dev)
+   (((Stat_buf_1).st_ino == (Stat_buf_2).st_ino         \
+     && (Stat_buf_1).st_dev == (Stat_buf_2).st_dev)     \
+    ? 1 - 2 * !(Stat_buf_1).st_ino : 0)
 
 #endif
