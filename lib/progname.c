588b135db5d12a4fd7ded7fd40021445cbaadaaa
Bruno Haible <bruno@clisp.org>
2009-10-04 12:14:52 +0200

    Remove the "lt-" prefix from program_invocation_short_name.

--- lib/progname.c
+++ lib/progname.c
@@ -48,7 +48,14 @@ set_program_name (const char *argv0)
     {
       argv0 = base;
       if (strncmp (base, "lt-", 3) == 0)
-	argv0 = base + 3;
+	{
+	  argv0 = base + 3;
+	  /* On glibc systems, remove the "lt-" prefix from the variable
+	     program_invocation_short_name.  */
+#if HAVE_DECL_PROGRAM_INVOCATION_SHORT_NAME
+	  program_invocation_short_name = (char *) argv0;
+#endif
+	}
     }
 
   /* But don't strip off a leading <dirname>/ in general, because when the user
