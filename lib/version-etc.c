9faf3d43bdbdab2aa0df10f3470208f260d51dcd
Eric Blake <ebb9@byu.net>
2009-11-09 15:18:13 -0700

    version-etc: match standards.texi style
    This assumes that PACKAGE_BUGREPORTS and PACKAGE_PACKAGER_BUG_REPORTS
    are generally email addresses, not URLs.
    
    Coreutils switched to a similar style on 2009-02-01, with no complaints
    (the difference is that coreutils inserts the program name, derived
    from argv[0], into the bug reporting line).  Around that time,
    standards.texi was also updated to mention this style.
    
    * lib/version-etc.c (emit_bug_reporting_address): Drop periods,
    and use <> only for URLs.
    
    Signed-off-by: Eric Blake <ebb9@byu.net>

--- lib/version-etc.c
+++ lib/version-etc.c
@@ -236,13 +236,17 @@ emit_bug_reporting_address (void)
      for this package.  Please add _another line_ saying
      "Report translation bugs to <...>\n" with the address for translation
      bugs (typically your translation team's web or email address).  */
-  printf (_("\nReport bugs to <%s>.\n"), PACKAGE_BUGREPORT);
+  printf (_("\nReport bugs to: %s\n"), PACKAGE_BUGREPORT);
 #ifdef PACKAGE_PACKAGER_BUG_REPORTS
-  printf (_("Report %s bugs to <%s>.\n"), PACKAGE_PACKAGER,
+  printf (_("Report %s bugs to: %s\n"), PACKAGE_PACKAGER,
 	  PACKAGE_PACKAGER_BUG_REPORTS);
 #endif
-  printf (_("%s home page: <http://www.gnu.org/software/%s/>.\n"),
+#ifdef PACKAGE_URL
+  printf (_("%s home page: <%s>\n"), PACKAGE_NAME, PACKAGE_URL);
+#else
+  printf (_("%s home page: <http://www.gnu.org/software/%s/>\n"),
 	  PACKAGE_NAME, PACKAGE);
-  fputs (_("General help using GNU software: <http://www.gnu.org/gethelp/>.\n"),
+#endif
+  fputs (_("General help using GNU software: <http://www.gnu.org/gethelp/>\n"),
 	 stdout);
 }
