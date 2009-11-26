dfd793fab03429ee3510958eee1ed33de282fbcc
Paolo Bonzini <bonzini@gnu.org>
2009-11-26 20:29:16 +0100

    w32sock: leave win32 error in place.
    * lib/w32sock.h (set_winsock_errno): Do not call WSASetLastError.

--- lib/w32sock.h
+++ lib/w32sock.h
@@ -32,7 +32,6 @@ static inline void
 set_winsock_errno (void)
 {
   int err = WSAGetLastError ();
-  WSASetLastError (0);
 
   /* Map some WSAE* errors to the runtime library's error codes.  */
   switch (err)
