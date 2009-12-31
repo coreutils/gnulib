00b8e3f73a4370d64a6d8ec20c28b13f2d856bfb
Eric Blake <ebb9@byu.net>
2009-12-31 13:43:28 -0700

    test-dup2: avoid compiler warning
    A warning cropped up from the 2009-12-28 change, when not using cloexec.
    
    * tests/test-dup2.c (is_inheritable): Only define if used.
    
    Signed-off-by: Eric Blake <ebb9@byu.net>

--- tests/test-dup2.c
+++ tests/test-dup2.c
@@ -57,11 +57,12 @@ is_open (int fd)
 #endif
 }
 
+#if GNULIB_CLOEXEC
 /* Return non-zero if FD is open and inheritable across exec/spawn.  */
 static int
 is_inheritable (int fd)
 {
-#if (defined _WIN32 || defined __WIN32__) && ! defined __CYGWIN__
+# if (defined _WIN32 || defined __WIN32__) && ! defined __CYGWIN__
   /* On Win32, the initial state of unassigned standard file
      descriptors is that they are open but point to an
      INVALID_HANDLE_VALUE, and there is no fcntl.  */
@@ -70,14 +71,15 @@ is_inheritable (int fd)
   if (h == INVALID_HANDLE_VALUE || GetHandleInformation (h, &flags) == 0)
     return 0;
   return (flags & HANDLE_FLAG_INHERIT) != 0;
-#else
-# ifndef F_GETFD
-#  error Please port fcntl to your platform
-# endif
+# else
+#  ifndef F_GETFD
+#   error Please port fcntl to your platform
+#  endif
   int i = fcntl (fd, F_GETFD);
   return 0 <= i && (i & FD_CLOEXEC) == 0;
-#endif
+# endif
 }
+#endif /* GNULIB_CLOEXEC */
 
 #if !O_BINARY
 # define setmode(f,m) zero ()
