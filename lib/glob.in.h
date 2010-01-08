0906b5f3d776d1a91b4c1f2c244cb01b2c6aabcd
John W. Eaton <jwe@octave.org>
2010-01-08 10:11:07 +0100

    glob: Fix C++ compilation.

--- lib/glob.in.h
+++ lib/glob.in.h
@@ -33,8 +33,13 @@
 #include <sys/stat.h>
 
 #ifndef __BEGIN_DECLS
-# define __BEGIN_DECLS
-# define __END_DECLS
+# ifdef __cplusplus
+#  define __BEGIN_DECLS  extern "C" {
+#  define __END_DECLS    }
+# else
+#  define __BEGIN_DECLS
+#  define __END_DECLS
+# endif
 #endif
 #ifndef __THROW
 # define __THROW
