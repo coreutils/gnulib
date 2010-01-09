5bc24f87fcd186b9e02d946ce9dc1319a465c336
Bruno Haible <bruno@clisp.org>
2010-01-09 17:34:29 +0100

    Add more tests.

--- tests/unictype/test-bidi_byname.c
+++ tests/unictype/test-bidi_byname.c
@@ -1,5 +1,5 @@
 /* Test the Unicode character type functions.
-   Copyright (C) 2007-2009 Free Software Foundation, Inc.
+   Copyright (C) 2007-2010 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -25,8 +25,27 @@
 int
 main ()
 {
+  ASSERT (uc_bidi_category_byname ("L") == UC_BIDI_L);
+  ASSERT (uc_bidi_category_byname ("LRE") == UC_BIDI_LRE);
+  ASSERT (uc_bidi_category_byname ("LRO") == UC_BIDI_LRO);
+  ASSERT (uc_bidi_category_byname ("R") == UC_BIDI_R);
+  ASSERT (uc_bidi_category_byname ("AL") == UC_BIDI_AL);
+  ASSERT (uc_bidi_category_byname ("RLE") == UC_BIDI_RLE);
+  ASSERT (uc_bidi_category_byname ("RLO") == UC_BIDI_RLO);
+  ASSERT (uc_bidi_category_byname ("PDF") == UC_BIDI_PDF);
+  ASSERT (uc_bidi_category_byname ("EN") == UC_BIDI_EN);
+  ASSERT (uc_bidi_category_byname ("ES") == UC_BIDI_ES);
+  ASSERT (uc_bidi_category_byname ("ET") == UC_BIDI_ET);
+  ASSERT (uc_bidi_category_byname ("AN") == UC_BIDI_AN);
+  ASSERT (uc_bidi_category_byname ("CS") == UC_BIDI_CS);
   ASSERT (uc_bidi_category_byname ("NSM") == UC_BIDI_NSM);
+  ASSERT (uc_bidi_category_byname ("BN") == UC_BIDI_BN);
+  ASSERT (uc_bidi_category_byname ("B") == UC_BIDI_B);
+  ASSERT (uc_bidi_category_byname ("S") == UC_BIDI_S);
+  ASSERT (uc_bidi_category_byname ("WS") == UC_BIDI_WS);
+  ASSERT (uc_bidi_category_byname ("ON") == UC_BIDI_ON);
   ASSERT (uc_bidi_category_byname ("X") < 0);
+  ASSERT (uc_bidi_category_byname ("") < 0);
 
   return 0;
 }
