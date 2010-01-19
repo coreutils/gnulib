30cd07fada4e1c27ca3da633531df9d45066b04a
Bruno Haible <bruno@clisp.org>
2010-01-19 03:02:25 +0100

    Tests for module 'asinl'.

---
+++ tests/test-asinl.c
@@ -0,0 +1,43 @@
+/* Test of asinl() function.
+   Copyright (C) 2010 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (asinl, long double, (long double));
+
+#include "fpucw.h"
+#include "macros.h"
+
+long double y;
+
+int
+main ()
+{
+  DECL_LONG_DOUBLE_ROUNDING
+
+  BEGIN_LONG_DOUBLE_ROUNDING ();
+
+  /* A particular value.  */
+  y = asinl (0.6L);
+  ASSERT (y >= 0.6435011087L && y <= 0.6435011088L);
+
+  return 0;
+}
