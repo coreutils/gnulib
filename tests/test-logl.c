6b91d7e06d63c3d4a34db6c898d133812169a8ff
Bruno Haible <bruno@clisp.org>
2010-01-19 03:04:45 +0100

    Tests for module 'logl'.

---
+++ tests/test-logl.c
@@ -0,0 +1,43 @@
+/* Test of logl() function.
+   Copyright (C) 2010 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (logl, long double, (long double));
+
+#include "fpucw.h"
+#include "macros.h"
+
+long double y;
+
+int
+main ()
+{
+  DECL_LONG_DOUBLE_ROUNDING
+
+  BEGIN_LONG_DOUBLE_ROUNDING ();
+
+  /* A particular value.  */
+  y = logl (0.6L);
+  ASSERT (y >= -0.5108256238L && y <= -0.5108256237L);
+
+  return 0;
+}
