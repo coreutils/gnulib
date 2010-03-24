1f87c50a60309f7ae9aafbc7d614f2760f7ded99
Eric Blake <eblake@redhat.com>
2010-03-24 17:25:49 -0600

    arpa/inet: fix mingw compilation warning
    Mingw doesn't have <arpa/inet.h>, so a conditional use of the
    pragma was never encountered, and led to mingw's gcc issuing
    warnings about duplicate declarations under -Wredundant-decls.
    Making the #pragma unconditional (as was already done in at
    least fcntl.in.h) shuts up gcc.
    
    * lib/arpa_inet.in.h (system_header): Hoist to be unconditional.
    Reported by Matthew Bolte.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- lib/arpa_inet.in.h
+++ lib/arpa_inet.in.h
@@ -16,6 +16,10 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.  */
 
+# if __GNUC__ >= 3
+@PRAGMA_SYSTEM_HEADER@
+# endif
+
 #ifndef _GL_ARPA_INET_H
 
 /* Gnulib's sys/socket.h is responsible for pulling in winsock2.h etc
@@ -26,10 +30,6 @@
 #endif
 
 #if @HAVE_ARPA_INET_H@
-
-# if __GNUC__ >= 3
-@PRAGMA_SYSTEM_HEADER@
-# endif
 
 /* The include_next requires a split double-inclusion guard.  */
 # @INCLUDE_NEXT@ @NEXT_ARPA_INET_H@
