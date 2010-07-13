5432287a033855c8c7a36f3aa055e0db60fb3383
Bruno Haible <bruno@clisp.org>
2010-07-13 23:43:14 +0200

    striconveh: Simplify last commit.

--- lib/striconveh.c
+++ lib/striconveh.c
@@ -972,26 +972,21 @@ mem_cd_iconveh_internal (const char *src, size_t srclen,
       size_t memsize = length + extra_alloc;
 
       if (*resultp != NULL && *lengthp >= memsize)
-        {
-          result = *resultp;
-          memcpy (result, tmpbuf, length);
-        }
+        result = *resultp;
       else
         {
           char *memory;
 
           memory = (char *) malloc (memsize > 0 ? memsize : 1);
           if (memory != NULL)
-            {
-              memcpy (memory, tmpbuf, length);
-              result = memory;
-            }
+            result = memory;
           else
             {
               errno = ENOMEM;
               return -1;
             }
         }
+      memcpy (result, tmpbuf, length);
     }
   else if (result != *resultp && length + extra_alloc < allocated)
     {
