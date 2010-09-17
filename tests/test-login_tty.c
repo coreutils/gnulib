b7c95ee2f21751f24fd6245a8545cc8b3c555e60
Bruno Haible <bruno@clisp.org>
2010-09-17 04:00:49 +0200

    login_tty: Stricter unit test.
    * modules/login_tty-tests (Depends-on): Add tcgetsid.
    * tests/test-login_tty.c (main): Also check the results of tcgetpgrp()
    and tcgetsid() after login_tty.
    Reported by Mats Erik Andersson <mats.andersson@gisladisker.se>.

--- tests/test-login_tty.c
+++ tests/test-login_tty.c
@@ -19,8 +19,12 @@
 /* Specification.  */
 extern int login_tty (int);
 
+#include <errno.h>
 #include <pty.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
 int
 main ()
@@ -45,6 +49,24 @@ main ()
       {
         fprintf (stderr, "login_tty failed\n");
         return 1;
+      }
+  }
+
+  /* From here on, we cannot use stderr for error messages any more.
+     If a test fails, just abort.  */
+
+  /* Check that fd = 0, 1, 2 are now open to the controlling terminal for the
+     current process and that it is a session of its own.  */
+  {
+    int fd;
+    for (fd = 0; fd < 3; fd++)
+      if (!(tcgetpgrp (fd) == getpid ()))
+        abort ();
+    for (fd = 0; fd < 3; fd++)
+      {
+        int sid = tcgetsid (fd);
+        if (!(sid == -1 ? errno == ENOSYS : sid == getpid ()))
+          abort ();
       }
   }
 
