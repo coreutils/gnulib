89c936487278c4567c724b3ec826873f46f72c5a
Bruno Haible <bruno@clisp.org>
2010-09-29 01:56:23 +0200

    poll: Assume ANSI C.
    * lib/poll.c (poll): Use an ANSI C declaration.

--- lib/poll.c
+++ lib/poll.c
@@ -315,10 +315,7 @@ compute_revents (int fd, int sought, fd_set *rfds, fd_set *wfds, fd_set *efds)
 #endif /* !MinGW */
 
 int
-poll (pfd, nfd, timeout)
-     struct pollfd *pfd;
-     nfds_t nfd;
-     int timeout;
+poll (struct pollfd *pfd, nfds_t nfd, int timeout)
 {
 #ifndef WIN32_NATIVE
   fd_set rfds, wfds, efds;
