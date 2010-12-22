9ca8092d849ed03418229af9a2aefdf784fd4db9
Bruno Haible <bruno@clisp.org>
2010-12-22 16:05:29 +0100

    floor: Implement result sign according to IEEE 754.
    * lib/floor.c (FUNC): Return +0.0 for 0 < x < 1.
    * tests/test-floor2.c (floor_reference): Keep in sync with lib/floor.c.
    * tests/test-floorf2.c (floorf_reference): Likewise.
    * tests/test-floorf-ieee.c (main): Test also values between -1 and 1.
    * tests/test-floor-ieee.c (main): Likewise.
    * tests/test-floorl-ieee.c (main): Likewise.

--- tests/test-floorf-ieee.c
+++ tests/test-floorf-ieee.c
@@ -37,9 +37,20 @@ main (int argc, char **argv _GL_UNUSED)
 {
   float (*my_floorf) (float) = argc ? floorf : dummy;
 
+  /* See IEEE 754, section 6.3:
+       "the sign of the result of the round floating-point number to
+        integral value operation is the sign of the operand. These rules
+        shall apply even when operands or results are zero or infinite."  */
+
   /* Zero.  */
   ASSERT (!signbit (my_floorf (0.0f)));
   ASSERT (!!signbit (my_floorf (minus_zerof)) == !!signbit (minus_zerof));
+  /* Positive numbers.  */
+  ASSERT (!signbit (my_floorf (0.3f)));
+  ASSERT (!signbit (my_floorf (0.7f)));
+  /* Negative numbers.  */
+  ASSERT (!!signbit (my_floorf (-0.3f)) == !!signbit (minus_zerof));
+  ASSERT (!!signbit (my_floorf (-0.7f)) == !!signbit (minus_zerof));
 
   return 0;
 }
--- tests/test-floorf2.c
+++ tests/test-floorf2.c
@@ -63,8 +63,12 @@ floorf_reference (DOUBLE x)
 
   if (z > L_(0.0))
     {
+      /* For 0 < x < 1, return +0.0 even if the current rounding mode is
+         FE_DOWNWARD.  */
+      if (z < L_(1.0))
+        z = L_(0.0);
       /* Avoid rounding errors for values near 2^k, where k >= MANT_DIG-1.  */
-      if (z < TWO_MANT_DIG)
+      else if (z < TWO_MANT_DIG)
         {
           /* Round to the next integer (nearest or up or down, doesn't matter).  */
           z += TWO_MANT_DIG;
