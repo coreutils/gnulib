6147723cd9000c4e840205fec3e64b0a5a5c1d04
Paul Eggert <eggert@cs.ucla.edu>
2010-12-24 00:06:16 -0800

    tests: port test-fdutimensat.c to Solaris 8
    * tests/test-fdutimensat.c (do_fdutimens): Don't assume
    fdutimensat works with a nonnegative fd and AT_SYMLINK_NOFOLLOW.
    On Solaris 8, it fails with errno == ENOSYS, because there is no
    futimens (so it can't use the fd), and there is no lutimens (so it
    can't implement AT_SYMLINK_NOFOLLOW on symlinks).

--- tests/test-fdutimensat.c
+++ tests/test-fdutimensat.c
@@ -48,12 +48,17 @@ static int
 do_fdutimens (char const *name, struct timespec const times[2])
 {
   int result;
+  int nofollow_result;
+  int nofollow_errno;
   int fd = openat (dfd, name, O_WRONLY);
   if (fd < 0)
     fd = openat (dfd, name, O_RDONLY);
   errno = 0;
+  nofollow_result = fdutimensat (fd, dfd, name, times, AT_SYMLINK_NOFOLLOW);
+  nofollow_errno = errno;
   result = fdutimensat (fd, dfd, name, times, 0);
-  ASSERT (fdutimensat (fd, dfd, name, times, AT_SYMLINK_NOFOLLOW) == result);
+  ASSERT (result == nofollow_result
+          || (nofollow_result == -1 && nofollow_errno == ENOSYS));
   if (0 <= fd)
     {
       int saved_errno = errno;
