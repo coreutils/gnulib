3e44a8369ff68484ca037028deecb287a49f9d4d
Bruno Haible <bruno@clisp.org>
2011-01-18 14:13:43 +0100

    Tests for module 'get-rusage-as'.
    * modules/get-rusage-as-tests: New file.
    * tests/test-get-rusage-as.c: New file.

---
+++ tests/test-get-rusage-as.c
@@ -0,0 +1,60 @@
+/* Test of getter for RLIMIT_AS.
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+#include "resource-ext.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "macros.h"
+
+int
+main ()
+{
+  uintptr_t value1, value2, value3;
+
+  value1 = get_rusage_as ();
+
+  malloc (0x88);
+
+  value2 = get_rusage_as ();
+
+  malloc (0x281237);
+
+  value3 = get_rusage_as ();
+
+  if (value1 == 0 && value2 == 0 && value3 == 0)
+    {
+      fprintf (stderr, "Skipping test: no way to determine address space size\n");
+      return 77;
+    }
+  else
+    {
+      /* The address space size is positive.  */
+      ASSERT (value1 > 0);
+
+      /* Allocating memory should not decrease the address space size.  */
+      ASSERT (value2 >= value1);
+      ASSERT (value3 >= value2);
+
+      /* Allocating 2.5 MB of memory should increase the address space size.  */
+      ASSERT (value3 > value1);
+
+      return 0;
+    }
+}
