fbfced515f325dd000c80fa35ade86e567d0ea3c
Bruno Haible <bruno@clisp.org>
2011-03-08 10:09:47 +0100

    regex-quote: New API.
    * lib/regex-quote.h: Include <stdbool.h>.
    (struct regex_quote_spec): New type.
    (regex_quote_spec_posix, regex_quote_spec_gnu, regex_quote_spec_pcre):
    New declarations.
    (regex_quote_length, regex_quote_copy, regex_quote): Take a
    'const struct regex_quote_spec *' argument.
    * lib/regex-quote.c (RE_*, PCRE_*): New macros.
    (pcre_special): New constant.
    (regex_quote_spec_posix, regex_quote_spec_gnu, regex_quote_spec_pcre):
    New functions.
    (regex_quote_length, regex_quote_copy, regex_quote): Take a
    'const struct regex_quote_spec *' argument.
    * modules/regex-quote (Depends-on): Add stdbool.
    * tests/test-regex-quote.c (check): Update for new API. Add test for
    anchored results.
    * NEWS: Mention the API change.
    Reported by Reuben Thomas and Eric Blake.

--- lib/regex-quote.h
+++ lib/regex-quote.h
@@ -15,27 +15,74 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
-#include <stddef.h>
+#ifndef _REGEX_QUOTE_H
+#define _REGEX_QUOTE_H
 
-/* regex_quote converts a literal string to a regular expression that will
-   look for this literal string.
-   cflags can be 0 or REG_EXTENDED.
+#include <stddef.h>
+#include <stdbool.h>
+
+
+/* Specifies a quotation task for converting a fixed string to a regular
+   expression pattern.  */
+struct regex_quote_spec
+{
+  /* True if the regular expression pattern consists of multibyte characters
+     (in the encoding given by the LC_CTYPE category of the locale),
+     false if it consists of single bytes or UTF-8 characters.  */
+  unsigned int /*bool*/ multibyte : 1;
+  /* True if the regular expression pattern shall match only entire lines.  */
+  unsigned int /*bool*/ anchored : 1;
+  /* Set of characters that need to be escaped (all ASCII), as a
+     NUL-terminated string.  */
+  char special[30 + 1];
+};
+
+
+/* Creates a quotation task that produces a POSIX regular expression, that is,
+   a pattern that can be compiled with regcomp().
+   CFLAGS can be 0 or REG_EXTENDED.
    If it is 0, the result is a Basic Regular Expression (BRE)
    <http://www.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap09.html#tag_09_03>.
    If it is REG_EXTENDED, the result is an Extended Regular Expression (ERE)
    <http://www.opengroup.org/onlinepubs/9699919799/basedefs/V1_chap09.html#tag_09_04>.
-   The result is not anchored; if you want it to match only complete lines,
-   you need to add "^" at the beginning of the result and "$" at the end of the
-   result.
- */
+   If ANCHORED is false, the regular expression will match substrings of lines.
+   If ANCHORED is true, it will match only complete lines,  */
+extern struct regex_quote_spec
+       regex_quote_spec_posix (int cflags, bool anchored);
+
+/* Creates a quotation task that produces a regular expression that can be
+   compiled with the GNU API function re_compile_pattern().
+   SYNTAX describes the syntax of the regular expression (such as
+   RE_SYNTAX_POSIX_BASIC, RE_SYNTAX_POSIX_EXTENDED, RE_SYNTAX_EMACS, all
+   defined in <regex.h>).  It must be the same value as 're_syntax_options'
+   at the moment of the re_compile_pattern() call.
+   If ANCHORED is false, the regular expression will match substrings of lines.
+   If ANCHORED is true, it will match only complete lines,  */
+extern struct regex_quote_spec
+       regex_quote_spec_gnu (unsigned long /*reg_syntax_t*/ syntax, bool anchored);
+
+/* Creates a quotation task that produces a PCRE regular expression, that is,
+   a pattern that can be compiled with pcre_compile().
+   OPTIONS is the same value as the second argument passed to pcre_compile().
+   If ANCHORED is false, the regular expression will match substrings of lines.
+   If ANCHORED is true, it will match only complete lines,  */
+extern struct regex_quote_spec
+       regex_quote_spec_pcre (int options, bool anchored);
+
 
 /* Returns the number of bytes needed for the quoted string.  */
-extern size_t regex_quote_length (const char *string, int cflags);
+extern size_t
+       regex_quote_length (const char *string, const struct regex_quote_spec *spec);
 
 /* Copies the quoted string to p and returns the incremented p.
-   There must be room for regex_quote_length (string, cflags) + 1 bytes at p.
- */
-extern char * regex_quote_copy (char *p, const char *string, int cflags);
+   There must be room for regex_quote_length (string, spec) + 1 bytes at p.  */
+extern char *
+       regex_quote_copy (char *p,
+                         const char *string, const struct regex_quote_spec *spec);
 
 /* Returns the freshly allocated quoted string.  */
-extern char * regex_quote (const char *string, int cflags);
+extern char *
+       regex_quote (const char *string, const struct regex_quote_spec *spec);
+
+
+#endif /* _REGEX_QUOTE_H */
