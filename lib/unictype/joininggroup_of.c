9ca4149056bfb051e7f567071d01c376c36ef001
Bruno Haible <bruno@clisp.org>
2011-03-21 23:12:53 +0100

    New module 'unictype/joininggroup-of'.
    * modules/unictype/joininggroup-of: New file.
    * lib/unictype/joininggroup_of.c: New file.
    * lib/unictype/joininggroup_of.h: New file, automatically generated by
    gen-uni-tables.

---
+++ lib/unictype/joininggroup_of.c
@@ -0,0 +1,33 @@
+/* Arabic joining group of Unicode characters.
+   Copyright (C) 2011 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2011.
+
+   This program is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published
+   by the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "unictype.h"
+
+#include "unictype/joininggroup_of.h"
+
+int
+uc_joining_group (ucs4_t uc)
+{
+  if (uc >= joining_group_header_0
+      && uc < joining_group_header_0
+              + sizeof (u_joining_group) / sizeof (u_joining_group[0]))
+    return u_joining_group[uc - joining_group_header_0];
+  return UC_JOINING_GROUP_NONE;
+}
