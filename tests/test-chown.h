5e29a7a095a6c960115cf48f5b28278194a0b2ca
Jim Meyering <meyering@redhat.com>
2011-04-10 09:30:22 +0200

    test-chown.h: correct a cast
    * tests/test-chown.h (test_chown): Cast -1 to gid_t (not uid_t)
    when the destination is a stat.st_gid.

--- tests/test-chown.h
+++ tests/test-chown.h
@@ -71,7 +71,7 @@ test_chown (int (*func) (char const *, uid_t, gid_t), bool print)
   ASSERT (close (creat (BASE "dir/file", 0600)) == 0);
   ASSERT (stat (BASE "dir/file", &st1) == 0);
   ASSERT (st1.st_uid != (uid_t) -1);
-  ASSERT (st1.st_gid != (uid_t) -1);
+  ASSERT (st1.st_gid != (gid_t) -1);
   ASSERT (st1.st_gid == getegid ());
 
   /* Sanity check of error cases.  */
