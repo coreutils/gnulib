95bbafdb715dcfdbe7f629e2411b300ccaa84db4
Bruno Haible <bruno@clisp.org>
2011-04-30 13:38:43 +0200

    sys_socket: Ensure 'struct iovec' definition.
    * lib/sys_socket.in.h: Include <sys/uio.h> also on platforms that have
    <sys/socket.h>.
    * doc/posix-headers/sys_socket.texi: Mention the OpenBSD problem.

--- lib/sys_socket.in.h
+++ lib/sys_socket.in.h
@@ -108,6 +108,12 @@ struct sockaddr_storage
 
 #endif
 
+/* Get struct iovec.  */
+/* But avoid namespace pollution on glibc systems.  */
+#if ! defined __GLIBC__
+# include <sys/uio.h>
+#endif
+
 #if @HAVE_SYS_SOCKET_H@
 
 /* A platform that has <sys/socket.h>.  */
@@ -175,9 +181,6 @@ typedef int socklen_t;
 #  endif
 
 # endif
-
-/* For struct iovec */
-# include <sys/uio.h>
 
 /* Rudimentary 'struct msghdr'; this works as long as you don't try to
    access msg_control or msg_controllen.  */
