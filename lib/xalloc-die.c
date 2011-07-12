d4dd1df2d3794089cf6a85982333218d8e2393de
Paul Eggert <eggert@cs.ucla.edu>
2011-07-12 01:08:27 -0700

    * lib/xalloc-die.c (xalloc_die): Modernize comment.

--- lib/xalloc-die.c
+++ lib/xalloc-die.c
@@ -33,7 +33,7 @@ xalloc_die (void)
 {
   error (exit_failure, 0, "%s", _("memory exhausted"));
 
-  /* The `noreturn' cannot be given to error, since it may return if
+  /* _Noreturn cannot be given to error, since it may return if
      its first argument is 0.  To help compilers understand the
      xalloc_die does not return, call abort.  Also, the abort is a
      safety feature if exit_failure is 0 (which shouldn't happen).  */
