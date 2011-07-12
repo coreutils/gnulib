5833dfd948db783dcfd90e1273104b778e2ec773
Jim Meyering <meyering@redhat.com>
2011-07-12 18:55:44 +0200

    maint.mk: prohibit inclusion of "verify.h" without use
    * top/maint.mk (sc_prohibit_verify_without_use): New rule.

--- top/maint.mk
+++ top/maint.mk
@@ -621,6 +621,12 @@ sc_prohibit_stddef_without_use:
 	re='\<($(_stddef_syms_re)) *\('					\
 	  $(_sc_header_without_use)
 
+# Prohibit the inclusion of verify.h without an actual use.
+sc_prohibit_verify_without_use:
+	@h='verify.h'							\
+	re='\<(verify(true|expr)?|static_assert) *\('			\
+	  $(_sc_header_without_use)
+
 # Don't include xfreopen.h unless you use one of its functions.
 sc_prohibit_xfreopen_without_use:
 	@h='xfreopen.h' re='\<xfreopen *\(' $(_sc_header_without_use)
