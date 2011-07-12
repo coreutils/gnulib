6bddfff3ba873f16c5b9840c3ddf265408b9aef4
Paul Eggert <eggert@cs.ucla.edu>
2011-07-12 01:09:30 -0700

    openat: use stdnoreturn.h
    * lib/openat.h: Include <stdnoreturn.h>.
    (_GL_ATTRIBUTE_NORETURN): Remove; all uses replaced with noreturn.
    * modules/openat (Depends-on): Add stdnoreturn.

--- lib/openat.h
+++ lib/openat.h
@@ -26,12 +26,7 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <stdbool.h>
-
-#if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 8)
-# define _GL_ATTRIBUTE_NORETURN __attribute__ ((__noreturn__))
-#else
-# define _GL_ATTRIBUTE_NORETURN /* empty */
-#endif
+#include <stdnoreturn.h>
 
 #if !HAVE_OPENAT
 
@@ -47,8 +42,8 @@ bool openat_needs_fchdir (void);
 
 #endif
 
-void openat_restore_fail (int) _GL_ATTRIBUTE_NORETURN;
-void openat_save_fail (int) _GL_ATTRIBUTE_NORETURN;
+noreturn void openat_restore_fail (int);
+noreturn void openat_save_fail (int);
 
 /* Using these function names makes application code
    slightly more readable than it would be with
