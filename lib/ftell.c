f0e396a8e31e3a489a8ed8eafb4581ff168a0392
Paul Eggert <eggert@cs.ucla.edu>
2011-07-25 09:21:22 -0700

    * lib/ftell.c (ftell): Comment out cast.

--- lib/ftell.c
+++ lib/ftell.c
@@ -28,7 +28,7 @@ ftell (FILE *fp)
   /* Use the replacement ftello function with all its workarounds.  */
   off_t offset = ftello (fp);
   if (LONG_MIN <= offset && offset <= LONG_MAX)
-    return (long)offset;
+    return /* (long) */ offset;
   else
     {
       errno = EOVERFLOW;
