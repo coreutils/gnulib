21c918ecedb2e44a857c1ff2fe2e4964a2f67312
Paul Eggert <eggert@cs.ucla.edu>
2011-09-18 21:46:34 -0700

    stat: use pathmax.h only if needed
    * lib/stat.c: Include pathmax.h only if REPLACE_FUNC_STAT_DIR.
    This is better for Emacs, which does not have a mingw port and
    therefore can avoid the pathmax module.

--- lib/stat.c
+++ lib/stat.c
@@ -44,8 +44,14 @@ orig_stat (const char *filename, struct stat *buf)
 #include <stdbool.h>
 #include <string.h>
 #include "dosname.h"
-#include "pathmax.h"
 #include "verify.h"
+
+#if REPLACE_FUNC_STAT_DIR
+# include "pathmax.h"
+# ifndef PATH_MAX
+#  error "Please port this replacement to your platform"
+# endif
+#endif
 
 /* Store information about NAME into ST.  Work around bugs with
    trailing slashes.  Mingw has other bugs (such as st_ino always
@@ -73,9 +79,6 @@ rpl_stat (char const *name, struct stat *st)
 #if REPLACE_FUNC_STAT_DIR
   /* The only known systems where REPLACE_FUNC_STAT_DIR is needed also
      have a constant PATH_MAX.  */
-# ifndef PATH_MAX
-#  error "Please port this replacement to your platform"
-# endif
 
   if (result == -1 && errno == ENOENT)
     {
