e430506b381d396c123aaef691744cf8bda4368b
Bruno Haible <bruno@clisp.org>
2011-09-20 23:13:46 +0200

    freopen tests: EBADF tests.
    * tests/test-freopen.c: Include errno.h, unistd.h.
    (main): Add tests for EBADF, commented out for the moment.

--- tests/test-freopen.c
+++ tests/test-freopen.c
@@ -23,12 +23,58 @@
 #include "signature.h"
 SIGNATURE_CHECK (freopen, FILE *, (char const *, char const *, FILE *));
 
+#include <errno.h>
+#include <unistd.h>
+
 #include "macros.h"
 
 int
 main ()
 {
+  const char *filename = "test-freopen.txt";
+
   ASSERT (freopen ("/dev/null", "r", stdin) != NULL);
+
+#if 0 /* freopen (NULL, ...) is unsupported on most platforms.  */
+  /* Test that freopen() sets errno if someone else closes the stream
+     fd behind the back of stdio.  */
+  {
+    FILE *fp = fopen (filename, "w+");
+    ASSERT (fp != NULL);
+    ASSERT (close (fileno (fp)) == 0);
+    errno = 0;
+    ASSERT (freopen (NULL, "r", fp) == NULL);
+    perror("freopen");
+    ASSERT (errno == EBADF);
+    fclose (fp);
+  }
+
+  /* Test that freopen() sets errno if the stream was constructed with
+     an invalid file descriptor.  */
+  {
+    FILE *fp = fdopen (-1, "w+");
+    if (fp != NULL)
+      {
+        errno = 0;
+        ASSERT (freopen (NULL, "r", fp) == NULL);
+        ASSERT (errno == EBADF);
+        fclose (fp);
+      }
+  }
+  {
+    FILE *fp = fdopen (99, "w+");
+    if (fp != NULL)
+      {
+        errno = 0;
+        ASSERT (freopen (NULL, "r", fp) == NULL);
+        ASSERT (errno == EBADF);
+        fclose (fp);
+      }
+  }
+#endif
+
+  /* Clean up.  */
+  unlink (filename);
 
   return 0;
 }
