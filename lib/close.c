895d4925b7eb3475050328ef820e8ddfee95e1de
Bruno Haible <bruno@clisp.org>
2011-09-24 02:48:19 +0200

    close: Support for MSVC 9.
    * lib/close.c: Include <errno.h>, msvc-inval.h.
    (close_nothrow): New function.
    (rpl_close): Use it.
    * m4/close.m4 (gl_FUNC_CLOSE): Replace close if the platform has an
    invalid parameter handler.
    * modules/close (Depends-on): Add msvc-inval.
    * modules/dup2-tests (Depends-on): Add close.
    * modules/dup3-tests (Depends-on): Likewise.
    * modules/fcntl-tests (Depends-on): Likewise.
    * modules/spawn-pipe-tests (Depends-on): Likewise.
    * modules/unistd-safer-tests (Depends-on): Likewise.
    * doc/posix-functions/close.texi: Mention the problem on MSVC.

--- lib/close.c
+++ lib/close.c
@@ -19,18 +19,45 @@
 /* Specification.  */
 #include <unistd.h>
 
+#include <errno.h>
+
 #include "fd-hook.h"
+#include "msvc-inval.h"
+
+#undef close
+
+#if HAVE_MSVC_INVALID_PARAMETER_HANDLER
+static int
+close_nothrow (int fd)
+{
+  int result;
+
+  TRY_MSVC_INVAL
+    {
+      result = close (fd);
+    }
+  CATCH_MSVC_INVAL
+    {
+      result = -1;
+      errno = EBADF;
+    }
+  DONE_MSVC_INVAL;
+
+  return result;
+}
+#else
+# define close_nothrow close
+#endif
 
 /* Override close() to call into other gnulib modules.  */
 
 int
 rpl_close (int fd)
-#undef close
 {
 #if WINDOWS_SOCKETS
-  int retval = execute_all_close_hooks (close, fd);
+  int retval = execute_all_close_hooks (close_nothrow, fd);
 #else
-  int retval = close (fd);
+  int retval = close_nothrow (fd);
 #endif
 
 #if REPLACE_FCHDIR
