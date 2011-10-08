87751c19f06c4cf3b5f9858c66fdda5761b1bccb
Bruno Haible <bruno@clisp.org>
2011-10-09 00:48:58 +0200

    Tests for module 'coshf'.
    * modules/coshf-tests: New file.
    * tests/test-coshf.c: New file.

---
+++ tests/test-coshf.c
@@ -0,0 +1,40 @@
+/* Test of coshf() function.
+   Copyright (C) 2010-2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010-2011.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (coshf, float, (float));
+
+#include "macros.h"
+
+volatile float x;
+float y;
+
+int
+main ()
+{
+  /* A particular value.  */
+  x = 0.6f;
+  y = coshf (x);
+  ASSERT (y >= 1.1854652f && y <= 1.1854653f);
+
+  return 0;
+}
