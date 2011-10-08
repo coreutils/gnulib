7b85ee2fc1cf2ab257647f946ae5d47c7f4f95e6
Bruno Haible <bruno@clisp.org>
2011-10-08 22:46:18 +0200

    Tests for module 'powf'.
    * modules/powf-tests: New file.
    * tests/test-powf.c: New file.

---
+++ tests/test-powf.c
@@ -0,0 +1,42 @@
+/* Test of powf() function.
+   Copyright (C) 2010-2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010-2011.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (powf, float, (float, float));
+
+#include "macros.h"
+
+volatile float x;
+volatile float y;
+float z;
+
+int
+main ()
+{
+  /* A particular value.  */
+  x = 243.0f;
+  y = 1.2f;
+  z = powf (x, y);
+  ASSERT (z >= 728.999f && z <= 729.001f);
+
+  return 0;
+}
