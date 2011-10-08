6314401ea71c339d05886365309e5546b335a2bf
Bruno Haible <bruno@clisp.org>
2011-10-09 00:59:39 +0200

    Tests for module 'tanhf'.
    * modules/tanhf-tests: New file.
    * tests/test-tanhf.c: New file.

---
+++ tests/test-tanhf.c
@@ -0,0 +1,40 @@
+/* Test of tanhf() function.
+   Copyright (C) 2010-2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010-2011.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (tanhf, float, (float));
+
+#include "macros.h"
+
+volatile float x;
+float y;
+
+int
+main ()
+{
+  /* A particular value.  */
+  x = 0.6f;
+  y = tanhf (x);
+  ASSERT (y >= 0.5370495f && y <= 0.5370497f);
+
+  return 0;
+}
