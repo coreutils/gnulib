2b5d199a03ec8e72615a222ef6ecf8337d52b3d1
Bruno Haible <bruno@clisp.org>
2011-11-06 02:31:24 +0100

    Tests for module 'fmaf'.
    * modules/fmaf-tests: New file.
    * tests/test-fmaf1.c: New file.
    * tests/test-fmaf2.c: New file.

---
+++ tests/test-fmaf1.c
@@ -0,0 +1,47 @@
+/* Test of fmaf().
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2011.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (fmaf, float, (float, float, float));
+
+#include "isnanf-nolibm.h"
+#include "infinity.h"
+#include "nan.h"
+#include "macros.h"
+
+#undef INFINITY
+#undef NAN
+
+#define DOUBLE float
+#define ISNAN isnanf
+#define INFINITY Infinityf ()
+#define NAN NaNf ()
+#define L_(literal) literal##f
+#include "test-fma1.h"
+
+int
+main ()
+{
+  test_function (fmaf);
+
+  return 0;
+}
---
+++ tests/test-fmaf2.c
@@ -0,0 +1,47 @@
+/* Test of fmaf().
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2011.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "float+.h"
+#include "infinity.h"
+#include "macros.h"
+
+#undef INFINITY
+
+#define DOUBLE float
+#define LDEXP ldexpf
+const int MIN_EXP = FLT_MIN_EXP; /* for gdb */
+#define MIN_EXP FLT_MIN_EXP
+const int MAX_EXP = FLT_MAX_EXP; /* for gdb */
+#define MAX_EXP FLT_MAX_EXP
+const int MANT_BIT = FLT_MANT_BIT; /* for gdb */
+#define MANT_BIT FLT_MANT_BIT
+#define INFINITY Infinityf ()
+#define L_(literal) literal##f
+#include "test-fma2.h"
+
+int
+main ()
+{
+  test_function (fmaf);
+
+  return 0;
+}
