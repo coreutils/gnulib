55c6491b0a19073b7ec2cd25c9474a654916fec1
Bruno Haible <bruno@clisp.org>
2011-11-06 02:33:39 +0100

    Tests for module 'fmal'.
    * modules/fmal-tests: New file.
    * tests/test-fmal1.c: New file.
    * tests/test-fmal2.c: New file.

---
+++ tests/test-fmal1.c
@@ -0,0 +1,52 @@
+/* Test of fmal().
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2011.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (fmal, long double, (long double, long double, long double));
+
+#include "fpucw.h"
+#include "isnanl-nolibm.h"
+#include "infinity.h"
+#include "nan.h"
+#include "macros.h"
+
+#undef INFINITY
+#undef NAN
+
+#define DOUBLE long double
+#define ISNAN isnanl
+#define INFINITY Infinityl ()
+#define NAN NaNl ()
+#define L_(literal) literal##L
+#include "test-fma1.h"
+
+int
+main ()
+{
+  DECL_LONG_DOUBLE_ROUNDING
+
+  BEGIN_LONG_DOUBLE_ROUNDING ();
+
+  test_function (fmal);
+
+  return 0;
+}
---
+++ tests/test-fmal2.c
@@ -0,0 +1,52 @@
+/* Test of fmal().
+   Copyright (C) 2011 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2011.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "fpucw.h"
+#include "float+.h"
+#include "infinity.h"
+#include "macros.h"
+
+#undef INFINITY
+
+#define DOUBLE long double
+#define LDEXP ldexpl
+const int MIN_EXP = LDBL_MIN_EXP; /* for gdb */
+#define MIN_EXP LDBL_MIN_EXP
+const int MAX_EXP = LDBL_MAX_EXP; /* for gdb */
+#define MAX_EXP LDBL_MAX_EXP
+const int MANT_BIT = LDBL_MANT_BIT; /* for gdb */
+#define MANT_BIT LDBL_MANT_BIT
+#define INFINITY Infinityl ()
+#define L_(literal) literal##L
+#include "test-fma2.h"
+
+int
+main ()
+{
+  DECL_LONG_DOUBLE_ROUNDING
+
+  BEGIN_LONG_DOUBLE_ROUNDING ();
+
+  test_function (fmal);
+
+  return 0;
+}
