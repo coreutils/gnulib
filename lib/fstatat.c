e0e8128146064983909c9756c02e2f8cd3fb9e12
Paul Eggert <eggert@cs.ucla.edu>
2011-11-18 18:01:44 -0800

    fstatat: fix configuration bug on mingw, OpenBSD 4, Solaris 8
    * lib/fstatat.c (AT_FUNC_NAME): Use HAVE_FSTAT, not
    HAVE_WORKING_FSTATAT_ZERO_FLAG, to decide whether to define
    rpl_fstatat or fstatat.  This should fix the other problem
    reported by Kai Habel in
    <http://lists.gnu.org/archive/html/bug-gnulib/2011-11/msg00237.html>.
    A similar problem was reported for OpenBSD 4.6 by Mats Erik Andersson
    <http://lists.gnu.org/archive/html/bug-gnulib/2011-11/msg00239.html>
    and I reproduced it on a Solaris 8 host we still have in production.

--- lib/fstatat.c
+++ lib/fstatat.c
@@ -118,7 +118,7 @@ stat_func (char const *name, struct stat *st)
    then give a diagnostic and exit nonzero.
    Otherwise, this function works just like Solaris' fstatat.  */
 
-# if ! HAVE_WORKING_FSTATAT_ZERO_FLAG
+# if HAVE_FSTATAT
 #  define AT_FUNC_NAME rpl_fstatat
 # else
 #  define AT_FUNC_NAME fstatat
