b2e2341dc7a666aa9ec733d6259d4fbab80c5e9b
Bruno Haible <bruno@clisp.org>
2012-01-12 21:58:09 +0100

    random_r: Support for MSVC 9.
    * lib/random_r.c: Include stdint.h, not inttypes.h.

--- lib/random_r.c
+++ lib/random_r.c
@@ -61,7 +61,7 @@
 #include <limits.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <inttypes.h>
+#include <stdint.h>
 
 
 /* An improved random number generation package.  In addition to the standard
