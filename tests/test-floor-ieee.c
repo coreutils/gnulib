b175e453d79f9fef17fde7a61899f89c1fdabe0e
Bruno Haible <bruno@clisp.org>
2012-02-26 14:24:11 +0100

    floor-ieee tests: More tests.
    * tests/test-floor-ieee.c: Include isnand-nolibm.h, infinity.h, nan.h.
    (main): Add tests for [MX] shaded specification in POSIX.
    * modules/floor-ieee-tests (Files): Add tests/infinity.h, tests/nan.h.
    (Depends-on): Add isnand-nolibm.

--- tests/test-floor-ieee.c
+++ tests/test-floor-ieee.c
@@ -18,7 +18,10 @@
 
 #include <math.h>
 
+#include "isnand-nolibm.h"
 #include "minus-zero.h"
+#include "infinity.h"
+#include "nan.h"
 #include "macros.h"
 
 int
@@ -38,6 +41,14 @@ main ()
   /* Negative numbers.  */
   ASSERT (!!signbit (floor (-0.3)) == !!signbit (minus_zerod));
   ASSERT (!!signbit (floor (-0.7)) == !!signbit (minus_zerod));
+
+  /* [MX] shaded specification in POSIX.  */
+
+  /* NaN.  */
+  ASSERT (isnand (floor (NaNd ())));
+  /* Infinity.  */
+  ASSERT (floor (Infinityd ()) == Infinityd ());
+  ASSERT (floor (- Infinityd ()) == - Infinityd ());
 
   return 0;
 }
