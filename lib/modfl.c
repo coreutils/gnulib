06ef7ef9193810369b351b00548cfaa48fad0952
Bruno Haible <bruno@clisp.org>
2012-02-27 04:03:02 +0100

    modfl-ieee: Fix test failures.
    * lib/modfl.c (modfl): Treat NaN and Inf specially.
    * modules/modfl (Depends-on): Add isfinite, isinf.

--- lib/modfl.c
+++ lib/modfl.c
@@ -35,9 +35,25 @@ modfl (long double x, long double *iptr)
 long double
 modfl (long double x, long double *iptr)
 {
-  long double integer_part = truncl (x);
-  *iptr = integer_part;
-  return x - integer_part;
+  if (isfinite (x))
+    {
+      long double integer_part = truncl (x);
+      *iptr = integer_part;
+      return x - integer_part;
+    }
+  else
+    {
+      if (isinf (x))
+        {
+          *iptr = x;
+          return 1.0L / x;
+        }
+      else /* isnanl (x) */
+        {
+          *iptr = x;
+          return x;
+        }
+    }
 }
 
 #endif
