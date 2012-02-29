cecd5115b408a0c0d04af38206d897a627bded76
Bruno Haible <bruno@clisp.org>
2012-02-29 13:24:23 +0100

    Tests for module 'hypotf'.
    * modules/hypotf-tests: New file.
    * tests/test-hypotf.c: New file.

---
+++ tests/test-hypotf.c
@@ -0,0 +1,62 @@
+/* Test of hypotf() function.
+   Copyright (C) 2010-2012 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (hypotf, float, (float, float));
+
+#include <float.h>
+
+#include "macros.h"
+
+volatile float x;
+volatile float y;
+float z;
+
+int
+main ()
+{
+  /* A particular value.  */
+  x = 0.4f;
+  y = 0.6f;
+  z = hypot (x, y);
+  ASSERT (z >= 0.7211102f && z <= 0.7211103f);
+
+  /* Overflow.  */
+  x = FLT_MAX;
+  y = FLT_MAX * 0.5f;
+  z = hypotf (x, y);
+  ASSERT (z == HUGE_VALF);
+
+  /* No underflow.  */
+  x = FLT_MIN;
+  y = 0.0f;
+  z = hypotf (x, y);
+  ASSERT (z == FLT_MIN);
+
+  /* No underflow.  */
+  x = FLT_MIN * 2.0f;
+  y = FLT_MIN * 3.0f;
+  z = hypotf (x, y);
+  ASSERT (z >= FLT_MIN * 2.0f && z <= FLT_MIN * 4.0f);
+
+  return 0;
+}
