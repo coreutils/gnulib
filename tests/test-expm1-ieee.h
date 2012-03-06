f5e8d1d113d7767091266896f6dc5d5b2939b301
Bruno Haible <bruno@clisp.org>
2012-03-07 00:39:56 +0100

    Tests for module 'expm1-ieee'.
    * modules/expm1-ieee-tests: New file.
    * tests/test-expm1-ieee.c: New file.
    * tests/test-expm1-ieee.h: New file.

---
+++ tests/test-expm1-ieee.h
@@ -0,0 +1,43 @@
+/* Test of expm1*() function family.
+   Copyright (C) 2012 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+static void
+test_function (void)
+{
+  /* [MX] shaded specification in POSIX.  */
+
+  /* Underflow.  */
+  ASSERT (EXPM1 (-100000.0) == - L_(1.0));
+
+  /* NaN.  */
+  ASSERT (ISNAN (EXPM1 (NAN)));
+
+  /* Zero.  */
+  {
+    DOUBLE z = EXPM1 (L_(0.0));
+    ASSERT (z == L_(0.0));
+    ASSERT (!signbit (z));
+  }
+  {
+    DOUBLE z = EXPM1 (MINUS_ZERO);
+    ASSERT (z == L_(0.0));
+    ASSERT (!!signbit (z) == !!signbit (MINUS_ZERO));
+  }
+
+  /* Infinity.  */
+  ASSERT (EXPM1 (- INFINITY) == - L_(1.0));
+  ASSERT (EXPM1 (INFINITY) == INFINITY);
+}
