af234b4cb9b96e36e9f22143bd435a8e7decd24f
Bruno Haible <bruno@clisp.org>
2012-03-09 01:14:09 +0100

    Tests for module 'exp2l'.
    * modules/exp2l-tests: New file.
    * tests/test-exp2l.c: New file.

---
+++ tests/test-exp2l.c
@@ -0,0 +1,53 @@
+/* Test of exp2l() function.
+   Copyright (C) 2010-2012 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (exp2l, long double, (long double));
+
+#include <float.h>
+
+#include "fpucw.h"
+#include "macros.h"
+
+#define DOUBLE long double
+#define L_(literal) literal##L
+#define MANT_DIG LDBL_MANT_DIG
+#define EXP2 exp2l
+#define RANDOM randoml
+#include "test-exp2.h"
+
+int
+main ()
+{
+  DECL_LONG_DOUBLE_ROUNDING
+
+  BEGIN_LONG_DOUBLE_ROUNDING ();
+
+  /* A particular value.  */
+  x = 0.6L;
+  y = exp2l (x);
+  ASSERT (y >= 1.515716566L && y <= 1.515716567L);
+
+  test_function ();
+
+  return 0;
+}
