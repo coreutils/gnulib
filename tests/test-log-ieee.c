6d947f828e1404e19a29a6913b7c967f44f5c0f0
Bruno Haible <bruno@clisp.org>
2012-03-10 10:58:53 +0100

    Tests for module 'log-ieee'.
    * modules/log-ieee-tests: New file.
    * tests/test-log-ieee.c: New file.

---
+++ tests/test-log-ieee.c
@@ -0,0 +1,45 @@
+/* Test of log() function.
+   Copyright (C) 2012 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "isnand-nolibm.h"
+#include "minus-zero.h"
+#include "infinity.h"
+#include "nan.h"
+#include "macros.h"
+
+#undef INFINITY
+#undef NAN
+
+#define DOUBLE double
+#define ISNAN isnand
+#define INFINITY Infinityd ()
+#define NAN NaNd ()
+#define L_(literal) literal
+#define MINUS_ZERO minus_zerod
+#define LOG log
+#include "test-log-ieee.h"
+
+int
+main ()
+{
+  test_function ();
+
+  return 0;
+}
