2c610a9bc4b3500bba42d66cb39d2e874b73e882
Bruno Haible <bruno@clisp.org>
2012-03-11 20:52:19 +0100

    Tests for module 'log2f-ieee'.
    * modules/log2f-ieee-tests: New file.
    * tests/test-log2f-ieee.c: New file.
    * tests/test-log2-ieee.h: New file.

---
+++ tests/test-log2-ieee.h
@@ -0,0 +1,40 @@
+/* Test of log2*() function family.
+   Copyright (C) 2012 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+static void
+test_function (void)
+{
+  /* [MX] shaded specification in POSIX.  */
+
+  /* Negative arguments.  */
+  ASSERT (ISNAN (LOG2 (- L_(0.00001))));
+  ASSERT (ISNAN (LOG2 (- L_(1.0))));
+  ASSERT (ISNAN (LOG2 (- L_(1000000.0))));
+  ASSERT (ISNAN (LOG2 (- INFINITY)));
+
+  /* NaN.  */
+  ASSERT (ISNAN (LOG2 (NAN)));
+
+  /* Only rational value.  */
+  {
+    DOUBLE z = LOG2 (L_(1.0));
+    ASSERT (z == L_(0.0));
+    ASSERT (!signbit (z));
+  }
+
+  /* Infinity.  */
+  ASSERT (LOG2 (INFINITY) == INFINITY);
+}
