coreutils/gnulib

commit 44de969cd62abbfe3e7cc7641a8dea7673fd2d6d

maint.mk: avoid spurious failure of _sc_search_regexp-using tests
* top/maint.mk: Initialize _sc_search_regexp parameters, so that
envvar settings cannot interfere.  Otherwise, setting envvars like
prohibit=foo require=bar, etc. would cause spurious test failures.

top/maint.mk
@@ -219,6 +219,17 @@ syntax-check: $(local-check)
 # when filtering by name via in_files, we explicitly filter out matching
 # names here as well.
 
+# Initialize each, so that envvar settings cannot interfere.
+export require =
+export prohibit =
+export exclude =
+export in_vc_files =
+export in_files =
+export containing =
+export non_containing =
+export halt =
+export with_grep_options =
+
 # By default, _sc_search_regexp does not ignore case.
 export ignore_case =
 _ignore_case = $$(test -n "$$ignore_case" && printf %s -i || :)

commit 3d0ba3fe7865e7030b4d2097f122e64b16f0a5c9

maint.mk: fix syntax checks without exclusions
Commit 727075d0 skipped per-line exclusions if ${exclude} is left
undefined, but was immediately broken by commit 44de969c which
always initialized the variable to the empty string.

* top/maint.mk (_sc_search_regexp): Allow for empty variable.
Reported by Daniel P. Berrange.

Signed-off-by: Eric Blake <eblake@redhat.com>

top/maint.mk
@@ -279,7 +279,7 @@ define _sc_search_regexp
    if test -n "$$files"; then						\
      if test -n "$$prohibit"; then					\
        grep $$with_grep_options $(_ignore_case) -nE "$$prohibit" $$files \
-         | grep -vE "$${exclude-^$$}"					\
+         | grep -vE "$${exclude:-^$$}"					\
          && { msg="$$halt" $(_sc_say_and_exit) } || :;			\
      else								\
        grep $$with_grep_options $(_ignore_case) -LE "$$require" $$files \