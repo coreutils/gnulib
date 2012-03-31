c959d1f3dd8168e3ae4ce7cadab44c1d520bef44
Bruno Haible <bruno@clisp.org>
2012-03-31 21:41:32 +0200

    log10f tests: More tests.
    * modules/log10f-tests (Files): Add tests/test-log10.h,
    tests/minus-zero.h, tests/randomf.c.
    (Makefile.am): Add randomf.c to test_log10f_SOURCES.
    * tests/test-log10f.c: Include <float.h>, minus-zero.h, test-log10.h.
    (main): Invoke test_function.

--- tests/test-log10f.c
+++ tests/test-log10f.c
@@ -23,10 +23,19 @@
 #include "signature.h"
 SIGNATURE_CHECK (log10f, float, (float));
 
+#include <float.h>
+
+#include "minus-zero.h"
 #include "macros.h"
 
-volatile float x;
-float y;
+#define DOUBLE float
+#define HUGEVAL HUGE_VALF
+#define L_(literal) literal##f
+#define MANT_DIG FLT_MANT_DIG
+#define MINUS_ZERO minus_zerof
+#define LOG10 log10f
+#define RANDOM randomf
+#include "test-log10.h"
 
 int
 main ()
@@ -35,6 +44,8 @@ main ()
   x = 0.6f;
   y = log10f (x);
   ASSERT (y >= -0.2218488f && y <= -0.2218487f);
+
+  test_function ();
 
   return 0;
 }
