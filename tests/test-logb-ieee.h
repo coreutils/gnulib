b68d1ab22ef82139c2f1c5bfeeaf2e83edd54bf1
Bruno Haible <bruno@clisp.org>
2012-04-03 14:05:01 +0200

    Tests for module 'logbf-ieee'.
    * modules/logbf-ieee-tests: New file.
    * tests/test-logbf-ieee.c: New file.
    * tests/test-logb-ieee.h: New file.

---
+++ tests/test-logb-ieee.h
@@ -0,0 +1,28 @@
+/* Test of log1b*() function family.
+   Copyright (C) 2012 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+static void
+test_function (void)
+{
+  /* [MX] shaded specification in POSIX.  */
+
+  /* NaN.  */
+  ASSERT (ISNAN (LOGB (NAN)));
+
+  /* Infinity.  */
+  ASSERT (LOGB (INFINITY) == INFINITY);
+  ASSERT (LOGB (- INFINITY) == INFINITY);
+}
