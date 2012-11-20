908b11d3f4211b4ee5cb6b5ec0749e49a85394a5
Theophile Ranquet <ranquet@lrde.epita.fr>
2012-11-20 10:38:45 +0000

    maint.mk: avoid gratuitous failure
    Reported by Stefano Lattarini in
    <http://lists.gnu.org/archive/html/bug-bison/2012-11/msg00022.html>
    * top/maint.mk (public-submodule-commit): Quote more safely.

--- top/maint.mk
+++ top/maint.mk
@@ -1390,8 +1390,9 @@ submodule-checks ?= no-submodule-changes public-submodule-commit
 public-submodule-commit:
 	$(AM_V_GEN)if test -d $(srcdir)/.git; then			\
 	  cd $(srcdir) &&						\
-	  git submodule --quiet foreach test '$$(git rev-parse $$sha1)'	\
-	      = '$$(git merge-base origin $$sha1)'			\
+	  git submodule --quiet foreach					\
+	      test '"$$(git rev-parse "$$sha1")"'			\
+	      = '"$$(git merge-base origin "$$sha1")"'			\
 	    || { echo '$(ME): found non-public submodule commit' >&2;	\
 		 exit 1; };						\
 	else								\
