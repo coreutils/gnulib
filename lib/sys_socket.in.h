c75f87d6e47194491fbd3f5db974cf94a5c61f20
Paul Eggert <eggert@cs.ucla.edu>
2012-11-20 22:25:06 -0800

    sys_socket: better 'inline'
    * lib/sys_socket.c: New file.
    * lib/sys_socket.in.h (_GL_SYS_SOCKET_INLINE):
    New macro.  Replace all uses of 'static inline' with it.
    Use _GL_INLINE_HEADER_BEGIN, _GL_INLINE_HEADER_END.
    * m4/sys_socket_h.m4 (gl_HEADER_SYS_SOCKET): Do not require AC_C_INLINE.
    * modules/sys_socket (Files, lib_SOURCES): Add lib/sys_socket.c.
    (Depends-on): Add extern-inline.

--- lib/sys_socket.in.h
+++ lib/sys_socket.in.h
@@ -63,6 +63,11 @@
 #ifndef _@GUARD_PREFIX@_SYS_SOCKET_H
 #define _@GUARD_PREFIX@_SYS_SOCKET_H
 
+_GL_INLINE_HEADER_BEGIN
+#ifndef _GL_SYS_SOCKET_INLINE
+# define _GL_SYS_SOCKET_INLINE _GL_INLINE
+#endif
+
 /* The definitions of _GL_FUNCDECL_RPL etc. are copied here.  */
 
 /* The definition of _GL_ARG_NONNULL is copied here.  */
@@ -201,7 +206,7 @@ struct msghdr {
 
 /* Re-define FD_ISSET to avoid a WSA call while we are not using
    network sockets.  */
-static inline int
+_GL_SYS_SOCKET_INLINE int
 rpl_fd_isset (SOCKET fd, fd_set * set)
 {
   u_int i;
@@ -676,6 +681,8 @@ _GL_WARN_ON_USE (accept4, "accept4 is unportable - "
                  "use gnulib module accept4 for portability");
 # endif
 #endif
+
+_GL_INLINE_HEADER_END
 
 #endif /* _@GUARD_PREFIX@_SYS_SOCKET_H */
 #endif /* _@GUARD_PREFIX@_SYS_SOCKET_H */
