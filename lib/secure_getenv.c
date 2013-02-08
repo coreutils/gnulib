a0f402b76e57d57abd1961f20683a25d46179297
Paul Eggert <eggert@cs.ucla.edu>
2013-02-07 21:58:09 -0800

    secure_getenv: fix include typo
    * lib/secure_getenv.c: Include config.h.  Somehow I forgot!

--- lib/secure_getenv.c
+++ lib/secure_getenv.c
@@ -15,6 +15,8 @@
    You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+#include <config.h>
+
 #include <stdlib.h>
 
 #if !HAVE___SECURE_GETENV
