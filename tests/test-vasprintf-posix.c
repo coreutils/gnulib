068a8246f0e583a5f0a6943ac2db1a437136a998
Gary V. Vaughan <gary@gnu.org>
2013-03-09 12:30:56 +0700

    vasprintf-posix-tests: allow rounding 1.51 to 1.
    * tests/test-vasprintf-posix.c (test_function): Don't insist on
    round-to-even, since POSIX says rounding is implementation-defined
    and OS X 10.8.2 rounds 1.51 to 1 here.

--- tests/test-vasprintf-posix.c
+++ tests/test-vasprintf-posix.c
@@ -246,7 +246,8 @@ test_function (int (*my_asprintf) (char **, const char *, ...))
     int retval =
       my_asprintf (&result, "%.0a %d", 1.51, 33, 44, 55);
     ASSERT (result != NULL);
-    ASSERT (strcmp (result, "0x2p+0 33") == 0
+    ASSERT (strcmp (result, "0x1p+0 33") == 0
+            || strcmp (result, "0x2p+0 33") == 0
             || strcmp (result, "0x3p-1 33") == 0
             || strcmp (result, "0x6p-2 33") == 0
             || strcmp (result, "0xcp-3 33") == 0);
