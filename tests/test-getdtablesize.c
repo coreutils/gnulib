3bd0d48db800994cc10c3340c41d3242390dc68f
Eric Blake <eblake@redhat.com>
2013-09-26 11:26:29 -0600

    getdtablesize: work around cygwin issue
    Cygwin 1.7.25 has a bug that even though it claims to support
    RLIMIT_NOFILE inheritance, there is no enforcement of the soft
    limit, and getdtablesize() automatically grows until it reaches
    the hard limit which cannot be changed by setrlimit().  Best is
    to just treat things as an invariant limit, as several other
    modules assume that getdtablesize() will not change without an
    intervening setrlimit().
    
    * m4/getdtablesize.m4 (gl_FUNC_GETDTABLESIZE): Detect problem.
    * modules/getdtablesize (configure.ac): Build replacement.
    * m4/unistd_h.m4 (gl_UNISTD_H_DEFAULTS): Set up a witness.
    * modules/unistd (Makefile.am): Expose the witness.
    * lib/unistd.in.h (getdtablesize): Declare replacement.
    * lib/getdtablesize.c (rpl_getdtablesize): Work around it.
    * tests/test-getdtablesize.c (main): Test it.
    * doc/glibc-functions/getdtablesize.texi (getdtablesize): Document it.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- tests/test-getdtablesize.c
+++ tests/test-getdtablesize.c
@@ -29,6 +29,8 @@ int
 main (int argc, char *argv[])
 {
   ASSERT (getdtablesize () >= 3);
+  ASSERT (dup2 (0, getdtablesize() - 1) == getdtablesize () - 1);
+  ASSERT (dup2 (0, getdtablesize()) == -1);
 
   return 0;
 }
