53a617f5e118ec5642daa3da45fcef0bf5593b12
Jim Meyering <meyering@fb.com>
2013-10-20 11:32:58 -0700

    error: add the printf attribute to a static function
    * lib/error.c (error_tail): Add the printf attribute, to placate
    gcc's -Werror=suggest-attribute=format option.

--- lib/error.c
+++ lib/error.c
@@ -195,7 +195,7 @@ print_errno_message (int errnum)
 #endif
 }
 
-static void
+static void _GL_ATTRIBUTE_FORMAT_PRINTF (3, 0) _GL_ARG_NONNULL ((3))
 error_tail (int status, int errnum, const char *message, va_list args)
 {
 #if _LIBC
