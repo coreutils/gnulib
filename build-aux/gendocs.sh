aecd38787af5ca0000e184912194e8c83123eb7f
Paul Eggert <eggert@cs.ucla.edu>
2015-01-01 15:19:42 -0800

    gendocs: copyright date and version fix
    Reported by Karl Berry in:
    http://lists.gnu.org/archive/html/bug-gnulib/2015-01/msg00002.html
    * build-aux/gendocs.sh, doc/gendocs_template, doc/gendocs_template_min:
    Fix copyright date.
    * build-aux/gendocs.sh (scriptversion): Update.

--- build-aux/gendocs.sh
+++ build-aux/gendocs.sh
@@ -2,9 +2,9 @@
 # gendocs.sh -- generate a GNU manual in many formats.  This script is
 #   mentioned in maintain.texi.  See the help message below for usage details.
 
-scriptversion=2014-05-01.10
+scriptversion=2015-01-01.15
 
-# Copyright 2003-2013, 2015 Free Software Foundation, Inc.
+# Copyright 2003-2015 Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
