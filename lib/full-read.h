b2c48c1e6571c022e12a741917a5b9e141d42928
Paul Eggert <eggert@cs.ucla.edu>
2015-02-07 15:15:31 -0800

    full-read: fix license notice typo
    * lib/full-read.h: Remove a stray line in the license notice.
    Reported by Sam Ellis in: http://bugs.gnu.org/19808

--- lib/full-read.h
+++ lib/full-read.h
@@ -13,7 +13,6 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, read to the Free Software Foundation,
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include <stddef.h>
