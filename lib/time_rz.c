b0ac02e82a6a1ef9d6285bbb8bef917d4736ac9b
Paul Eggert <eggert@cs.ucla.edu>
2015-10-18 10:24:37 -0700

    time_rz: return NULL if localtime_r fails
    * lib/time_rz.c (localtime_rz): Return NULL if localtime_r fails,
    while still attempting to pacify bleeding-edge GCC.

--- lib/time_rz.c
+++ lib/time_rz.c
@@ -288,9 +288,8 @@ localtime_rz (timezone_t tz, time_t const *t, struct tm *tm)
       timezone_t old_tz = set_tz (tz);
       if (old_tz)
         {
-          if (localtime_r (t, tm) && !save_abbr (tz, tm))
-            tm = NULL;
-          if (revert_tz (old_tz))
+          bool abbr_saved = localtime_r (t, tm) && save_abbr (tz, tm);
+          if (revert_tz (old_tz) && abbr_saved)
             return tm;
         }
       return NULL;
