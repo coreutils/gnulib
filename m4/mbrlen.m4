coreutils/gnulib

commit 45228d9689a677588157fc8d26725a076f4f21e8

maint: use a more standard return from mbrtowc test
* m4/mbrtowc.m4 (gl_MBRTOWC_EMPTY_INPUT): Don't return 1
from the test program as this is non standard and often
indicates an unhandled case in the test program.
* m4/mbrlen.m4 (gl_MBRLEN_EMPTY_INPUT): Likewise.

m4/mbrlen.m4
@@ -253,10 +253,10 @@ changequote([,])dnl
            int
            main (void)
            {
-             return mbrlen ("", 0, &mbs) == (size_t) -2;
+             return ! mbrlen ("", 0, &mbs) == (size_t) -2;
            }]])],
-        [gl_cv_func_mbrlen_empty_input=no],
         [gl_cv_func_mbrlen_empty_input=yes],
+        [gl_cv_func_mbrlen_empty_input=no],
         [:])
     ])
 ])

m4/mbrtowc.m4
@@ -569,10 +569,10 @@ changequote([,])dnl
            int
            main (void)
            {
-             return mbrtowc (&wc, "", 0, &mbs) == (size_t) -2;
+             return ! mbrtowc (&wc, "", 0, &mbs) == (size_t) -2;
            }]])],
-        [gl_cv_func_mbrtowc_empty_input=no],
         [gl_cv_func_mbrtowc_empty_input=yes],
+        [gl_cv_func_mbrtowc_empty_input=no],
         [:])
     ])
 ])

commit 1f63650823cebf52044df840c81062ccb52163a2

maint: fix operator precedence in mbrtowc test
This is a fix for test breakage introduced by commit 45228d96; the
equality expression must be parenthesized when negated with '!',
otherwise we always get:

  test-mbrtowc.c:49: assertion 'ret == (size_t)(-2)' failed

* m4/mbrtowc.m4 (gl_MBRTOWC_EMPTY_INPUT): Negate the entire expression.
* m4/mbrlen.m4 (gl_MBRLEN_EMPTY_INPUT): Likewise.

m4/mbrlen.m4
@@ -253,7 +253,7 @@ changequote([,])dnl
            int
            main (void)
            {
-             return ! mbrlen ("", 0, &mbs) == (size_t) -2;
+             return mbrlen ("", 0, &mbs) != (size_t) -2;
            }]])],
         [gl_cv_func_mbrlen_empty_input=yes],
         [gl_cv_func_mbrlen_empty_input=no],

m4/mbrtowc.m4
@@ -569,7 +569,7 @@ changequote([,])dnl
            int
            main (void)
            {
-             return ! mbrtowc (&wc, "", 0, &mbs) == (size_t) -2;
+             return mbrtowc (&wc, "", 0, &mbs) != (size_t) -2;
            }]])],
         [gl_cv_func_mbrtowc_empty_input=yes],
         [gl_cv_func_mbrtowc_empty_input=no],