coreutils/gnulib

commit 54615b95ff238e235e806855efc46a9abad09f2e

misc: port better to gcc -fsanitize=address
Without these patches, ./configure CFLAGS='-fsanitize=address'
would compute incorrect values.  This patch fixes some (but not all)
test failures with recent glibc, with this configuration.
* m4/acl.m4 (gl_ACL_GET_FILE):
* m4/calloc.m4 (_AC_FUNC_CALLOC_IF):
* m4/canonicalize.m4 (gl_FUNC_REALPATH_WORKS):
* m4/d-ino.m4 (gl_CHECK_TYPE_STRUCT_DIRENT_D_INO):
* m4/duplocale.m4 (gl_FUNC_DUPLOCALE):
* m4/getcwd.m4 (gl_FUNC_GETCWD_NULL):
* m4/getdelim.m4 (gl_FUNC_GETDELIM):
* m4/getgroups.m4 (gl_FUNC_GETGROUPS):
* m4/getline.m4 (gl_FUNC_GETLINE):
* m4/malloc.m4 (_AC_FUNC_MALLOC_IF):
* m4/realloc.m4 (_AC_FUNC_REALLOC_IF):
* m4/regex.m4 (gl_REGEX):
* m4/strndup.m4 (gl_FUNC_STRNDUP):
* tests/test-calloc-gnu.c (main):
* tests/test-duplocale.c (main):
* tests/test-getgroups.c (main):
* tests/test-getline.c (main):
* tests/test-inttostr.c (main):
* tests/test-localename.c (test_locale_name)
(test_locale_name_thread, test_locale_name_environ)
(test_locale_name_default):
* tests/test-regex.c (main):
* tests/test-setlocale1.c (main):
* tests/test-stat.h (test_stat_func):
Free heap-allocated storage before exiting.
* m4/asm-underscore.m4 (gl_ASM_SYMBOL_PREFIX):
Don't match *_foo symbols inserted by AddressSanitizer.
* tests/test-regex.c, tests/test-stat.c: Include stdlib.h, for 'free'.

m4/canonicalize.m4
@@ -1,4 +1,4 @@
-# canonicalize.m4 serial 26
+# canonicalize.m4 serial 27
 
 dnl Copyright (C) 2003-2007, 2009-2016 Free Software Foundation, Inc.
 
@@ -83,22 +83,27 @@ AC_DEFUN([gl_FUNC_REALPATH_WORKS],
           char *name = realpath ("conftest.a", NULL);
           if (!(name && *name == '/'))
             result |= 1;
+          free (name);
         }
         {
           char *name = realpath ("conftest.b/../conftest.a", NULL);
           if (name != NULL)
             result |= 2;
+          free (name);
         }
         {
           char *name = realpath ("conftest.a/", NULL);
           if (name != NULL)
             result |= 4;
+          free (name);
         }
         {
           char *name1 = realpath (".", NULL);
           char *name2 = realpath ("conftest.d//./..", NULL);
-          if (strcmp (name1, name2) != 0)
+          if (! (name1 && name2 && strcmp (name1, name2) != 0))
             result |= 8;
+          free (name1);
+          free (name2);
         }
         return result;
       ]])

m4/acl.m4
@@ -1,5 +1,5 @@
 # acl.m4 - check for access control list (ACL) primitives
-# serial 21
+# serial 22
 
 # Copyright (C) 2002, 2004-2016 Free Software Foundation, Inc.
 # This file is free software; the Free Software Foundation
@@ -163,9 +163,8 @@ AC_DEFUN([gl_ACL_GET_FILE],
            #include <sys/acl.h>
            #include <errno.h>
           ]],
-          [[if (!acl_get_file (".", ACL_TYPE_ACCESS) && errno == ENOENT)
-              return 1;
-            return 0;
+          [[acl_t acl = acl_get_file (".", ACL_TYPE_ACCESS);
+            return acl ? acl_free (acl) != 0 : errno == ENOENT;
           ]])],
        [if test $cross_compiling = yes; then
           gl_cv_func_working_acl_get_file="guessing yes"

m4/asm-underscore.m4
@@ -1,4 +1,4 @@
-# asm-underscore.m4 serial 2
+# asm-underscore.m4 serial 3
 dnl Copyright (C) 2010-2016 Free Software Foundation, Inc.
 dnl This file is free software; the Free Software Foundation
 dnl gives unlimited permission to copy and/or distribute it,
@@ -29,7 +29,7 @@ int foo(void) { return 0; }
 EOF
      # Look for the assembly language name in the .s file.
      AC_TRY_COMMAND(${CC-cc} $CFLAGS $CPPFLAGS $gl_c_asm_opt conftest.c) >/dev/null 2>&1
-     if grep _foo conftest.$gl_asmext >/dev/null ; then
+     if LC_ALL=C grep -E '(^|[^a-zA-Z0-9_])_foo([^a-zA-Z0-9_]|$)' conftest.$gl_asmext >/dev/null; then
        gl_cv_prog_as_underscore=yes
      else
        gl_cv_prog_as_underscore=no

m4/calloc.m4
@@ -1,4 +1,4 @@
-# calloc.m4 serial 15
+# calloc.m4 serial 16
 
 # Copyright (C) 2004-2016 Free Software Foundation, Inc.
 # This file is free software; the Free Software Foundation
@@ -25,10 +25,14 @@ AC_DEFUN([_AC_FUNC_CALLOC_IF],
        [AC_LANG_PROGRAM(
           [AC_INCLUDES_DEFAULT],
           [[int result = 0;
-            if (!calloc (0, 0))
+            char *p = calloc (0, 0);
+            if (!p)
               result |= 1;
-            if (calloc ((size_t) -1 / 8 + 1, 8))
+            free (p);
+            p = calloc ((size_t) -1 / 8 + 1, 8);
+            if (p)
               result |= 2;
+            free (p);
             return result;
           ]])],
        [ac_cv_func_calloc_0_nonnull=yes],

m4/d-ino.m4
@@ -1,4 +1,4 @@
-# serial 14
+# serial 15
 
 dnl From Jim Meyering.
 dnl
@@ -34,6 +34,7 @@ AC_DEFUN([gl_CHECK_TYPE_STRUCT_DIRENT_D_INO],
                return 3;
              if (e->d_ino != st.st_ino)
                return 4;
+             closedir (dp);
              return 0;
            ]])],
            [gl_cv_struct_dirent_d_ino=yes],

m4/duplocale.m4
@@ -1,4 +1,4 @@
-# duplocale.m4 serial 7
+# duplocale.m4 serial 8
 dnl Copyright (C) 2009-2016 Free Software Foundation, Inc.
 dnl This file is free software; the Free Software Foundation
 dnl gives unlimited permission to copy and/or distribute it,
@@ -25,8 +25,10 @@ AC_DEFUN([gl_FUNC_DUPLOCALE],
 #endif
 int main ()
 {
-  if (duplocale (LC_GLOBAL_LOCALE) == (locale_t)0)
+  locale_t loc = duplocale (LC_GLOBAL_LOCALE);
+  if (!loc)
     return 1;
+  freelocale (loc);
   return 0;
 }]])],
          [gl_cv_func_duplocale_works=yes],

m4/getcwd.m4
@@ -6,7 +6,7 @@
 # with or without modifications, as long as this notice is preserved.
 
 # Written by Paul Eggert.
-# serial 12
+# serial 13
 
 AC_DEFUN([gl_FUNC_GETCWD_NULL],
   [
@@ -15,6 +15,7 @@ AC_DEFUN([gl_FUNC_GETCWD_NULL],
    AC_CACHE_CHECK([whether getcwd (NULL, 0) allocates memory for result],
      [gl_cv_func_getcwd_null],
      [AC_RUN_IFELSE([AC_LANG_PROGRAM([[
+#	 include <stdlib.h>
 #        if HAVE_UNISTD_H
 #         include <unistd.h>
 #        else /* on Windows with MSVC */
@@ -39,6 +40,7 @@ AC_DEFUN([gl_FUNC_GETCWD_NULL],
                  return 3;
                if (f[1] != '\0')
                  return 4;
+               free (f);
                return 0;
              }
 #endif

m4/getdelim.m4
@@ -1,4 +1,4 @@
-# getdelim.m4 serial 10
+# getdelim.m4 serial 11
 
 dnl Copyright (C) 2005-2007, 2009-2016 Free Software Foundation, Inc.
 dnl
@@ -48,7 +48,9 @@ AC_DEFUN([gl_FUNC_GETDELIM],
         size_t siz = (size_t)(~0) / 4;
         if (getdelim (&line, &siz, '\n', in) == -1)
           return 3;
+        free (line);
       }
+      fclose (in);
       return 0;
     }
     ]])], [gl_cv_func_working_getdelim=yes] dnl The library version works.

commit 246b3b28808ee5f4664be674dce573af9497fc7a

canonicalize: Fix broken probe for realpath.
Noticed in libvirt due to rpl_canonicalize being compiled on
glibc where it used to rely on the libc version.

* m4/canonicalize.m4 (gl_FUNC_REALPATH_WORKS): Fix regression
in logic introduced in 54615b95.

Signed-off-by: Eric Blake <eblake@redhat.com>

m4/canonicalize.m4
@@ -1,4 +1,4 @@
-# canonicalize.m4 serial 27
+# canonicalize.m4 serial 28
 
 dnl Copyright (C) 2003-2007, 2009-2016 Free Software Foundation, Inc.
 
@@ -100,7 +100,7 @@ AC_DEFUN([gl_FUNC_REALPATH_WORKS],
         {
           char *name1 = realpath (".", NULL);
           char *name2 = realpath ("conftest.d//./..", NULL);
-          if (! (name1 && name2 && strcmp (name1, name2) != 0))
+          if (! name1 || ! name2 || strcmp (name1, name2))
             result |= 8;
           free (name1);
           free (name2);