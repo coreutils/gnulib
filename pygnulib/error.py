46f22b34f9b446076c58b7822561fcc0a78ba656
Dmitry Selyutin <ghostmansd@gmail.com>
2017-08-27 14:29:47 +0300

    provide the most common gnulib-specific error codes

---
+++ pygnulib/error.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# encoding: UTF-8
+
+
+
+class AutoconfVersionError(Exception):
+    """minimum supported autoconf version mismatch"""
+    def __init__(self, version):
+        fmt = "minimum supported autoconf version is %f"
+        message = fmt % version
+        super().__init__(message)
+
+
+
+class M4BaseMismatchError(Exception):
+    """<gnulib-comp.m4> is expected to contain gl_M4_BASE([m4base])"""
+    def __init__(self, m4_base):
+        fmt = "<gnulib-comp.m4> is expected to contain gl_M4_BASE([%s])"
+        message = fmt % m4_base
+        super().__init__(message)
+
+
+
+class ConditionalDependenciesUnavailableError(Exception):
+    """conditional dependencies are not supported with tests"""
+    def __init__(self):
+        message = "conditional dependencies are not supported with tests"
+        super().__init__(message)
+
+
+
+class IncompatibleLicenseError(Exception):
+    """incompatible licenses on modules"""
+    def __init__(self, modules):
+        fmt = "incompatible licenses on modules: %r"
+        message = fmt % modules
+        super().__init__(message)
+
+
+
+class EmptyFileListError(Exception):
+    """cannot process empty file list"""
+    def __init__(self):
+        message = "cannot process empty file list"
+        super().__init__(message)
+
+
+
+class UnknownLicenseError(Exception):
+    """module lacks a license"""
+    def __init__(self, module):
+        fmt = "module lacks a license: %r"
+        message = fmt % module
+        super().__init__(message)
