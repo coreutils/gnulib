b2fc4a431927a6fbdd523bef93357134d9c489a9
Bruno Haible <bruno@clisp.org>
2017-09-23 09:26:48 +0200

    monetary: New module.
    * modules/monetary: New file.
    * lib/monetary.in.h: New file.
    * m4/monetary_h.m4: New file.
    * doc/posix-headers/monetary.texi: Mention the new module.
    * modules/monetary-tests: New file.
    * tests/test-monetary.c: New file.
    * modules/monetary-c++-tests: New file.
    * tests/test-monetary-c++.cc: New file.
    * modules/duplocale-tests (configure.ac): Use AC_CHECK_HEADERS_ONCE.

---
+++ lib/monetary.in.h
@@ -0,0 +1,105 @@
+/* Wrapper around <monetary.h>.
+   Copyright (C) 2017 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <https://www.gnu.org/licenses/>.  */
+
+#ifndef _@GUARD_PREFIX@_MONETARY_H
+
+#if __GNUC__ >= 3
+@PRAGMA_SYSTEM_HEADER@
+#endif
+@PRAGMA_COLUMNS@
+
+/* The include_next requires a split double-inclusion guard.  */
+#if @HAVE_MONETARY_H@
+# @INCLUDE_NEXT@ @NEXT_MONETARY_H@
+#endif
+
+#ifndef _@GUARD_PREFIX@_MONETARY_H
+#define _@GUARD_PREFIX@_MONETARY_H
+
+#if @GNULIB_STRFMON_L@ && @HAVE_XLOCALE_H@
+/* Get locale_t on Mac OS X 10.12.  */
+# include <xlocale.h>
+#endif
+
+/* Like in <stdio.h>.  */
+#if __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7)
+# define _GL_ATTRIBUTE_FORMAT(spec) __attribute__ ((__format__ spec))
+#else
+# define _GL_ATTRIBUTE_FORMAT(spec) /* empty */
+#endif
+
+/* _GL_ATTRIBUTE_FORMAT_STRFMON
+   indicates to GCC that the function takes a format string and arguments,
+   where the format string directives are the ones standardized by ISO C99
+   and POSIX.  */
+#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 4)
+# define _GL_ATTRIBUTE_FORMAT_STRFMON(formatstring_parameter, first_argument) \
+   _GL_ATTRIBUTE_FORMAT ((__gnu_strfmon__, formatstring_parameter, first_argument))
+#elif __GNUC__ >= 3
+# define _GL_ATTRIBUTE_FORMAT_STRFMON(formatstring_parameter, first_argument) \
+   _GL_ATTRIBUTE_FORMAT ((__strfmon__, formatstring_parameter, first_argument))
+#else
+# define _GL_ATTRIBUTE_FORMAT_STRFMON(formatstring_parameter, first_argument) /* empty */
+#endif
+
+
+/* The definitions of _GL_FUNCDECL_RPL etc. are copied here.  */
+
+/* The definition of _GL_ARG_NONNULL is copied here.  */
+
+/* The definition of _GL_WARN_ON_USE is copied here.  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#if @GNULIB_STRFMON_L@
+/* Converts a monetary value to a string.
+   See the POSIX:2008 specification
+   <http://pubs.opengroup.org/onlinepubs/9699919799/functions/strfmon_l.html.  */
+# if @REPLACE_STRFMON_L@
+#  if !(defined __cplusplus && defined GNULIB_NAMESPACE)
+#   define strfmon_l rpl_strfmon_l
+#  endif
+_GL_FUNCDECL_RPL (strfmon_l, ssize_t, (char *s, size_t maxsize, locale_t locale,
+                                       const char *format, ...)
+                                      _GL_ATTRIBUTE_FORMAT_STRFMON (4, 5)
+                                      _GL_ARG_NONNULL ((4)));
+_GL_CXXALIAS_RPL (strfmon_l, ssize_t, (char *s, size_t maxsize, locale_t locale,
+                                       const char *format, ...));
+# else
+#  if @HAVE_STRFMON_L@
+_GL_CXXALIAS_SYS (strfmon_l, ssize_t, (char *s, size_t maxsize, locale_t locale,
+                                       const char *format, ...));
+#  endif
+# endif
+_GL_CXXALIASWARN (strfmon_l);
+#elif defined GNULIB_POSIXCHECK
+# undef strfmon_l
+# if HAVE_RAW_DECL_STRFMON_L
+_GL_WARN_ON_USE (strfmon_l, "strfmon_l is buggy on older glibc systems - "
+                 "use gnulib module chown for portability");
+# endif
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _@GUARD_PREFIX@_MONETARY_H */
+#endif /* _@GUARD_PREFIX@_MONETARY_H */
---
+++ m4/monetary_h.m4
@@ -0,0 +1,65 @@
+# monetary_h.m4 serial 1
+dnl Copyright (C) 2017 Free Software Foundation, Inc.
+dnl This file is free software; the Free Software Foundation
+dnl gives unlimited permission to copy and/or distribute it,
+dnl with or without modifications, as long as this notice is preserved.
+
+AC_DEFUN([gl_MONETARY_H],
+[
+  dnl Use AC_REQUIRE here, so that the default behavior below is expanded
+  dnl once only, before all statements that occur in other macros.
+  AC_REQUIRE([gl_MONETARY_H_BODY])
+])
+
+AC_DEFUN([gl_MONETARY_H_BODY],
+[
+  AC_REQUIRE([gl_MONETARY_H_DEFAULTS])
+
+  AC_CHECK_HEADERS_ONCE([monetary.h])
+  dnl For now, we provide a <monetary.h> wrapper only if the system already
+  dnl has a <monetary.h>.
+  if test $ac_cv_header_monetary_h = yes; then
+    MONETARY_H='monetary.h'
+
+    gl_CHECK_NEXT_HEADERS([monetary.h])
+    if test $ac_cv_header_monetary_h = yes; then
+      HAVE_MONETARY_H=1
+    else
+      HAVE_MONETARY_H=0
+    fi
+    AC_SUBST([HAVE_MONETARY_H])
+
+    AC_CHECK_HEADERS_ONCE([xlocale.h])
+    if test $ac_cv_header_xlocale_h = yes; then
+      HAVE_XLOCALE_H=1
+    else
+      HAVE_XLOCALE_H=0
+    fi
+    AC_SUBST([HAVE_XLOCALE_H])
+
+    dnl Check for declarations of anything we want to poison if the
+    dnl corresponding gnulib module is not in use.
+    gl_WARN_ON_USE_PREPARE([[
+      #include <monetary.h>
+      ]], [strfmon_l])
+  else
+    MONETARY_H=''
+  fi
+  AC_SUBST([MONETARY_H])
+  AM_CONDITIONAL([GL_GENERATE_MONETARY_H], [test -n "$MONETARY_H"])
+])
+
+AC_DEFUN([gl_MONETARY_MODULE_INDICATOR],
+[
+  dnl Use AC_REQUIRE here, so that the default settings are expanded once only.
+  AC_REQUIRE([gl_MONETARY_H_DEFAULTS])
+  gl_MODULE_INDICATOR_SET_VARIABLE([$1])
+])
+
+AC_DEFUN([gl_MONETARY_H_DEFAULTS],
+[
+  GNULIB_STRFMON_L=0;      AC_SUBST([GNULIB_STRFMON_L])
+  dnl Assume proper GNU behavior unless another module says otherwise.
+  HAVE_STRFMON_L=1;        AC_SUBST([HAVE_STRFMON_L])
+  REPLACE_STRFMON_L=0;     AC_SUBST([REPLACE_STRFMON_L])
+])
