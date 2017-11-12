dd1890b79725d4a044039b476e6ad4592d2c9399
Paul Eggert <eggert@cs.ucla.edu>
2017-11-11 22:33:38 -0800

    faccessat: port to macOS (Bug#29231)
    macOS faccessat has the same bug that lstat does: if the file
    name ends in '/' it ignores the trailing slash.
    Problem reported for Emacs by Vincent Zhang.
    * doc/posix-functions/faccessat.texi (faccessat): Document this.
    * lib/faccessat.c (_GL_INCLUDING_UNISTD_H): Define and undef
    around the initial includes.  Include errno.h, string.h, sys/stat.h.
    (orig_faccessat) [HAVE_FACCESSAT]: New function.
    Include "unistd.h" after defining it.
    (rpl_faccessat) [HAVE_FACCESSAT]: New implementation.
    * lib/unistd.in.h (faccessat) [REPLACE_FACCESSAT]:
    Handle in the usual way.
    * m4/faccessat.m4 (gl_FUNC_FACCESSAT): Replace faccessat if
    lstat dereferences symlinks, since faccessat is likely to
    have the same problem.
    * m4/unistd_h.m4 (gl_UNISTD_H_DEFAULTS): Default REPLACE_ACCESSAT.
    * modules/faccessat (Depends-on): Add fstatat.
    Depend if REPLACE_FACCESSAT is 1, too.
    (configure.ac): Link if REPLACE_FACCESSAT is 1.
    * modules/faccessat-tests (Depends-on): Add symlink.
    * modules/unistd (unistd.h): Substitute REPLACE_FACCESSAT.
    * tests/test-faccessat.c (main): Test for the bug.

--- tests/test-faccessat.c
+++ tests/test-faccessat.c
@@ -26,6 +26,8 @@ SIGNATURE_CHECK (faccessat, int, (int, const char *, int, int));
 
 #include "macros.h"
 
+#define BASE "test-lstat.t"
+
 int
 main (void)
 {
@@ -41,6 +43,32 @@ main (void)
     ASSERT (faccessat (99, "foo", F_OK, 0) == -1);
     ASSERT (errno == EBADF);
   }
+
+  /* Test behavior with trailing slash.  */
+  ASSERT (faccessat (AT_FDCWD, ".", X_OK, 0) == 0);
+  ASSERT (faccessat (AT_FDCWD, "./", X_OK, 0) == 0);
+  ASSERT (close (open (BASE "file", O_CREAT | O_WRONLY, 0)) == 0);
+  ASSERT (faccessat (AT_FDCWD, BASE "file", F_OK, 0) == 0);
+  ASSERT (faccessat (AT_FDCWD, BASE "file/", F_OK, 0) != 0);
+  unlink (BASE "link1");
+  if (symlink (".", BASE "link1") == 0)
+    {
+      ASSERT (faccessat (AT_FDCWD, BASE "link1", X_OK, 0) == 0);
+      ASSERT (faccessat (AT_FDCWD, BASE "link1/", X_OK, 0) == 0);
+
+      unlink (BASE "link2");
+      ASSERT (symlink (BASE "file", BASE "link2") == 0);
+      ASSERT (faccessat (AT_FDCWD, BASE "link2", F_OK, 0) == 0);
+      ASSERT (faccessat (AT_FDCWD, BASE "link2/", F_OK, 0) != 0);
+      unlink (BASE "link2");
+
+      unlink (BASE "link3");
+      ASSERT (symlink (BASE "no-such-file", BASE "link3") == 0);
+      ASSERT (faccessat (AT_FDCWD, BASE "link3", F_OK, 0) != 0);
+      ASSERT (faccessat (AT_FDCWD, BASE "link3/", F_OK, 0) != 0);
+      unlink (BASE "link3");
+    }
+  unlink (BASE "link1");
 
   return 0;
 }
