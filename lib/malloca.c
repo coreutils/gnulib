f0dc63fc7ec039999981a4b7555f725c0aa65cc7
Bruno Haible <bruno@clisp.org>
2018-02-03 00:32:41 +0100

    malloca: Add an argument check.
    Suggested by Paul Eggert.
    
    * lib/malloca.c (freea): Check against an invalid argument.

--- lib/malloca.c
+++ lib/malloca.c
@@ -78,6 +78,12 @@ mmalloca (size_t n)
 void
 freea (void *p)
 {
+  /* Check argument.  */
+  if ((uintptr_t) p & (sa_alignment_max - 1))
+    {
+      /* p was not the result of a malloca() call.  Invalid argument.  */
+      abort ();
+    }
   /* Determine whether p was a non-NULL pointer returned by mmalloca().  */
   if ((uintptr_t) p & sa_alignment_max)
     {
