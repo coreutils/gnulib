a96f69a534a91f5a97379e32aa62bacb84e65d82
Bruno Haible <bruno@clisp.org>
2018-03-16 16:25:02 +0100

    glob: Don't compile replacements on recent glibc systems.
    * lib/glob.in.h: Use the usual idiom for the double-inclusion guard. If
    REPLACE_GLOB is 0, include the system's <glob.h> and use
    _GL_CXXALIAS_SYS.
    * m4/glob.m4 (gl_GLOB): Set REPLACE_GLOB instead of GLOB_H. Accept
    _GNU_GLOB_INTERFACE_VERSION 2 as well. Delete the file conf$$-globtest
    inside the AC_RUN_IFELSE block. Remove GL_GENERATE_GLOB_H conditional.
    * modules/glob (Dependencies): Test REPLACE_GLOB instead of GLOB_H.
    Remove snippet/warn-on-use.
    (configure.ac): Test REPLACE_GLOB instead of GLOB_H.
    (Makefile.am): Create glob.h always. Update list of substitutions in
    glob.h. Don't depend on $(WARN_ON_USE_H).

--- lib/glob.in.h
+++ lib/glob.in.h
@@ -17,21 +17,22 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, see <https://www.gnu.org/licenses/>.  */
 
-#ifndef _GL_GLOB_H
-#define _GL_GLOB_H
+#ifndef _@GUARD_PREFIX@_GLOB_H
 
-#include <libc-config.h>
-
-#include <stddef.h>
-
-/* On some systems, such as AIX 5.1, <sys/stat.h> does a "#define stat stat64".
-   Make sure this definition is seen before glob-libc.h defines types that
-   rely on 'struct stat'.  */
-#include <sys/stat.h>
-
-#ifndef __USE_GNU
-# define __USE_GNU    1
+#if __GNUC__ >= 3
+@PRAGMA_SYSTEM_HEADER@
 #endif
+@PRAGMA_COLUMNS@
+
+/* The include_next requires a split double-inclusion guard.  */
+#if !@REPLACE_GLOB@
+# @INCLUDE_NEXT@ @NEXT_GLOB_H@
+#endif
+
+#ifndef _@GUARD_PREFIX@_GLOB_H
+#define _@GUARD_PREFIX@_GLOB_H
+
+/* The definitions of _GL_FUNCDECL_RPL etc. are copied here.  */
 
 /* GCC 2.95 and later have "__restrict"; C99 compilers have
    "restrict", and "configure" may have defined "restrict".
@@ -48,35 +49,71 @@
 # endif
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef int (*_gl_glob_errfunc_fn) (const char *, int);
+#ifdef __cplusplus
+}
+#endif
 
-#define glob rpl_glob
-#define globfree rpl_globfree
-#define glob_pattern_p rpl_glob_pattern_p
-#define __glob_pattern_p glob_pattern_p
 
-#define __GLOB_GNULIB 1
+#if @REPLACE_GLOB@
+
+
+/* Preparations for including the standard GNU C Library header.  */
+
+# include <libc-config.h>
+
+# include <stddef.h>
+
+/* On some systems, such as AIX 5.1, <sys/stat.h> does a "#define stat stat64".
+   Make sure this definition is seen before glob-libc.h defines types that
+   rely on 'struct stat'.  */
+# include <sys/stat.h>
+
+# ifndef __USE_GNU
+#  define __USE_GNU    1
+# endif
+
+# define glob rpl_glob
+# define globfree rpl_globfree
+# define glob_pattern_p rpl_glob_pattern_p
+# define __glob_pattern_p glob_pattern_p
+
+# define __GLOB_GNULIB 1
+
 
 /* Now the standard GNU C Library header should work.  */
-#include "glob-libc.h"
+# include "glob-libc.h"
 
-__BEGIN_DECLS
-typedef int (*_gl_glob_errfunc_fn) (const char *, int);
-__END_DECLS
 
-#if defined __cplusplus && defined GNULIB_NAMESPACE
-# undef glob
-# undef globfree
-# undef glob_pattern_p
+# if defined __cplusplus && defined GNULIB_NAMESPACE
+#  undef glob
+#  undef globfree
+#  undef glob_pattern_p
 _GL_CXXALIAS_RPL (glob, int, (const char *_Restrict_ __pattern, int __flags,
                               _gl_glob_errfunc_fn __errfunc,
                               glob_t *_Restrict_ __pglob));
 _GL_CXXALIAS_RPL (globfree, void, (glob_t *__pglob));
 _GL_CXXALIAS_RPL (glob_pattern_p, int, (const char *__pattern, int __quote));
-# if 0  /* The C function name is rpl_glob, not glob.  */
+# endif
+
+#else
+
+_GL_CXXALIAS_SYS (glob, int, (const char *_Restrict_ __pattern, int __flags,
+                              _gl_glob_errfunc_fn __errfunc,
+                              glob_t *_Restrict_ __pglob));
+_GL_CXXALIAS_SYS (globfree, void, (glob_t *__pglob));
+_GL_CXXALIAS_SYS (glob_pattern_p, int, (const char *__pattern, int __quote));
+
+#endif
+
+#if 0  /* The C function name is rpl_glob in some cases, not glob.  */
 _GL_CXXALIASWARN (glob);
 _GL_CXXALIASWARN (globfree);
 _GL_CXXALIASWARN (glob_pattern_p);
-# endif
 #endif
 
-#endif /* _GL_GLOB_H */
+#endif /* _@GUARD_PREFIX@_GLOB_H */
+#endif /* _@GUARD_PREFIX@_GLOB_H */
