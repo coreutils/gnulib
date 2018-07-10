76b8ad4404ff6a159b9ff589ad872204466f7fd7
Simon Marchi <simon.marchi@ericsson.com>
2018-07-09 19:03:46 -0700

    mkancesdirs: Add extern "C" to header
    * lib/mkancesdirs.h: Add extern "C".

--- lib/mkancesdirs.h
+++ lib/mkancesdirs.h
@@ -1,4 +1,13 @@
 #include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct savewd;
 ptrdiff_t mkancesdirs (char *, struct savewd *,
                        int (*) (char const *, char const *, void *), void *);
+
+#ifdef __cplusplus
+}
+#endif
