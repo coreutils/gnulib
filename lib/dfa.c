c5e76a95605b1ef70559f90ebb656e0a4efb8fd9
Assaf Gordon <assafgordon@gmail.com>
2018-08-01 22:14:21 -0600

    dfa: fix memory leak
    * lib/dfa.c (dfafree): Add missing free() on dfa->superset.

--- lib/dfa.c
+++ lib/dfa.c
@@ -3530,7 +3530,10 @@ dfafree (struct dfa *d)
     }
 
   if (d->superset)
-    dfafree (d->superset);
+    {
+      dfafree (d->superset);
+      free (d->superset);
+    }
 }
 
 /* Having found the postfix representation of the regular expression,
