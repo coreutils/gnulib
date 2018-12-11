eaf9a78f20bdb8e805ae7501bba020272b1f91cf
Bruno Haible <bruno@clisp.org>
2018-12-11 20:56:25 +0100

    avltree-omap: New module.
    * lib/gl_avltree_omap.h: New file.
    * lib/gl_avltree_omap.c: New file.
    * lib/gl_avltree_ordered.h: Code moved to here from
    lib/gl_avltree_oset.c. Parameterize.
    * lib/gl_avltree_oset.c: Include gl_avltree_ordered.h.
    * lib/gl_anytree_omap.h: New file.
    * modules/avltree-omap: New file.
    * modules/avltree-oset (Files): Add lib/gl_avltree_ordered.h.
    (Makefile.am): Add gl_avltree_ordered.h to lib_SOURCES.

---
+++ lib/gl_anytree_omap.h
@@ -0,0 +1,305 @@
+/* Ordered map data type implemented by a binary tree.
+   Copyright (C) 2006-2007, 2009-2018 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2018.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Common code of gl_avltree_omap.c and gl_rbtree_omap.c.  */
+
+/* An item on the stack used for iterating across the elements.  */
+typedef struct
+{
+  gl_omap_node_t node;
+  bool rightp;
+} iterstack_item_t;
+
+/* A stack used for iterating across the elements.  */
+typedef iterstack_item_t iterstack_t[MAXHEIGHT];
+
+static gl_omap_t
+gl_tree_nx_create_empty (gl_omap_implementation_t implementation,
+                         gl_mapkey_compar_fn compar_fn,
+                         gl_mapkey_dispose_fn kdispose_fn,
+                         gl_mapvalue_dispose_fn vdispose_fn)
+{
+  struct gl_omap_impl *map =
+    (struct gl_omap_impl *) malloc (sizeof (struct gl_omap_impl));
+
+  if (map == NULL)
+    return NULL;
+
+  map->base.vtable = implementation;
+  map->base.compar_fn = compar_fn;
+  map->base.kdispose_fn = kdispose_fn;
+  map->base.vdispose_fn = vdispose_fn;
+  map->root = NULL;
+  map->count = 0;
+
+  return map;
+}
+
+static size_t
+gl_tree_size (gl_omap_t map)
+{
+  return map->count;
+}
+
+static bool
+gl_tree_search (gl_omap_t map, const void *key, const void **valuep)
+{
+  gl_mapkey_compar_fn compar = map->base.compar_fn;
+  gl_omap_node_t node;
+
+  for (node = map->root; node != NULL; )
+    {
+      int cmp = (compar != NULL
+                 ? compar (node->key, key)
+                 : (node->key > key ? 1 :
+                    node->key < key ? -1 : 0));
+
+      if (cmp < 0)
+        node = node->right;
+      else if (cmp > 0)
+        node = node->left;
+      else /* cmp == 0 */
+        {
+          /* We have a key equal to KEY.  */
+          *valuep = node->value;
+          return true;
+        }
+    }
+  return false;
+}
+
+static bool
+gl_tree_search_atleast (gl_omap_t map,
+                        gl_mapkey_threshold_fn threshold_fn,
+                        const void *threshold,
+                        const void **keyp, const void **valuep)
+{
+  gl_omap_node_t node;
+
+  for (node = map->root; node != NULL; )
+    {
+      if (! threshold_fn (node->key, threshold))
+        node = node->right;
+      else
+        {
+          /* We have a key >= THRESHOLD.  But we need the leftmost such
+             element.  */
+          gl_omap_node_t found = node;
+          node = node->left;
+          for (; node != NULL; )
+            {
+              if (! threshold_fn (node->key, threshold))
+                node = node->right;
+              else
+                {
+                  found = node;
+                  node = node->left;
+                }
+            }
+          *keyp = found->key;
+          *valuep = found->value;
+          return true;
+        }
+    }
+  return false;
+}
+
+static int
+gl_tree_nx_getput (gl_omap_t map, const void *key, const void *value,
+                   const void **oldvaluep)
+{
+  gl_mapkey_compar_fn compar;
+  gl_omap_node_t node = map->root;
+
+  if (node == NULL)
+    {
+      if (gl_tree_nx_add_first (map, key, value) == NULL)
+        return -1;
+      return true;
+    }
+
+  compar = map->base.compar_fn;
+
+  for (;;)
+    {
+      int cmp = (compar != NULL
+                 ? compar (node->key, key)
+                 : (node->key > key ? 1 :
+                    node->key < key ? -1 : 0));
+
+      if (cmp < 0)
+        {
+          if (node->right == NULL)
+            {
+              if (gl_tree_nx_add_after (map, node, key, value) == NULL)
+                return -1;
+              return true;
+            }
+          node = node->right;
+        }
+      else if (cmp > 0)
+        {
+          if (node->left == NULL)
+            {
+              if (gl_tree_nx_add_before (map, node, key, value) == NULL)
+                return -1;
+              return true;
+            }
+          node = node->left;
+        }
+      else /* cmp == 0 */
+        {
+          *oldvaluep = node->value;
+          node->value = value;
+          return false;
+        }
+    }
+}
+
+static bool
+gl_tree_getremove (gl_omap_t map, const void *key, const void **oldvaluep)
+{
+  gl_mapkey_compar_fn compar = map->base.compar_fn;
+  gl_omap_node_t node;
+
+  for (node = map->root; node != NULL; )
+    {
+      int cmp = (compar != NULL
+                 ? compar (node->key, key)
+                 : (node->key > key ? 1 :
+                    node->key < key ? -1 : 0));
+
+      if (cmp < 0)
+        node = node->right;
+      else if (cmp > 0)
+        node = node->left;
+      else /* cmp == 0 */
+        {
+          /* We have a key equal to KEY.  */
+          *oldvaluep = node->value;
+          gl_tree_remove_node (map, node);
+          return true;
+        }
+    }
+  return false;
+}
+
+static void
+gl_tree_omap_free (gl_omap_t map)
+{
+  /* Iterate across all elements in post-order.  */
+  gl_omap_node_t node = map->root;
+  iterstack_t stack;
+  iterstack_item_t *stack_ptr = &stack[0];
+
+  for (;;)
+    {
+      /* Descend on left branch.  */
+      for (;;)
+        {
+          if (node == NULL)
+            break;
+          stack_ptr->node = node;
+          stack_ptr->rightp = false;
+          node = node->left;
+          stack_ptr++;
+        }
+      /* Climb up again.  */
+      for (;;)
+        {
+          if (stack_ptr == &stack[0])
+            goto done_iterate;
+          stack_ptr--;
+          node = stack_ptr->node;
+          if (!stack_ptr->rightp)
+            break;
+          /* Free the current node.  */
+          if (map->base.vdispose_fn != NULL)
+            map->base.vdispose_fn (node->value);
+          if (map->base.kdispose_fn != NULL)
+            map->base.kdispose_fn (node->key);
+          free (node);
+        }
+      /* Descend on right branch.  */
+      stack_ptr->rightp = true;
+      node = node->right;
+      stack_ptr++;
+    }
+ done_iterate:
+  free (map);
+}
+
+/* --------------------- gl_omap_iterator_t Data Type --------------------- */
+
+static gl_omap_iterator_t
+gl_tree_iterator (gl_omap_t map)
+{
+  gl_omap_iterator_t result;
+  gl_omap_node_t node;
+
+  result.vtable = map->base.vtable;
+  result.map = map;
+  /* Start node is the leftmost node.  */
+  node = map->root;
+  if (node != NULL)
+    while (node->left != NULL)
+      node = node->left;
+  result.p = node;
+  /* End point is past the rightmost node.  */
+  result.q = NULL;
+#if defined GCC_LINT || defined lint
+  result.i = 0;
+  result.j = 0;
+  result.count = 0;
+#endif
+
+  return result;
+}
+
+static bool
+gl_tree_iterator_next (gl_omap_iterator_t *iterator,
+                       const void **keyp, const void **valuep)
+{
+  if (iterator->p != iterator->q)
+    {
+      gl_omap_node_t node = (gl_omap_node_t) iterator->p;
+      *keyp = node->key;
+      *valuep = node->value;
+      /* Advance to the next node.  */
+      if (node->right != NULL)
+        {
+          node = node->right;
+          while (node->left != NULL)
+            node = node->left;
+        }
+      else
+        {
+          while (node->parent != NULL && node->parent->right == node)
+            node = node->parent;
+          node = node->parent;
+        }
+      iterator->p = node;
+      return true;
+    }
+  else
+    return false;
+}
+
+static void
+gl_tree_iterator_free (gl_omap_iterator_t *iterator)
+{
+}
---
+++ lib/gl_avltree_omap.c
@@ -0,0 +1,75 @@
+/* Ordered map data type implemented by a binary tree.
+   Copyright (C) 2006-2007, 2009-2018 Free Software Foundation, Inc.
+   Written by Bruno Haible <bruno@clisp.org>, 2018.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "gl_avltree_omap.h"
+
+#include <stdlib.h>
+
+/* -------------------------- gl_omap_t Data Type -------------------------- */
+
+/* Parameterization of gl_avltree_ordered.h.  */
+#define CONTAINER_T gl_omap_t
+#define CONTAINER_IMPL gl_omap_impl
+#define CONTAINER_IMPL_BASE gl_omap_impl_base
+#define NODE_IMPL gl_omap_node_impl
+#define NODE_T gl_omap_node_t
+#define NODE_PAYLOAD_FIELDS \
+  const void *key; \
+  const void *value;
+#define NODE_PAYLOAD_PARAMS \
+  const void *key, const void *value
+#define NODE_PAYLOAD_ASSIGN(node) \
+  node->key = key; \
+  node->value = value;
+#define NODE_PAYLOAD_DISPOSE \
+  if (container->base.vdispose_fn != NULL) \
+    container->base.vdispose_fn (node->value); \
+  if (container->base.kdispose_fn != NULL) \
+    container->base.kdispose_fn (node->key);
+
+#include "gl_avltree_ordered.h"
+
+/* Generic binary tree code.  */
+#include "gl_anytree_omap.h"
+
+/* For debugging.  */
+void
+gl_avltree_omap_check_invariants (gl_omap_t map)
+{
+  size_t counter = 0;
+  if (map->root != NULL)
+    check_invariants (map->root, NULL, &counter);
+  if (!(map->count == counter))
+    abort ();
+}
+
+const struct gl_omap_implementation gl_avltree_omap_implementation =
+  {
+    gl_tree_nx_create_empty,
+    gl_tree_size,
+    gl_tree_search,
+    gl_tree_search_atleast,
+    gl_tree_nx_getput,
+    gl_tree_getremove,
+    gl_tree_omap_free,
+    gl_tree_iterator,
+    gl_tree_iterator_next,
+    gl_tree_iterator_free
+  };
