91584ed612fa26a505c8fc4c7f6fb19f7413795d
Bruno Haible <bruno@clisp.org>
2019-04-07 13:37:02 +0200

    Add copyright notices in several files.
    Reported by <ineiev@gnu.org> in <https://savannah.gnu.org/bugs/?54809>.
    
    * lib/_Noreturn.h: Add LGPLv2+ copyright notice.
    * lib/libunistring.valgrind: Likewise.
    * lib/iconv_open-*.gperf: Add GPLv2+ copyright notice.
    * lib/uniname/gen-uninames.lisp: Add GPLv3+ copyright notice.
    * lib/memchr.valgrind: Likewise.
    * lib/memchr2.valgrind: Likewise.
    * lib/rawmemchr.valgrind: Likewise.
    * lib/relocatable.valgrind: Likewise.
    * lib/strchrnul.valgrind: Likewise.

--- lib/uniname/gen-uninames.lisp
+++ lib/uniname/gen-uninames.lisp
@@ -1,7 +1,22 @@
 #!/usr/local/bin/clisp -C
 
 ;;; Creation of gnulib's uninames.h from the UnicodeData.txt table.
-;;; Bruno Haible 2000-12-28
+
+;;; Copyright (C) 2000-2019 Free Software Foundation, Inc.
+;;; Written by Bruno Haible <bruno@clisp.org>, 2000-12-28.
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 (defparameter add-comments nil)
 
