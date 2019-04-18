coreutils/gnulib

commit 3703dbbe88dd0454ca5759b24b2f827783986245

di-set: Fix memory leak.
* lib/di-set.c (di_set_free): Free the ino_map through ino_map_free(),
not free().

lib/di-set.c
@@ -136,7 +136,7 @@ void
 di_set_free (struct di_set *dis)
 {
   hash_free (dis->dev_map);
-  free (dis->ino_map);
+  ino_map_free (dis->ino_map);
   free (dis->probe);
   free (dis);
 }

commit 22b911f63ca11395f03ce2ce3cc0b371a37a576d

di-set: allow free with 'ino_map' being NULL
* lib/di-set.c (di_set_free): Avoid ino_map_free() when dis->ino_map
is NULL.  Bug introduced in commit 3703dbbe88dd.
* tests/test-di-set.c: Add di_set_free() right after di_set_alloc()
as a test.

lib/di-set.c
@@ -136,7 +136,8 @@ void
 di_set_free (struct di_set *dis)
 {
   hash_free (dis->dev_map);
-  ino_map_free (dis->ino_map);
+  if (dis->ino_map)
+    ino_map_free (dis->ino_map);
   free (dis->probe);
   free (dis);
 }

tests/test-di-set.c
@@ -27,6 +27,9 @@ main (void)
 {
   struct di_set *dis = di_set_alloc ();
   ASSERT (dis);
+  di_set_free (dis); /* free with dis->ino_map still being NULL */
+  dis = di_set_alloc ();
+  ASSERT (dis);
 
   ASSERT (di_set_lookup (dis, 2, 5) == 0); /* initial lookup fails */
   ASSERT (di_set_insert (dis, 2, 5) == 1); /* first insertion succeeds */