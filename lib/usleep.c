54d342c75690664d6d8e5c34d8512e253db23cf6
Bruno Haible <bruno@clisp.org>
2019-07-02 19:57:03 +0200

    usleep: Implement with millisecond resolution on native Windows.
    * lib/usleep.c (usleep): On native Windows, implement using Sleep().
    * doc/pastposix-functions/usleep.texi: Update accordingly.

--- lib/usleep.c
+++ lib/usleep.c
@@ -28,6 +28,11 @@
 
 #include <errno.h>
 
+#if defined _WIN32 && ! defined __CYGWIN__
+# define WIN32_LEAN_AND_MEAN  /* avoid including junk */
+# include <windows.h>
+#endif
+
 #ifndef HAVE_USLEEP
 # define HAVE_USLEEP 0
 #endif
@@ -39,7 +44,20 @@
 
 int
 usleep (useconds_t micro)
+#undef usleep
 {
+#if defined _WIN32 && ! defined __CYGWIN__
+  unsigned int milliseconds = micro / 1000;
+  if (sizeof milliseconds < sizeof micro && micro / 1000 != milliseconds)
+    {
+      errno = EINVAL;
+      return -1;
+    }
+  if (micro % 1000)
+    milliseconds++;
+  Sleep (milliseconds);
+  return 0;
+#else
   unsigned int seconds = micro / 1000000;
   if (sizeof seconds < sizeof micro && micro / 1000000 != seconds)
     {
@@ -50,9 +68,9 @@ usleep (useconds_t micro)
     seconds++;
   while ((seconds = sleep (seconds)) != 0);
 
-#undef usleep
-#if !HAVE_USLEEP
-# define usleep(x) 0
-#endif
+# if !HAVE_USLEEP
+#  define usleep(x) 0
+# endif
   return usleep (micro % 1000000);
+#endif
 }
