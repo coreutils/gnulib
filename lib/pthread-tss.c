672c3be2f4c01f3b446ac3588bfde2eb6fd6ccea
Bruno Haible <bruno@clisp.org>
2019-07-15 02:41:00 +0200

    pthread-tss: New module.
    * lib/pthread-tss.c: New file.
    * m4/pthread-tss.m4: New file.
    * modules/pthread-tss: New file.
    * doc/posix-functions/pthread_key_create.texi: Mention the new module.
    * doc/posix-functions/pthread_setspecific.texi: Likewise.
    * doc/posix-functions/pthread_getspecific.texi: Likewise.
    * doc/posix-functions/pthread_key_delete.texi: Likewise.

---
+++ lib/pthread-tss.c
@@ -0,0 +1,94 @@
+/* POSIX thread-specific storage.
+   Copyright (C) 2010-2019 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2019.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include <pthread.h>
+
+#if (defined _WIN32 && ! defined __CYGWIN__) && USE_WINDOWS_THREADS
+# include "windows-tls.h"
+#else
+# include <stdlib.h>
+#endif
+
+#if (defined _WIN32 && ! defined __CYGWIN__) && USE_WINDOWS_THREADS
+/* Use Windows threads.  */
+
+int
+pthread_key_create (pthread_key_t *keyp, void (*destructor) (void *))
+{
+  return glwthread_tls_key_create (keyp, destructor);
+}
+
+int
+pthread_setspecific (pthread_key_t key, const void *value)
+{
+  return glwthread_tls_set (key, (void *) value);
+}
+
+void *
+pthread_getspecific (pthread_key_t key)
+{
+  return glwthread_tls_get (key);
+}
+
+int
+pthread_key_delete (pthread_key_t key)
+{
+  return glwthread_tls_key_delete (key);
+}
+
+#elif HAVE_PTHREAD_H
+/* Provide workarounds for POSIX threads.  */
+
+#else
+/* Provide a dummy implementation for single-threaded applications.  */
+
+int
+pthread_key_create (pthread_key_t *keyp, void (*destructor) (void *))
+{
+  pthread_key_t key = (void **) malloc (sizeof (void *));
+  if (key == NULL)
+    return ENOMEM;
+  *key = NULL;
+  *keyp = key;
+  return 0;
+}
+
+int
+pthread_setspecific (pthread_key_t key, const void *value)
+{
+  *key = (void *) value;
+  return 0;
+}
+
+void *
+pthread_getspecific (pthread_key_t key)
+{
+  return *key;
+}
+
+int
+pthread_key_delete (pthread_key_t key)
+{
+  free (key);
+  return 0;
+}
+
+#endif
---
+++ m4/pthread-tss.m4
@@ -0,0 +1,28 @@
+# pthread-tss.m4 serial 1
+dnl Copyright (C) 2019 Free Software Foundation, Inc.
+dnl This file is free software; the Free Software Foundation
+dnl gives unlimited permission to copy and/or distribute it,
+dnl with or without modifications, as long as this notice is preserved.
+
+AC_DEFUN([gl_PTHREAD_TSS],
+[
+  AC_REQUIRE([gl_PTHREAD_H])
+  AC_REQUIRE([AC_CANONICAL_HOST])
+
+  if { case "$host_os" in mingw*) true;; *) false;; esac; } \
+     && test $gl_threads_api = windows; then
+    dnl Choose function names that don't conflict with the mingw-w64 winpthreads
+    dnl library.
+    REPLACE_PTHREAD_KEY_CREATE=1
+    REPLACE_PTHREAD_SETSPECIFIC=1
+    REPLACE_PTHREAD_GETSPECIFIC=1
+    REPLACE_PTHREAD_KEY_DELETE=1
+  else
+    if test $HAVE_PTHREAD_H = 0; then
+      HAVE_PTHREAD_KEY_CREATE=0
+      HAVE_PTHREAD_SETSPECIFIC=0
+      HAVE_PTHREAD_GETSPECIFIC=0
+      HAVE_PTHREAD_KEY_DELETE=0
+    fi
+  fi
+])
