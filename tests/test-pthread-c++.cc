79e228295ece60c7ba44fc68d313893270d503ca
Bruno Haible <bruno@clisp.org>
2019-07-15 02:36:24 +0200

    pthread-h: Add declarations of essential pthread functions.
    * lib/pthread.in.h: Include snippets.
    (pthread_create, pthread_attr_init, pthread_attr_getdetachstate,
    pthread_attr_setdetachstate, pthread_attr_destroy, pthread_self,
    pthread_equal, pthread_detach, pthread_join, pthread_exit, pthread_once,
    pthread_mutex_init, pthread_mutexattr_init, pthread_mutexattr_gettype,
    pthread_mutexattr_settype, pthread_mutexattr_getrobust,
    pthread_mutexattr_setrobust, pthread_mutexattr_destroy,
    pthread_mutex_lock, pthread_mutex_trylock, pthread_mutex_unlock,
    pthread_mutex_destroy, pthread_rwlock_init, pthread_rwlockattr_init,
    pthread_rwlockattr_destroy, pthread_rwlock_rdlock,
    pthread_rwlock_wrlock, pthread_rwlock_tryrdlock,
    pthread_rwlock_trywrlock, pthread_rwlock_timedrdlock,
    pthread_rwlock_timedwrlock, pthread_rwlock_unlock,
    pthread_rwlock_destroy, pthread_cond_init, pthread_condattr_init,
    pthread_condattr_destroy, pthread_cond_wait, pthread_cond_timedwait,
    pthread_cond_signal, pthread_cond_broadcast, pthread_cond_destroy,
    pthread_key_create, pthread_setspecific, pthread_getspecific,
    pthread_key_delete, pthread_spin_init, pthread_spin_lock,
    pthread_spin_trylock, pthread_spin_unlock, pthread_spin_destroy): New
    declarations.
    (pthread_mutex_timedlock): Move declaration.
    * m4/pthread_h.m4 (gl_PTHREAD_H): Check whether the new functions are
    declared.
    (gl_PTHREAD_H_DEFAULTS): Initialize GNULIB_PTHREAD_THREAD,
    GNULIB_PTHREAD_ONCE, GNULIB_PTHREAD_MUTEX, GNULIB_PTHREAD_RWLOCK,
    GNULIB_PTHREAD_COND, GNULIB_PTHREAD_TSS, GNULIB_PTHREAD_SPIN and the
    HAVE_* and REPLACE_* variables for the new functions.
    * modules/pthread-h (Depends-on): Add snippet/c++defs,
    snippet/_Noreturn, snippet/arg-nonnull, snippet/warn-on-use.
    (Makefile.am): Substitute GNULIB_PTHREAD_THREAD, GNULIB_PTHREAD_ONCE,
    GNULIB_PTHREAD_MUTEX, GNULIB_PTHREAD_RWLOCK, GNULIB_PTHREAD_COND,
    GNULIB_PTHREAD_TSS, GNULIB_PTHREAD_SPIN and the HAVE_* and REPLACE_*
    variables for the new functions. Split the sed script, to avoid the
    limit of 99 commands of HP-UX sed.
    * tests/test-pthread-c++.cc: Check the signature of the new functions.

--- tests/test-pthread-c++.cc
+++ tests/test-pthread-c++.cc
@@ -24,9 +24,268 @@
 #include "signature.h"
 
 
+/* =========== Thread functions =========== */
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_create, int,
+                 (pthread_t *, const pthread_attr_t *,
+                  void * (*) (void*), void *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_attr_init, int, (pthread_attr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_attr_getdetachstate, int,
+                 (pthread_attr_t *, int *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_attr_setdetachstate, int,
+                 (pthread_attr_t *, int));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_attr_destroy, int,
+                 (pthread_attr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_self, pthread_t, (void));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_equal, int, (pthread_t, pthread_t));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_detach, int, (pthread_t));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_join, int, (pthread_t, void **));
+#endif
+
+#if GNULIB_TEST_PTHREAD_THREAD
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_exit, _Noreturn void, (void *));
+#endif
+
+/* =========== Once-only control (initialization) functions =========== */
+
+#if GNULIB_TEST_PTHREAD_ONCE
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_once, int,
+                 (pthread_once_t *, void (*) (void)));
+#endif
+
+/* =========== Mutex functions =========== */
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutex_init, int,
+                 (pthread_mutex_t *, const pthread_mutexattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutexattr_init, int,
+                 (pthread_mutexattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutexattr_gettype, int,
+                 (const pthread_mutexattr_t *, int *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutexattr_settype, int,
+                 (pthread_mutexattr_t *, int));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutexattr_getrobust, int,
+                 (const pthread_mutexattr_t *, int *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutexattr_setrobust, int,
+                 (pthread_mutexattr_t *, int));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutexattr_destroy, int,
+                 (pthread_mutexattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutex_lock, int,
+                 (pthread_mutex_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutex_trylock, int,
+                 (pthread_mutex_t *));
+#endif
+
 #if GNULIB_TEST_PTHREAD_MUTEX_TIMEDLOCK
 SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutex_timedlock, int,
                  (pthread_mutex_t *, const struct timespec *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutex_unlock, int,
+                 (pthread_mutex_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_MUTEX
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_mutex_destroy, int,
+                 (pthread_mutex_t *));
+#endif
+
+/* =========== Read-write lock functions =========== */
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_init, int,
+                 (pthread_rwlock_t *, const pthread_rwlockattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlockattr_init, int,
+                 (pthread_rwlockattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlockattr_destroy, int,
+                 (pthread_rwlockattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_rdlock, int,
+                 (pthread_rwlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_wrlock, int,
+                 (pthread_rwlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_tryrdlock, int,
+                 (pthread_rwlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_trywrlock, int,
+                 (pthread_rwlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_timedrdlock, int,
+                 (pthread_rwlock_t *, const struct timespec *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_timedwrlock, int,
+                 (pthread_rwlock_t *, const struct timespec *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_unlock, int,
+                 (pthread_rwlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_RWLOCK
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_rwlock_destroy, int,
+                 (pthread_rwlock_t *));
+#endif
+
+/* =========== Condition variable functions =========== */
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_cond_init, int,
+                 (pthread_cond_t *, const pthread_condattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_condattr_init, int,
+                 (pthread_condattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_condattr_destroy, int,
+                 (pthread_condattr_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_cond_wait, int,
+                 (pthread_cond_t *, pthread_mutex_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_cond_timedwait, int,
+                 (pthread_cond_t *, pthread_mutex_t *,
+                  const struct timespec *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_cond_signal, int,
+                 (pthread_cond_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_cond_broadcast, int,
+                 (pthread_cond_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_COND
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_cond_destroy, int,
+                 (pthread_cond_t *));
+#endif
+
+/* =========== Thread-specific storage functions =========== */
+
+#if GNULIB_TEST_PTHREAD_TSS
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_key_create, int,
+                 (pthread_key_t *, void (*) (void *)));
+#endif
+
+#if GNULIB_TEST_PTHREAD_TSS
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_setspecific, int,
+                 (pthread_key_t, const void *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_TSS
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_getspecific, void *,
+                 (pthread_key_t));
+#endif
+
+#if GNULIB_TEST_PTHREAD_TSS
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_key_delete, int, (pthread_key_t));
+#endif
+
+/* =========== Spinlock functions =========== */
+
+#if GNULIB_TEST_PTHREAD_SPIN
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_spin_init, int,
+                 (pthread_spinlock_t *, int));
+#endif
+
+#if GNULIB_TEST_PTHREAD_SPIN
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_spin_lock, int,
+                 (pthread_spinlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_SPIN
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_spin_trylock, int,
+                 (pthread_spinlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_SPIN
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_spin_unlock, int,
+                 (pthread_spinlock_t *));
+#endif
+
+#if GNULIB_TEST_PTHREAD_SPIN
+SIGNATURE_CHECK (GNULIB_NAMESPACE::pthread_spin_destroy, int,
+                 (pthread_spinlock_t *));
 #endif
 
 
