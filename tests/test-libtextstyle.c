982bb6cf0704b36d7d655b34e9d4b7617d8137a6
Bruno Haible <bruno@clisp.org>
2019-08-14 02:48:59 +0200

    libtextstyle-optional tests: Support the NO_COLOR environment variable.
    * tests/test-libtextstyle.c (main): Do not emit styling when the
    environment variable NO_COLOR is set.

--- tests/test-libtextstyle.c
+++ tests/test-libtextstyle.c
@@ -57,7 +57,9 @@ main (int argc, char *argv[])
 
   #if HAVE_LIBTEXTSTYLE
   if (color_mode == color_yes
-      || (color_mode == color_tty && isatty (STDOUT_FILENO))
+      || (color_mode == color_tty
+          && isatty (STDOUT_FILENO)
+          && getenv ("NO_COLOR") == NULL)
       || color_mode == color_html)
     {
       /* If no style file is explicitly specified, use the default in the
