ddfc731627e86f590eb99b4f1b07d04cc9b1ef79
Jim Meyering <meyering@fb.com>
2020-01-04 22:52:20 -0800

    update-copyright: reenable its always-skipped test
    * tests/test-update-copyright.sh: Restore the "-pi" options removed
    on 2019-06-15.  Without those, an internal preliminary test would
    fail, causing this test always to be skipped.
    Verify that the test is now run and passes via this:
      ./gnulib-tool --test --dir /tmp/x --with-tests update-copyright

--- tests/test-update-copyright.sh
+++ tests/test-update-copyright.sh
@@ -51,7 +51,7 @@ cat <<\EOF > $s
 # the script through the perl program found in $PATH.  The '-x' option
 # is essential as well; without it, perl would re-execute the script
 # through /bin/sh.  When executed by  perl, the next two lines are a no-op.
-eval 'exec perl -wSx "$0" "$@"'
+eval 'exec perl -wSx -pi "$0" "$@"'
      if 0;
 
 s/a/b/
