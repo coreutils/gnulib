f3243f20107435f620338aa15e1fdd5779d639fe
Paul Eggert <eggert@cs.ucla.edu>
2020-02-16 12:47:52 -0800

    xnanosleep: prefer pause, and get remaining time
    Problem reported by Vladimir Panteleev in:
    https://lists.gnu.org/r/bug-gnulib/2020-02/msg00052.html
    * lib/xnanosleep.c: Include intprops.h, unistd.h.
    (xnanosleep) [HAVE_PAUSE]: Prefer pause when sleeping infinitely.
    (xnanosleep): Obtain remaining time when nanosleep is interrupted.
    * m4/xnanosleep.m4 (gl_XNANOSLEEP): Check for 'pause'.
    * modules/xnanosleep (Depends-on): Add intprops, unistd.

--- lib/xnanosleep.c
+++ lib/xnanosleep.c
@@ -22,10 +22,12 @@
 
 #include "xnanosleep.h"
 
+#include <intprops.h>
 #include <timespec.h>
 
 #include <errno.h>
 #include <time.h>
+#include <unistd.h>
 
 /* Sleep until the time (call it WAKE_UP_TIME) specified as
    SECONDS seconds after the time this function is called.
@@ -37,6 +39,17 @@
 int
 xnanosleep (double seconds)
 {
+#if HAVE_PAUSE
+  if (1.0 + TYPE_MAXIMUM (time_t) <= seconds)
+    {
+      do
+        pause ();
+      while (errno == EINTR);
+
+      /* pause failed (!); fall back on repeated nanosleep calls.  */
+    }
+#endif
+
   struct timespec ts_sleep = dtotimespec (seconds);
 
   for (;;)
@@ -46,9 +59,14 @@ xnanosleep (double seconds)
          set errno to EINTR.  nanosleep from linux-2.6.10, as well as
          implementations by (all?) other vendors, doesn't return -1
          in that case;  either it continues sleeping (if time remains)
-         or it returns zero (if the wake-up time has passed).  */
+         or it returns zero (if the wake-up time has passed).
+
+         Gnulib's replacement nanosleep sometimes does not update
+         TS_SLEEP, and it is possible some kernels have a similar bug.
+         However, this merely causes xnanosleep to sleep longer than
+         necessary, which is not a correctness bug.  */
       errno = 0;
-      if (nanosleep (&ts_sleep, NULL) == 0)
+      if (nanosleep (&ts_sleep, &ts_sleep) == 0)
         break;
       if (errno != EINTR && errno != 0)
         return -1;
