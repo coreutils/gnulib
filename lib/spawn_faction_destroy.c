5d0516ca8513f9346fb6a0ded2ba1569fe0f806a
Bruno Haible <bruno@clisp.org>
2020-04-28 12:29:04 +0200

    posix_spawn_file_actions_destroy: Fix a crash (bug from 2019-06-10).
    * lib/spawn_faction_destroy.c (posix_spawn_file_actions_destroy): Don't
    access elements of the wrong union member.

--- lib/spawn_faction_destroy.c
+++ lib/spawn_faction_destroy.c
@@ -42,8 +42,10 @@ posix_spawn_file_actions_destroy (posix_spawn_file_actions_t *file_actions)
       switch (sa->tag)
         {
         case spawn_do_open:
-        case spawn_do_chdir:
           free (sa->action.open_action.path);
+          break;
+        case spawn_do_chdir:
+          free (sa->action.chdir_action.path);
           break;
         default:
           /* No cleanup required.  */
