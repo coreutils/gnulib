21ccf3c7f2319147fcb2e62aa423c33a6603e875
Bruno Haible <bruno@clisp.org>
2020-06-27 03:14:43 +0200

    tmpfile: Fix a typo.
    * lib/tmpfile.c: Fix a typo.

--- lib/tmpfile.c
+++ lib/tmpfile.c
@@ -25,7 +25,7 @@
 #include <stdbool.h>
 
 #if defined _WIN32 && ! defined __CYGWIN__
-/* A native Windows platforms.  */
+/* A native Windows platform.  */
 
 # include <fcntl.h>
 # include <string.h>
@@ -50,7 +50,7 @@
    used on native Windows and Android.  */
 
 #if defined _WIN32 && ! defined __CYGWIN__
-/* A native Windows platforms.  */
+/* A native Windows platform.  */
 
 /* Don't assume that UNICODE is not defined.  */
 # undef OSVERSIONINFO
