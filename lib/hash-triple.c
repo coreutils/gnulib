8e867216109f764c195790a53f67bf1bc925b27f
Bruno Haible <bruno@clisp.org>
2020-07-08 02:07:38 +0200

    canonicalize: Trim module dependencies.
    * lib/hash-triple.h: Group declarations.
    * lib/hash-triple-simple.c: New file, extracted from lib/hash-triple.c.
    * lib/hash-triple.c: Don't include <stdlib.h>, <string.h>, hash-pjw.h.
    (STREQ): Remove macro.
    (triple_hash, triple_compare_ino_str, triple_free): Remove functions.
    * modules/hash-triple-simple: New file, based on modules/hash-triple.
    * modules/hash-triple (Files): Remove lib/hash-triple.h.
    (Depends-on): Add hash-triple-simple. Remove hash-pjw.
    * modules/canonicalize (Depends-on): Remove hash-triple. Add
    hash-triple-simple.
    * modules/file-set (Depends-on): Likewise.

--- lib/hash-triple.c
+++ lib/hash-triple.c
@@ -18,27 +18,11 @@
 
 #include <config.h>
 
+/* Specification.  */
 #include "hash-triple.h"
 
-#include <stdlib.h>
-#include <string.h>
-
-#include "hash-pjw.h"
 #include "same.h"
 #include "same-inode.h"
-
-#define STREQ(a, b) (strcmp (a, b) == 0)
-
-/* Hash an F_triple, and *do* consider the file name.  */
-size_t
-triple_hash (void const *x, size_t table_size)
-{
-  struct F_triple const *p = x;
-  size_t tmp = hash_pjw (p->name, table_size);
-
-  /* Ignoring the device number here should be fine.  */
-  return (tmp ^ p->st_ino) % table_size;
-}
 
 /* Hash an F_triple, without considering the file name.  */
 size_t
@@ -57,21 +41,4 @@ triple_compare (void const *x, void const *y)
   struct F_triple const *a = x;
   struct F_triple const *b = y;
   return (SAME_INODE (*a, *b) && same_name (a->name, b->name)) ? true : false;
-}
-
-bool
-triple_compare_ino_str (void const *x, void const *y)
-{
-  struct F_triple const *a = x;
-  struct F_triple const *b = y;
-  return (SAME_INODE (*a, *b) && STREQ (a->name, b->name)) ? true : false;
-}
-
-/* Free an F_triple.  */
-void
-triple_free (void *x)
-{
-  struct F_triple *a = x;
-  free (a->name);
-  free (a);
 }
