c06bbace6ef996131ccd88df0afe679e4ecc078c
Paul Eggert <eggert@cs.ucla.edu>
2020-09-16 15:40:20 -0700

    fnmatch: adjust to match glibc fix
    This fixes handling of collating symbols in fnmatch (glibc bug 26620).
    This does not affect Gnulib; it merely keeps Gnulib and glibc
    closer together, to help with any eventual merge, by incorporating
    a recent glibc patch.  The patch and the following commentary is
    by Andreas Schwab.
    * lib/fnmatch_loop.c (FCT) [WIDE_CHAR_VERSION]: The variable idx
    contains the index into the extra array, whereas wextra points
    into the extra array at this index, containing the length of the
    following collating sequence in the wide character representation.

--- lib/fnmatch_loop.c
+++ lib/fnmatch_loop.c
@@ -549,7 +549,7 @@ FCT (const CHAR *pattern, const CHAR *string, const CHAR *string_end,
                                 /* Get the collation sequence value.  */
                                 is_seqval = true;
 # if WIDE_CHAR_VERSION
-                                cold = wextra[1 + wextra[idx]];
+                                cold = wextra[1 + wextra[0]];
 # else
                                 idx += 1 + extra[idx];
                                 /* Adjust for the alignment.  */
@@ -726,7 +726,7 @@ FCT (const CHAR *pattern, const CHAR *string, const CHAR *string_end,
                                     /* Get the collation sequence value.  */
                                     is_seqval = true;
 # if WIDE_CHAR_VERSION
-                                    cend = wextra[1 + wextra[idx]];
+                                    cend = wextra[1 + wextra[0]];
 # else
                                     idx += 1 + extra[idx];
                                     /* Adjust for the alignment.  */
