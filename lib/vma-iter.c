00e688fc22c7bfb0bba2bd8a7b2a7d22d21d31ef
Bruno Haible <bruno@clisp.org>
2020-12-01 19:57:17 +0100

    vma-iter: Add support for macOS11/arm64.
    Patch suggested by Hill Ma <maahiuzeon@gmail.com> in
    <https://gitlab.com/gnu-clisp/clisp/-/issues/27>
    and by Martin Storsjö <martin@martin.st> in
    <https://lists.gnu.org/archive/html/bug-gnulib/2020-12/msg00003.html>.
    
    * lib/vma-iter.c (vma_iterate): On arm64, use 64-bit type definitions.

--- lib/vma-iter.c
+++ lib/vma-iter.c
@@ -1333,7 +1333,7 @@ vma_iterate (vma_iterate_callback_fn callback, void *data)
          In 64-bit processes, we could use vm_region_64 or mach_vm_region.
          I choose vm_region_64 because it uses the same types as vm_region,
          resulting in less conditional code.  */
-# if defined __ppc64__ || defined __x86_64__
+# if defined __aarch64__ || defined __ppc64__ || defined __x86_64__
       struct vm_region_basic_info_64 info;
       mach_msg_type_number_t info_count = VM_REGION_BASIC_INFO_COUNT_64;
 
