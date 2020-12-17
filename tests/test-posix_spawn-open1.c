7860f4b9c1a17d00a77498f11deb2de866f108e8
Bruno Haible <bruno@clisp.org>
2020-12-17 02:47:40 +0100

    posix_spawn-tests: Rename test.
    * tests/test-posix_spawn-open1.c: Renamed from
    tests/test-posix_spawn3.c.
    * modules/posix_spawn-tests (Files, Makefile.am): Update.

--- tests/test-posix_spawn-open1.c
+++ tests/test-posix_spawn-open1.c
@@ -38,7 +38,7 @@ SIGNATURE_CHECK (posix_spawn, int, (pid_t *, char const *,
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define CHILD_PROGRAM_FILENAME "test-posix_spawn3"
+#define CHILD_PROGRAM_FILENAME "test-posix_spawn-open1"
 #define DATA_FILENAME "t!#$%&'()*+,-;=?@[\\]^_`{|}~.tmp"
 /* On Cygwin, '*' '?' '\\' '|' cannot be used in file names.  */
 #if defined __CYGWIN__
