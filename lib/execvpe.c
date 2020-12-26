a50484eb14bf2c2a8dc420ba2ad037fd85f9c739
Bruno Haible <bruno@clisp.org>
2020-12-26 14:23:10 +0100

    execvpe: New module.
    * lib/execvpe.c: New file.
    * m4/execvpe.m4: New file.
    * modules/execvpe: New file.
    * doc/glibc-functions/execvpe.texi: Mention the Windows problems and the
    new module.

---
+++ lib/execvpe.c
@@ -0,0 +1,62 @@
+/* execvpe() function: Execute a program, replacing the current process.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2020.  */
+
+/* Don't use __attribute__ __nonnull__ in this compilation unit.  Otherwise gcc
+   may optimize away the program == NULL and argv == NULL tests below.  */
+#define _GL_ARG_NONNULL(params)
+
+#include <config.h>
+
+/* Specification.  */
+#include <unistd.h>
+
+#include <errno.h>
+#include <stdlib.h>
+
+#include "findprog.h"
+
+int
+execvpe (const char *program, char * const *argv, char * const *env)
+{
+  if (program == NULL
+      || argv == NULL
+      /* The callee is not expecting a NULL argv[0].  */
+      || argv[0] == NULL
+      || env == NULL)
+    {
+      errno = EINVAL;
+      return -1;
+    }
+
+  const char *resolved_progname =
+    find_in_given_path (program, getenv ("PATH"), NULL, true);
+  if (resolved_progname == NULL)
+    return -1;
+
+  /* Invoke execve.  */
+  execve (resolved_progname, argv, env);
+
+  /* If execve returned, it must have failed.  */
+  if (resolved_progname != program)
+    {
+      int saved_errno = errno;
+      free ((char *) resolved_progname);
+      errno = saved_errno;
+    }
+  return -1;
+}
---
+++ m4/execvpe.m4
@@ -0,0 +1,24 @@
+# execvpe.m4 serial 1
+dnl Copyright (C) 2020 Free Software Foundation, Inc.
+dnl This file is free software; the Free Software Foundation
+dnl gives unlimited permission to copy and/or distribute it,
+dnl with or without modifications, as long as this notice is preserved.
+
+AC_DEFUN([gl_FUNC_EXECVPE],
+[
+  AC_REQUIRE([gl_UNISTD_H_DEFAULTS])
+  AC_REQUIRE([AC_CANONICAL_HOST])
+
+  dnl Persuade glibc <unistd.h> to declare execvpe().
+  AC_REQUIRE([AC_USE_SYSTEM_EXTENSIONS])
+
+  case "$host_os" in
+    mingw*) REPLACE_EXECVPE=1 ;;
+    *)
+      AC_CHECK_FUNCS([execvpe])
+      if test $ac_cv_func_execvpe != yes; then
+        HAVE_EXECVPE=0
+      fi
+      ;;
+  esac
+])
