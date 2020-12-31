ee523efd38e33c3d7acb77ea8c5941c88d54a8cb
Bruno Haible <bruno@clisp.org>
2020-12-31 22:16:52 +0100

    malloc-h: New module.
    * lib/malloc.in.h: New file.
    * m4/malloc_h.m4: New file.
    * modules/malloc-h: New file.
    * doc/glibc-headers/malloc.texi: New file.
    * doc/gnulib.texi (Glibc Header File Substitutes): Include it.

---
+++ lib/malloc.in.h
@@ -0,0 +1,69 @@
+/* Substitute for and wrapper around <malloc.h>.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <https://www.gnu.org/licenses/>.  */
+
+#ifndef _@GUARD_PREFIX@_MALLOC_H
+
+#if __GNUC__ >= 3
+@PRAGMA_SYSTEM_HEADER@
+#endif
+@PRAGMA_COLUMNS@
+
+/* The include_next requires a split double-inclusion guard.  */
+#if @HAVE_MALLOC_H@
+# @INCLUDE_NEXT@ @NEXT_MALLOC_H@
+#endif
+
+#ifndef _@GUARD_PREFIX@_MALLOC_H
+#define _@GUARD_PREFIX@_MALLOC_H
+
+/* Solaris declares memalign() in <stdlib.h>, not in <malloc.h>.
+   Also get size_t.  */
+#include <stdlib.h>
+
+/* The definitions of _GL_FUNCDECL_RPL etc. are copied here.  */
+
+/* The definition of _GL_WARN_ON_USE is copied here.  */
+
+
+/* Declare overridden functions.  */
+
+#if @GNULIB_MEMALIGN@
+# if @REPLACE_MEMALIGN@
+#  if !(defined __cplusplus && defined GNULIB_NAMESPACE)
+#   undef memalign
+#   define memalign rpl_memalign
+#  endif
+_GL_FUNCDECL_RPL (memalign, void *, (size_t alignment, size_t size));
+_GL_CXXALIAS_RPL (memalign, void *, (size_t alignment, size_t size));
+# else
+#  if @HAVE_MEMALIGN@
+_GL_CXXALIAS_SYS (memalign, void *, (size_t alignment, size_t size));
+#  endif
+# endif
+# if @HAVE_MEMALIGN@
+_GL_CXXALIASWARN (memalign);
+# endif
+#elif defined GNULIB_POSIXCHECK
+# undef memalign
+# if HAVE_RAW_DECL_MEMALIGN
+_GL_WARN_ON_USE (memalign, "memalign is not portable - "
+                 "use gnulib module memalign for portability");
+# endif
+#endif
+
+
+#endif /* _@GUARD_PREFIX@_MALLOC_H */
+#endif /* _@GUARD_PREFIX@_MALLOC_H */
---
+++ m4/malloc_h.m4
@@ -0,0 +1,47 @@
+# malloc_h.m4 serial 1
+dnl Copyright (C) 2020 Free Software Foundation, Inc.
+dnl This file is free software; the Free Software Foundation
+dnl gives unlimited permission to copy and/or distribute it,
+dnl with or without modifications, as long as this notice is preserved.
+
+dnl Written by Bruno Haible.
+
+AC_DEFUN([gl_MALLOC_H],
+[
+  dnl Use AC_REQUIRE here, so that the default behavior below is expanded
+  dnl once only, before all statements that occur in other macros.
+  AC_REQUIRE([gl_MALLOC_H_DEFAULTS])
+
+  gl_CHECK_NEXT_HEADERS([malloc.h])
+  if test $ac_cv_header_malloc_h = yes; then
+    HAVE_MALLOC_H=1
+  else
+    HAVE_MALLOC_H=0
+  fi
+  AC_SUBST([HAVE_MALLOC_H])
+
+  dnl Check for declarations of anything we want to poison if the
+  dnl corresponding gnulib module is not in use.
+  gl_WARN_ON_USE_PREPARE([[
+#if HAVE_MALLOC_H
+# include <malloc.h>
+#endif
+    ]], [memalign])
+])
+
+AC_DEFUN([gl_MALLOC_MODULE_INDICATOR],
+[
+  dnl Use AC_REQUIRE here, so that the default settings are expanded once only.
+  AC_REQUIRE([gl_MALLOC_H_DEFAULTS])
+  gl_MODULE_INDICATOR_SET_VARIABLE([$1])
+  dnl Define it also as a C macro, for the benefit of the unit tests.
+  gl_MODULE_INDICATOR_FOR_TESTS([$1])
+])
+
+AC_DEFUN([gl_MALLOC_H_DEFAULTS],
+[
+  GNULIB_MEMALIGN=0;      AC_SUBST([GNULIB_MEMALIGN])
+  dnl Assume proper GNU behavior unless another module says otherwise.
+  HAVE_MEMALIGN=1;        AC_SUBST([HAVE_MEMALIGN])
+  REPLACE_MEMALIGN=0;     AC_SUBST([REPLACE_MEMALIGN])
+])
