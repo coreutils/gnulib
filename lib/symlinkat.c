3d1894d8dd3f33aeda81d674c2a1f19fded60085
Bruno Haible <bruno@clisp.org>
2021-01-05 05:37:50 +0100

    symlinkat: Fix trailing slash handling.
    * lib/symlinkat.c (rpl_symlinkat): Don't follow symlinks during the
    trailing slash handling.
    * modules/symlinkat (Depends-on): Update conditions.

--- lib/symlinkat.c
+++ lib/symlinkat.c
@@ -27,6 +27,7 @@
 #if HAVE_SYMLINKAT
 # undef symlinkat
 
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <string.h>
 
@@ -38,7 +39,8 @@ rpl_symlinkat (char const *contents, int fd, char const *name)
   if (len && name[len - 1] == '/')
     {
       struct stat st;
-      if (fstatat (fd, name, &st, 0) == 0 || errno == EOVERFLOW)
+      if (fstatat (fd, name, &st, AT_SYMLINK_NOFOLLOW) == 0
+          || errno == EOVERFLOW)
         errno = EEXIST;
       return -1;
     }
