0b38e1d69f03d3977d7ae7926c1efeb461a8a971
Bruno Haible <bruno@clisp.org>
2021-02-08 23:18:34 +0100

    stddef: Fix test-stddef compilation error on MidnightBSD/x86.
    * lib/stddef.in.h (_GL_STDDEF_ALIGNAS, rpl_max_align_t, max_align_t):
    Don't ignore HAVE_MAX_ALIGN_T if the compiler is clang.

--- lib/stddef.in.h
+++ lib/stddef.in.h
@@ -109,7 +109,7 @@ typedef long max_align_t;
     && defined __cplusplus
 # include <cstddef>
 #else
-# if ! (@HAVE_MAX_ALIGN_T@ || defined _GCC_MAX_ALIGN_T)
+# if ! (@HAVE_MAX_ALIGN_T@ || (defined _GCC_MAX_ALIGN_T && !defined __clang__))
 #  if !GNULIB_defined_max_align_t
 /* On the x86, the maximum storage alignment of double, long, etc. is 4,
    but GCC's C11 ABI for x86 says that max_align_t has an alignment of 8,
