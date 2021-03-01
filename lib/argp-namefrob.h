3d15181e1eb6db8e4471c2dcee6104b51d11b8b2
Paul Eggert <eggert@cs.ucla.edu>
2021-03-01 00:27:25 -0800

    unlocked-io: do not redefine getc_unlocked etc.
    I ran into this problem on AIX 7.1 with GNU Tar, which
    enables visibility of getc_unlocked etc. after testing
    whether they’re declared, causing argp-namefrob.h and
    unlocked-io.h to redefine the macros.
    * lib/argp-namefrob.h, lib/unlocked-io.h:
    (clearerr_unlocked, feof_unlocked ferror_unlocked)
    (fflush_unlocked, fgets_unlocked, fputc_unlocked, fputs_unlocked)
    (fread_unlocked, fwrite_unlocked, getc_unlocked, getchar_unlocked)
    (putc_unlocked, putchar_unlocked): Do not define if already defined.

--- lib/argp-namefrob.h
+++ lib/argp-namefrob.h
@@ -95,43 +95,56 @@
 #undef __vsnprintf
 #define __vsnprintf vsnprintf
 
-#if defined(HAVE_DECL_CLEARERR_UNLOCKED) && !HAVE_DECL_CLEARERR_UNLOCKED
+#if (defined HAVE_DECL_CLEARERR_UNLOCKED  && !HAVE_DECL_CLEARERR_UNLOCKED \
+     && !defined clearerr_unlocked)
 # define clearerr_unlocked(x) clearerr (x)
 #endif
-#if defined(HAVE_DECL_FEOF_UNLOCKED) && !HAVE_DECL_FEOF_UNLOCKED
+#if (defined HAVE_DECL_FEOF_UNLOCKED && !HAVE_DECL_FEOF_UNLOCKED \
+     && !defined feof_unlocked)
 # define feof_unlocked(x) feof (x)
 #endif
-#if defined(HAVE_DECL_FERROR_UNLOCKED) && !HAVE_DECL_FERROR_UNLOCKED
+#if (defined HAVE_DECL_FERROR_UNLOCKED && !HAVE_DECL_FERROR_UNLOCKED \
+     && !defined ferror_unlocked)
 # define ferror_unlocked(x) ferror (x)
 #endif
-#if defined(HAVE_DECL_FFLUSH_UNLOCKED) && !HAVE_DECL_FFLUSH_UNLOCKED
+#if (defined HAVE_DECL_FFLUSH_UNLOCKED && !HAVE_DECL_FFLUSH_UNLOCKED \
+     && !defined fflush_unlocked)
 # define fflush_unlocked(x) fflush (x)
 #endif
-#if defined(HAVE_DECL_FGETS_UNLOCKED) && !HAVE_DECL_FGETS_UNLOCKED
+#if (defined HAVE_DECL_FGETS_UNLOCKED && !HAVE_DECL_FGETS_UNLOCKED \
+     && !defined fgets_unlocked)
 # define fgets_unlocked(x,y,z) fgets (x,y,z)
 #endif
-#if defined(HAVE_DECL_FPUTC_UNLOCKED) && !HAVE_DECL_FPUTC_UNLOCKED
+#if (defined HAVE_DECL_FPUTC_UNLOCKED && !HAVE_DECL_FPUTC_UNLOCKED \
+     && !defined fputc_unlocked)
 # define fputc_unlocked(x,y) fputc (x,y)
 #endif
-#if defined(HAVE_DECL_FPUTS_UNLOCKED) && !HAVE_DECL_FPUTS_UNLOCKED
+#if (defined HAVE_DECL_FPUTS_UNLOCKED && !HAVE_DECL_FPUTS_UNLOCKED \
+     && !defined fputs_unlocked)
 # define fputs_unlocked(x,y) fputs (x,y)
 #endif
-#if defined(HAVE_DECL_FREAD_UNLOCKED) && !HAVE_DECL_FREAD_UNLOCKED
+#if (defined HAVE_DECL_FREAD_UNLOCKED && !HAVE_DECL_FREAD_UNLOCKED \
+     && !defined fread_unlocked)
 # define fread_unlocked(w,x,y,z) fread (w,x,y,z)
 #endif
-#if defined(HAVE_DECL_FWRITE_UNLOCKED) && !HAVE_DECL_FWRITE_UNLOCKED
+#if (defined HAVE_DECL_FWRITE_UNLOCKED && !HAVE_DECL_FWRITE_UNLOCKED \
+     && !defined fwrite_unlocked)
 # define fwrite_unlocked(w,x,y,z) fwrite (w,x,y,z)
 #endif
-#if defined(HAVE_DECL_GETC_UNLOCKED) && !HAVE_DECL_GETC_UNLOCKED
+#if (defined HAVE_DECL_GETC_UNLOCKED && !HAVE_DECL_GETC_UNLOCKED \
+     && !defined getc_unlocked)
 # define getc_unlocked(x) getc (x)
 #endif
-#if defined(HAVE_DECL_GETCHAR_UNLOCKED) && !HAVE_DECL_GETCHAR_UNLOCKED
-#  define getchar_unlocked() getchar ()
+#if (defined HAVE_DECL_GETCHAR_UNLOCKED && !HAVE_DECL_GETCHAR_UNLOCKED \
+     && !defined getchar_unlocked)
+# define getchar_unlocked() getchar ()
 #endif
-#if defined(HAVE_DECL_PUTC_UNLOCKED) && !HAVE_DECL_PUTC_UNLOCKED
+#if (defined HAVE_DECL_PUTC_UNLOCKED && !HAVE_DECL_PUTC_UNLOCKED \
+     && !defined putc_unlocked)
 # define putc_unlocked(x,y) putc (x,y)
 #endif
-#if defined(HAVE_DECL_PUTCHAR_UNLOCKED) && !HAVE_DECL_PUTCHAR_UNLOCKED
+#if (defined HAVE_DECL_PUTCHAR_UNLOCKED && !HAVE_DECL_PUTCHAR_UNLOCKED \
+     && !defined putchar_unlocked)
 # define putchar_unlocked(x) putchar (x)
 #endif
 
