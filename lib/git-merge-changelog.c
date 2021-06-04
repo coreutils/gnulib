555bd9ca6484b9ddc28311c57fc783eb5e84185c
Bruno Haible <bruno@clisp.org>
2021-06-04 22:13:43 +0200

    git-merge-changelog: Fix license.
    * modules/git-merge-changelog (License): Change back to GPL.
    * lib/git-merge-changelog.c: Use a GPLv3+ notice.

--- lib/git-merge-changelog.c
+++ lib/git-merge-changelog.c
@@ -1,12 +1,12 @@
 /* git-merge-changelog - git "merge" driver for GNU style ChangeLog files.
    Copyright (C) 2008-2021 Bruno Haible <bruno@clisp.org>
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+   This file is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 3 of the License,
+   or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
+   This file is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
