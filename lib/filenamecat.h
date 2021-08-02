b279b399553d273317a7faa59c3ebbfba0c353cd
Paul Eggert <eggert@cs.ucla.edu>
2021-08-01 17:31:38 -0700

    filenamecat-lgpl: improve -fanalyzer malloc checking

--- lib/filenamecat.h
+++ lib/filenamecat.h
@@ -18,10 +18,15 @@
 
 /* Written by Jim Meyering.  */
 
+#include <stdlib.h>
+
 #if GNULIB_FILENAMECAT
 char *file_name_concat (char const *dir, char const *base,
-                        char **base_in_result);
+                        char **base_in_result)
+  _GL_ATTRIBUTE_MALLOC _GL_ATTRIBUTE_DEALLOC_FREE
+  _GL_ATTRIBUTE_RETURNS_NONNULL;
 #endif
 
 char *mfile_name_concat (char const *dir, char const *base,
-                         char **base_in_result);
+                         char **base_in_result)
+  _GL_ATTRIBUTE_MALLOC _GL_ATTRIBUTE_DEALLOC_FREE;
