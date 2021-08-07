ae2745385e6f56a6f486b4846a4b64bb40af128d
Bruno Haible <bruno@clisp.org>
2021-08-07 19:29:57 +0200

    di-set: Improve GCC 11 allocation-deallocation checking.
    * lib/di-set.h (di_set_free): Move declaration up.
    (di_set_alloc): Declare that deallocation must happen through
    'di_set_free'.

--- lib/di-set.h
+++ lib/di-set.h
@@ -21,9 +21,15 @@
 
 # include <sys/types.h>
 
-struct di_set *di_set_alloc (void);
-int di_set_insert (struct di_set *, dev_t, ino_t) _GL_ATTRIBUTE_NONNULL ((1));
+struct di_set;
+
 void di_set_free (struct di_set *) _GL_ATTRIBUTE_NONNULL ((1));
+
+struct di_set *di_set_alloc (void)
+  _GL_ATTRIBUTE_MALLOC _GL_ATTRIBUTE_DEALLOC (di_set_free, 1);
+
+int di_set_insert (struct di_set *, dev_t, ino_t) _GL_ATTRIBUTE_NONNULL ((1));
+
 int di_set_lookup (struct di_set *dis, dev_t dev, ino_t ino)
   _GL_ATTRIBUTE_NONNULL ((1));
 
