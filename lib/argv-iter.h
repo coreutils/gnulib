3f14ad2a151664a660916ab0d08adb1f6011db62
Bruno Haible <bruno@clisp.org>
2021-08-07 19:05:31 +0200

    argv-iter: Improve GCC 11 allocation-deallocation checking.
    * lib/argv-iter.h (argv_iter_free): Move declaration up.
    (argv_iter_init_argv, argv_iter_init_stream): Declare that deallocation
    must happen through 'argv_iter_free'.

--- lib/argv-iter.h
+++ lib/argv-iter.h
@@ -30,13 +30,14 @@ enum argv_iter_err
   AI_ERR_READ
 };
 
+void argv_iter_free (struct argv_iterator *)
+  _GL_ARG_NONNULL ((1));
+
 struct argv_iterator *argv_iter_init_argv (char **argv)
-  _GL_ARG_NONNULL ((1));
+  _GL_ARG_NONNULL ((1)) _GL_ATTRIBUTE_DEALLOC (argv_iter_free, 1);
 struct argv_iterator *argv_iter_init_stream (FILE *fp)
-  _GL_ARG_NONNULL ((1));
+  _GL_ARG_NONNULL ((1)) _GL_ATTRIBUTE_DEALLOC (argv_iter_free, 1);
 char *argv_iter (struct argv_iterator *, enum argv_iter_err *)
   _GL_ARG_NONNULL ((1, 2));
 size_t argv_iter_n_args (struct argv_iterator const *)
   _GL_ATTRIBUTE_PURE _GL_ARG_NONNULL ((1));
-void argv_iter_free (struct argv_iterator *)
-  _GL_ARG_NONNULL ((1));
