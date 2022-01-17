67e02aaea40a04eed6ed7abcfdf12d89e2793f77
Paul Eggert <eggert@cs.ucla.edu>
2022-01-16 22:31:02 -0800

    xstrtoll-tests: use %lld for long long
    * tests/test-xstrtoll.c, tests/test-xstrtoull.c (__spec):
    Do not assume long long is 64 bits, or that exact-width
    64-bit types exist.  Although these assumptions are true
    on current Gnulib platforms they are not true in general.
    Also, GCC warns if PRId64 is "ld" but intmax_t is long long
    even when long and long long are both 64 bits.

--- tests/test-xstrtoll.c
+++ tests/test-xstrtoll.c
@@ -1,4 +1,4 @@
 #define __xstrtol xstrtoll
 #define __strtol_t long long int
-#define __spec PRId64
+#define __spec "lld"
 #include "test-xstrtol.c"
--- tests/test-xstrtoull.c
+++ tests/test-xstrtoull.c
@@ -1,4 +1,4 @@
 #define __xstrtol xstrtoull
 #define __strtol_t unsigned long long int
-#define __spec PRIu64
+#define __spec "llu"
 #include "test-xstrtol.c"
