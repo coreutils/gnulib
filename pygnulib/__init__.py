3e44e3bb0c1a91faa5b31b257c9f32fb752f8c2d
Bruno Haible <bruno@clisp.org>
2022-07-29 16:41:28 +0200

    gnulib-tool.py: Modernize the file headers.
    * pygnulib/*.py: Remove '#!/usr/bin/python' (not needed) and
    'encoding: UTF-8' lines (default in Python 3). Add copyright notice.

--- pygnulib/__init__.py
+++ pygnulib/__init__.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2002-2022 Free Software Foundation, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 '''Gnulib - The GNU Portability Library
 GNU software has a well-deserved reputation for running on many different types
 of systems. While our primary goal is to write software for the GNU system, many
@@ -15,6 +30,6 @@ coding standards, the GNU maintainer information, the GPL and other licenses (in
 Texinfo), assorted configuration scripts, and more. The goal is to provide all
 the common infrastructure needed by GNU packages.'''
 
-__copyright__ = '2012-2017 Free Software Foundation, Inc.'
+__copyright__ = '2012-2022 Free Software Foundation, Inc.'
 __author__ = 'Dmitriy Selyutin'
 __license__ = 'GNU GPLv3+'
