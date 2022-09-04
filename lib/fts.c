d41252ce5c8c65cc96d1a9162d7aaa69272c65ae
Bruno Haible <bruno@clisp.org>
2022-09-04 16:14:17 +0200

    fts: Fix compilation error with MSVC 14.
    * lib/fts.c (S_IFBLK): Add fallback definition.

--- lib/fts.c
+++ lib/fts.c
@@ -115,6 +115,9 @@ static char sccsid[] = "@(#)fts.c       8.6 (Berkeley) 8/14/94";
 # define DT_SOCK 7
 #endif
 
+#ifndef S_IFBLK
+# define S_IFBLK 0
+#endif
 #ifndef S_IFLNK
 # define S_IFLNK 0
 #endif
