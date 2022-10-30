37e0cb5e7e027dfcf2011d2f8a236d4ee0562f05
Paul Eggert <eggert@cs.ucla.edu>
2022-10-30 12:39:08 -0700

    thread: pacify gcc -Wbad-function-cast
    * lib/glthread/thread.h (gl_thread_self_pointer): With POSIX
    threads, cast the call to gl_thread_t before casting to void *.

--- lib/glthread/thread.h
+++ lib/glthread/thread.h
@@ -228,7 +228,7 @@ extern const gl_thread_t gl_null_thread;
 #  define gl_thread_self() \
      (pthread_in_use () ? pthread_self () : (pthread_t) 0)
 #  define gl_thread_self_pointer() \
-     (pthread_in_use () ? (void *) pthread_self () : NULL)
+     (pthread_in_use () ? (void *) (pthread_t) pthread_self () : NULL)
 # endif
 # define gl_thread_exit(RETVAL) \
     (void) (pthread_in_use () ? (pthread_exit (RETVAL), 0) : 0)
