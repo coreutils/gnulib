3ef8e9910ae8f0e5d63cff8f37e22bb5aa9109ae
Bruno Haible <bruno@clisp.org>
2023-03-08 10:41:00 +0100

    time-h: Renamed from time.
    * modules/time-h: Renamed from modules/time.
    * tests/test-time-h.c: Renamed from tests/test-time.c.
    * modules/time-h-tests: Renamed from modules/time-tests. Update.
    * tests/test-time-h-c++.cc: Renamed from tests/test-time-c++.cc.
    * tests/test-time-h-c++2.cc: Renamed from tests/test-time-c++2.cc.
    * modules/time-h-c++-tests: Renamed from modules/time-c++-tests. Update.
    * modules/cond (Depends-on): Update.
    * modules/ctime (Depends-on): Likewise.
    * modules/localtime (Depends-on): Likewise.
    * modules/mktime (Depends-on): Likewise.
    * modules/nanosleep (Depends-on): Likewise.
    * modules/parse-datetime (Depends-on): Likewise.
    * modules/posixcheck (Depends-on): Likewise.
    * modules/pthread-h (Depends-on): Likewise.
    * modules/stat-time (Depends-on): Likewise.
    * modules/stat-time-tests (Depends-on): Likewise.
    * modules/strftime-fixes (Depends-on): Likewise.
    * modules/strptime (Depends-on): Likewise.
    * modules/sys_stat (Depends-on): Likewise.
    * modules/tempname (Depends-on): Likewise.
    * modules/threads-h (Depends-on): Likewise.
    * modules/time_r (Depends-on): Likewise.
    * modules/time_rz (Depends-on): Likewise.
    * modules/timegm (Depends-on): Likewise.
    * modules/timespec (Depends-on): Likewise.
    * modules/timespec_get (Depends-on): Likewise.
    * modules/timespec_getres (Depends-on): Likewise.
    * modules/tzset (Depends-on): Likewise.
    * modules/usleep-tests (Depends-on): Likewise.
    * modules/utime (Depends-on): Likewise.
    * modules/utimecmp (Depends-on): Likewise.
    * modules/utimens (Depends-on): Likewise.
    * modules/windows-cond (Depends-on): Likewise.
    * modules/windows-timedmutex (Depends-on): Likewise.
    * modules/windows-timedrecmutex (Depends-on): Likewise.
    * modules/windows-timedrwlock (Depends-on): Likewise.
    * modules/xnanosleep (Depends-on): Likewise.
    * doc/posix-headers/time.texi: Update.
    * NEWS: Mention the change.

---
+++ tests/test-time-h-c++.cc
@@ -0,0 +1,90 @@
+/* Test of <time.h> substitute in C++ mode.
+   Copyright (C) 2010-2023 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010.  */
+
+#define GNULIB_NAMESPACE gnulib
+#include <config.h>
+
+#include <time.h>
+
+#include "signature.h"
+
+
+#if GNULIB_TEST_TIMESPEC_GET
+SIGNATURE_CHECK (GNULIB_NAMESPACE::timespec_get, int, (struct timespec *, int));
+#endif
+
+#if GNULIB_TEST_NANOSLEEP
+SIGNATURE_CHECK (GNULIB_NAMESPACE::nanosleep, int,
+                 (struct timespec const *, struct timespec *));
+#endif
+
+#if GNULIB_TEST_TZSET
+SIGNATURE_CHECK (GNULIB_NAMESPACE::tzset, void, (void));
+#endif
+
+#if GNULIB_TEST_MKTIME
+SIGNATURE_CHECK (GNULIB_NAMESPACE::mktime, time_t, (struct tm *));
+#endif
+
+#if GNULIB_TEST_TIME_R
+SIGNATURE_CHECK (GNULIB_NAMESPACE::localtime_r, struct tm *,
+                 (time_t const *, struct tm *));
+
+SIGNATURE_CHECK (GNULIB_NAMESPACE::gmtime_r, struct tm *,
+                 (time_t const *, struct tm *));
+#endif
+
+#if GNULIB_TEST_LOCALTIME
+SIGNATURE_CHECK (GNULIB_NAMESPACE::localtime, struct tm *, (time_t const *));
+#endif
+
+#if 0
+SIGNATURE_CHECK (GNULIB_NAMESPACE::gmtime, struct tm *, (time_t const *));
+#endif
+
+#if GNULIB_TEST_STRPTIME
+SIGNATURE_CHECK (GNULIB_NAMESPACE::strptime, char *,
+                 (char const *, char const *, struct tm *));
+#endif
+
+#if GNULIB_TEST_CTIME
+SIGNATURE_CHECK (GNULIB_NAMESPACE::ctime, char *, (time_t const *));
+#endif
+
+#if GNULIB_TEST_STRFTIME
+SIGNATURE_CHECK (GNULIB_NAMESPACE::strftime, size_t,
+                 (char *, size_t, const char *, const struct tm *));
+#endif
+
+#if GNULIB_TEST_TIMERZ
+SIGNATURE_CHECK (GNULIB_NAMESPACE::tzalloc, timezone_t, (char const *));
+SIGNATURE_CHECK (GNULIB_NAMESPACE::tzfree, void, (timezone_t));
+SIGNATURE_CHECK (GNULIB_NAMESPACE::localtime_rz, struct tm *,
+                 (timezone_t, time_t const *, struct tm *));
+SIGNATURE_CHECK (GNULIB_NAMESPACE::mktime_z, time_t, (timezone_t, struct tm *));
+#endif
+
+#if GNULIB_TEST_TIMEGM
+SIGNATURE_CHECK (GNULIB_NAMESPACE::timegm, time_t, (struct tm *));
+#endif
+
+
+int
+main ()
+{
+}
