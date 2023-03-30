a9447b51ea6d87507b1e853023c7adc428267ece
Bruno Haible <bruno@clisp.org>
2023-03-30 14:28:16 +0200

    string-desc tests: Fix "make distcheck" failure.
    * tests/test-string-desc.sh: Run the test in a subdir.

--- tests/test-string-desc.sh
+++ tests/test-string-desc.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
+. "${srcdir=.}/init.sh"; path_prepend_ .
 
-./test-string-desc${EXEEXT} > test-string-desc-1.tmp 3> test-string-desc-3.tmp || exit 1
+${CHECKER} test-string-desc${EXEEXT} > test-string-desc-1.tmp 3> test-string-desc-3.tmp || Exit 1
 
 printf 'Hello world!The\0quick\0brown\0\0fox\0' > test-string-desc.ok
 
 : "${DIFF=diff}"
-${DIFF} test-string-desc.ok test-string-desc-1.tmp || { echo "string_desc_fwrite KO" 1>&2; exit 1; }
-${DIFF} test-string-desc.ok test-string-desc-3.tmp || { echo "string_desc_write KO" 1>&2; exit 1; }
+${DIFF} test-string-desc.ok test-string-desc-1.tmp || { echo "string_desc_fwrite KO" 1>&2; Exit 1; }
+${DIFF} test-string-desc.ok test-string-desc-3.tmp || { echo "string_desc_write KO" 1>&2; Exit 1; }
 
-rm -f test-string-desc-1.tmp test-string-desc-3.tmp
-
-exit 0
+Exit 0
