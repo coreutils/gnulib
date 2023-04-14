e8bbebe5b5cc672aa28e5b4fb91e3624415d0e00
Bruno Haible <bruno@clisp.org>
2023-04-14 12:48:01 +0200

    sys_sem: New module.
    * lib/sys_sem.in.h: New file.
    * m4/sys_sem_h.m4: New file.
    * modules/sys_sem: New file.
    * doc/posix-headers/sys_sem.texi: Mention the new module.

---
+++ lib/sys_sem.in.h
@@ -0,0 +1,30 @@
+/* Provide a more reliable sys/sem.h.
+
+   Copyright (C) 2023 Free Software Foundation, Inc.
+
+   This file is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
+
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#if __GNUC__ >= 3
+@PRAGMA_SYSTEM_HEADER@
+#endif
+@PRAGMA_COLUMNS@
+
+/* This file uses #include_next of a system file that defines time_t.
+   For the 'year2038' module to work right, <config.h> needs to have been
+   included before.  */
+#if !_GL_CONFIG_H_INCLUDED
+ #error "Please include config.h first."
+#endif
+
+#@INCLUDE_NEXT@ @NEXT_SYS_SEM_H@
