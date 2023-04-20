60e3e2105814c20cd2cd3dd1f9d20f254e34d81b
Bruno Haible <bruno@clisp.org>
2023-04-20 19:03:27 +0200

    readtokens tests: Fix test failure on mingw.
    * tests/test-readtokens.sh: Transform CRLF to LF in the output, before
    comparing with the expected output.

--- tests/test-readtokens.sh
+++ tests/test-readtokens.sh
@@ -7,17 +7,20 @@ ${CHECKER} test-readtokens || fail=1
 
 # Simplest case.
 echo a:b:c: > exp || fail=1
-printf a:b:c | ${CHECKER} test-readtokens : > out 2>&1 || fail=1
+printf a:b:c | ${CHECKER} test-readtokens : > tmp 2>&1 || fail=1
+LC_ALL=C tr -d '\r' < tmp > out || fail=1
 compare exp out || fail=1
 
 # Use NUL as the delimiter.
 echo a:b:c: > exp || fail=1
-printf 'a\0b\0c' | ${CHECKER} test-readtokens '\0' > out 2>&1 || fail=1
+printf 'a\0b\0c' | ${CHECKER} test-readtokens '\0' > tmp 2>&1 || fail=1
+LC_ALL=C tr -d '\r' < tmp > out || fail=1
 compare exp out || fail=1
 
 # Two delimiter bytes, and adjacent delimiters in the input.
 echo a:b:c: > exp || fail=1
-printf a:-:b-:c:: | ${CHECKER} test-readtokens :- > out 2>&1 || fail=1
+printf a:-:b-:c:: | ${CHECKER} test-readtokens :- > tmp 2>&1 || fail=1
+LC_ALL=C tr -d '\r' < tmp > out || fail=1
 compare exp out || fail=1
 
 Exit $fail
