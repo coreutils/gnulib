0604bae99dacf1830e8e7f562059136d0b0eb501
Jim Meyering <meyering@meta.com>
2023-06-06 20:49:22 -0700

    maint.mk: regenerate regex to reflect new functions in xalloc.h
    * top/maint.mk (sc_prohibit_xalloc_without_use) [_xa1]:
    Regenerate using the command listed in the comment.

--- top/maint.mk
+++ top/maint.mk
@@ -2,7 +2,7 @@
 # This Makefile fragment tries to be general-purpose enough to be
 # used by many projects via the gnulib maintainer-makefile module.
 
-## Copyright (C) 2001-2022 Free Software Foundation, Inc.
+## Copyright (C) 2001-2023 Free Software Foundation, Inc.
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -614,7 +614,7 @@ sc_prohibit_error_without_use:
 # 2) notice that "c(har)?|[mz]" is equivalent to the shorter and more readable
 # "char|[cmz]"
 # x(((2n?)?re|char|n(re|m)|[cmz])alloc|alloc_(oversized|die)|(mem|str)dup)
-_xa1 = x(((2n?)?re|char|n(re|m)|[cmz])alloc|alloc_(oversized|die)|(mem|str)dup)
+_xa1 = x(i(m(emdup0?|alloc)|realloc(array)?|([cz]|nm)alloc)|([pz]|c(har)?|2n?re|nm)alloc|realloc(array)?|m(alloc|emdup)|strdup)
 _xa2 = X([CZ]|N?M)ALLOC
 sc_prohibit_xalloc_without_use:
 	@h='xalloc.h' \
