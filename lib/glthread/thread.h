f4015ac6ea3e8170b16b905dcc6a508a915434f2
Bruno Haible <bruno@clisp.org>
2023-06-25 20:18:41 +0200

    thread: Avoid compiler warnings on Solaris.
    * lib/glthread/thread.h: Include <stdint.h>.
    (gl_thread_self_pointer): Do the cast from pthread_t to 'void *' via
    intptr_t.
    * modules/thread (Depends-on): Add stdint.

--- lib/glthread/thread.h
+++ lib/glthread/thread.h
@@ -130,6 +130,9 @@ extern _Noreturn void gl_thread_exit (void *return_value);
 
 # include <pthread.h>
 
+/* Get intptr_t.  */
+# include <stdint.h>
+
 /* On IRIX, pthread_atfork is declared in <unistd.h>, not in <pthread.h>.  */
 # if defined __sgi
 #  include <unistd.h>
@@ -233,7 +236,7 @@ extern const gl_thread_t gl_null_thread;
 #  define gl_thread_self() \
      (pthread_in_use () ? pthread_self () : (pthread_t) 0)
 #  define gl_thread_self_pointer() \
-     (pthread_in_use () ? (void *) (pthread_t) pthread_self () : NULL)
+     (pthread_in_use () ? (void *) (intptr_t) (pthread_t) pthread_self () : NULL)
 # endif
 # define gl_thread_exit(RETVAL) \
     (void) (pthread_in_use () ? (pthread_exit (RETVAL), 0) : 0)
