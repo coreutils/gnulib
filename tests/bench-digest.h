117c429a235909864fb642b78ad276cc174fc842
Bruno Haible <bruno@clisp.org>
2023-07-11 21:43:27 +0200

    tests: Create new file bench.h.
    * tests/bench.h: New file, extracted from tests/bench-digest.h.
    * tests/bench-digest.h: Include it.
    * modules/crypto/md5-buffer-tests (Files): Add tests/bench.h.
    * modules/crypto/sha1-buffer-tests (Files): Likewise.
    * modules/crypto/sha256-buffer-tests (Files): Likewise.
    * modules/crypto/sha512-buffer-tests (Files): Likewise.

--- tests/bench-digest.h
+++ tests/bench-digest.h
@@ -16,58 +16,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/resource.h>
-#include <sys/time.h>
 
-struct timings_state
-{
-  /* Filled when the timings start.  */
-  struct timeval real_start;
-  struct timeval user_start;
-  struct timeval sys_start;
-  /* Filled when the timings end.  */
-  long real_usec;
-  long user_usec;
-  long sys_usec;
-};
-
-static void
-timing_start (struct timings_state *ts)
-{
-  struct rusage usage;
-
-  getrusage (RUSAGE_SELF, &usage);
-  ts->user_start = usage.ru_utime;
-  ts->sys_start = usage.ru_stime;
-
-  gettimeofday (&ts->real_start, NULL);
-}
-
-static void
-timing_end (struct timings_state *ts)
-{
-  struct timeval real_end;
-  struct rusage usage;
-
-  gettimeofday (&real_end, NULL);
-
-  getrusage (RUSAGE_SELF, &usage);
-
-  ts->real_usec = (real_end.tv_sec - ts->real_start.tv_sec) * 1000000
-                  + real_end.tv_usec - ts->real_start.tv_usec;
-  ts->user_usec = (usage.ru_utime.tv_sec - ts->user_start.tv_sec) * 1000000
-                  + usage.ru_utime.tv_usec - ts->user_start.tv_usec;
-  ts->sys_usec = (usage.ru_stime.tv_sec - ts->sys_start.tv_sec) * 1000000
-                 + usage.ru_stime.tv_usec - ts->sys_start.tv_usec;
-}
-
-static void
-timing_output (const struct timings_state *ts)
-{
-  printf ("real %10.6f\n", (double)ts->real_usec / 1000000.0);
-  printf ("user %7.3f\n", (double)ts->user_usec / 1000000.0);
-  printf ("sys  %7.3f\n", (double)ts->sys_usec / 1000000.0);
-}
+#include "bench.h"
 
 int
 main (int argc, char *argv[])
