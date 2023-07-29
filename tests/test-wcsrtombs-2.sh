1e209b30b4cbde8aa17a62639e62d4f36f61776b
Bruno Haible <bruno@clisp.org>
2023-07-30 00:56:31 +0200

    wcsrtombs tests: Renumber tests.
    * tests/test-wcsrtombs*.sh: Renamed 1 -> 2 -> 3 -> 4 -> 5.
    * tests/test-wcsrtombs.c: Update.
    * modules/wcsrtombs-tests: Update.

--- tests/test-wcsrtombs-2.sh
+++ tests/test-wcsrtombs-2.sh
@@ -12,4 +12,4 @@ if test $LOCALE_FR = none; then
 fi
 
 LC_ALL=$LOCALE_FR \
-${CHECKER} ./test-wcsrtombs${EXEEXT} 1
+${CHECKER} ./test-wcsrtombs${EXEEXT} 2
--- tests/test-wcsrtombs-5.sh
+++ tests/test-wcsrtombs-5.sh
@@ -12,4 +12,4 @@ if test $LOCALE_ZH_CN = none; then
 fi
 
 LC_ALL=$LOCALE_ZH_CN \
-${CHECKER} ./test-wcsrtombs${EXEEXT} 4
+${CHECKER} ./test-wcsrtombs${EXEEXT} 5
