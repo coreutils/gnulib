ef2d2d353fb083023616c838b3d67996d0ba4bb8
Bruno Haible <bruno@clisp.org>
2023-07-30 00:25:57 +0200

    mbsrtowcs tests: Renumber tests.
    * tests/test-mbsrtowcs*.sh: Renamed 1 -> 2 -> 3 -> 4 -> 5 -> 1.
    * tests/test-mbsrtowcs.c: Update.
    * modules/mbsrtowcs-tests: Update.

--- tests/test-mbsrtowcs-1.sh
+++ tests/test-mbsrtowcs-1.sh
@@ -2,8 +2,8 @@
 
 # Test whether the POSIX locale has encoding errors.
 LC_ALL=C \
-${CHECKER} ./test-mbsrtowcs${EXEEXT} 5 || exit 1
+${CHECKER} ./test-mbsrtowcs${EXEEXT} 1 || exit 1
 LC_ALL=POSIX \
-${CHECKER} ./test-mbsrtowcs${EXEEXT} 5 || exit 1
+${CHECKER} ./test-mbsrtowcs${EXEEXT} 1 || exit 1
 
 exit 0
