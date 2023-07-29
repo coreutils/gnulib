051b60503c0e63901c601f7901047938b4931915
Bruno Haible <bruno@clisp.org>
2023-07-29 21:39:02 +0200

    btowc tests: Renumber tests.
    * tests/test-btowc*.sh: Renamed 1 -> 2 -> 3 -> 1.
    * tests/test-btowc.c: Update.
    * modules/btowc-tests: Update.

--- tests/test-btowc-1.sh
+++ tests/test-btowc-1.sh
@@ -2,8 +2,8 @@
 
 # Test whether the POSIX locale has encoding errors.
 LC_ALL=C \
-${CHECKER} ./test-btowc${EXEEXT} 3 || exit 1
+${CHECKER} ./test-btowc${EXEEXT} 1 || exit 1
 LC_ALL=POSIX \
-${CHECKER} ./test-btowc${EXEEXT} 3 || exit 1
+${CHECKER} ./test-btowc${EXEEXT} 1 || exit 1
 
 exit 0
