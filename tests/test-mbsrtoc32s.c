860f1c47de8cf708358fbb0a1961c940ccd8083c
Bruno Haible <bruno@clisp.org>
2023-07-30 00:38:01 +0200

    mbsrtoc32s tests: Renumber tests.
    * tests/test-mbsrtoc32s-*.sh: Renamed 1 -> 2 -> 3 -> 4 -> 5 -> 1.
    * tests/test-mbsrtoc32s.c: Update.

--- tests/test-mbsrtoc32s.c
+++ tests/test-mbsrtoc32s.c
@@ -78,8 +78,8 @@ main (int argc, char *argv[])
      "C" locale.  Furthermore, when you attempt to set the "C" or "POSIX"
      locale via setlocale(), what you get is a "C" locale with UTF-8 encoding,
      that is, effectively the "C.UTF-8" locale.  */
-  if (argc > 1 && strcmp (argv[1], "5") == 0 && MB_CUR_MAX > 1)
-    argv[1] = "2";
+  if (argc > 1 && strcmp (argv[1], "1") == 0 && MB_CUR_MAX > 1)
+    argv[1] = "3";
 #endif
 
   if (argc > 1)
@@ -102,200 +102,6 @@ main (int argc, char *argv[])
           switch (argv[1][0])
             {
             case '1':
-              /* Locale encoding is ISO-8859-1 or ISO-8859-15.  */
-              {
-                char input[] = "B\374\337er"; /* "Büßer" */
-                memset (&state, '\0', sizeof (mbstate_t));
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input, 1, &state);
-                ASSERT (ret == 1);
-                ASSERT (wc == 'B');
-                ASSERT (mbsinit (&state));
-                input[0] = '\0';
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input + 1, 1, &state);
-                ASSERT (ret == 1);
-                ASSERT (c32tob (wc) == (unsigned char) '\374');
-                ASSERT (mbsinit (&state));
-                input[1] = '\0';
-
-                src = input + 2;
-                temp_state = state;
-                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 1, &temp_state);
-                ASSERT (ret == 3);
-                ASSERT (src == input + 2);
-                ASSERT (mbsinit (&state));
-
-                src = input + 2;
-                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 1, &state);
-                ASSERT (ret == (unlimited ? 3 : 1));
-                ASSERT (src == (unlimited ? NULL : input + 3));
-                ASSERT (c32tob (buf[0]) == (unsigned char) '\337');
-                if (unlimited)
-                  {
-                    ASSERT (buf[1] == 'e');
-                    ASSERT (buf[2] == 'r');
-                    ASSERT (buf[3] == 0);
-                    ASSERT (buf[4] == (char32_t) 0xBADFACE);
-                  }
-                else
-                  ASSERT (buf[1] == (char32_t) 0xBADFACE);
-                ASSERT (mbsinit (&state));
-              }
-              break;
-
-            case '2':
-              /* Locale encoding is UTF-8.  */
-              {
-                char input[] = "s\303\274\303\237\360\237\230\213!"; /* "süß😋!" */
-                memset (&state, '\0', sizeof (mbstate_t));
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input, 1, &state);
-                ASSERT (ret == 1);
-                ASSERT (wc == 's');
-                ASSERT (mbsinit (&state));
-                input[0] = '\0';
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input + 1, 1, &state);
-                ASSERT (ret == (size_t)(-2));
-                ASSERT (wc == (char32_t) 0xBADFACE);
-                ASSERT (!mbsinit (&state));
-                input[1] = '\0';
-
-                src = input + 2;
-                temp_state = state;
-                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 2, &temp_state);
-                ASSERT (ret == 4);
-                ASSERT (src == input + 2);
-                ASSERT (!mbsinit (&state));
-
-                src = input + 2;
-                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 2, &state);
-                ASSERT (ret == (unlimited ? 4 : 2));
-                ASSERT (src == (unlimited ? NULL : input + 5));
-                ASSERT (c32tob (buf[0]) == EOF);
-                ASSERT (c32tob (buf[1]) == EOF);
-                if (unlimited)
-                  {
-                    ASSERT (buf[2] == 0x1F60B); /* expect Unicode encoding */
-                    ASSERT (buf[3] == '!');
-                    ASSERT (buf[4] == 0);
-                    ASSERT (buf[5] == (char32_t) 0xBADFACE);
-                  }
-                else
-                  ASSERT (buf[2] == (char32_t) 0xBADFACE);
-                ASSERT (mbsinit (&state));
-              }
-              break;
-
-            case '3':
-              /* Locale encoding is EUC-JP.  */
-              {
-                char input[] = "<\306\374\313\334\270\354>"; /* "<日本語>" */
-                memset (&state, '\0', sizeof (mbstate_t));
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input, 1, &state);
-                ASSERT (ret == 1);
-                ASSERT (wc == '<');
-                ASSERT (mbsinit (&state));
-                input[0] = '\0';
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input + 1, 2, &state);
-                ASSERT (ret == 2);
-                ASSERT (c32tob (wc) == EOF);
-                ASSERT (mbsinit (&state));
-                input[1] = '\0';
-                input[2] = '\0';
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input + 3, 1, &state);
-                ASSERT (ret == (size_t)(-2));
-                ASSERT (wc == (char32_t) 0xBADFACE);
-                ASSERT (!mbsinit (&state));
-                input[3] = '\0';
-
-                src = input + 4;
-                temp_state = state;
-                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 2, &temp_state);
-                ASSERT (ret == 3);
-                ASSERT (src == input + 4);
-                ASSERT (!mbsinit (&state));
-
-                src = input + 4;
-                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 2, &state);
-                ASSERT (ret == (unlimited ? 3 : 2));
-                ASSERT (src == (unlimited ? NULL : input + 7));
-                ASSERT (c32tob (buf[0]) == EOF);
-                ASSERT (c32tob (buf[1]) == EOF);
-                if (unlimited)
-                  {
-                    ASSERT (buf[2] == '>');
-                    ASSERT (buf[3] == 0);
-                    ASSERT (buf[4] == (char32_t) 0xBADFACE);
-                  }
-                else
-                  ASSERT (buf[2] == (char32_t) 0xBADFACE);
-                ASSERT (mbsinit (&state));
-              }
-              break;
-
-            case '4':
-              /* Locale encoding is GB18030.  */
-              #if GL_CHAR32_T_IS_UNICODE && (defined __NetBSD__ || defined __sun)
-              fputs ("Skipping test: The GB18030 converter in this system's iconv is broken.\n", stderr);
-              return 77;
-              #endif
-              {
-                char input[] = "s\250\271\201\060\211\070\224\071\375\067!"; /* "süß😋!" */
-                memset (&state, '\0', sizeof (mbstate_t));
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input, 1, &state);
-                ASSERT (ret == 1);
-                ASSERT (wc == 's');
-                ASSERT (mbsinit (&state));
-                input[0] = '\0';
-
-                wc = (char32_t) 0xBADFACE;
-                ret = mbrtoc32 (&wc, input + 1, 1, &state);
-                ASSERT (ret == (size_t)(-2));
-                ASSERT (wc == (char32_t) 0xBADFACE);
-                ASSERT (!mbsinit (&state));
-                input[1] = '\0';
-
-                src = input + 2;
-                temp_state = state;
-                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 2, &temp_state);
-                ASSERT (ret == 4);
-                ASSERT (src == input + 2);
-                ASSERT (!mbsinit (&state));
-
-                src = input + 2;
-                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 2, &state);
-                ASSERT (ret == (unlimited ? 4 : 2));
-                ASSERT (src == (unlimited ? NULL : input + 7));
-                ASSERT (c32tob (buf[0]) == EOF);
-                ASSERT (c32tob (buf[1]) == EOF);
-                if (unlimited)
-                  {
-                    ASSERT (c32tob (buf[2]) == EOF);
-                    ASSERT (buf[3] == '!');
-                    ASSERT (buf[4] == 0);
-                    ASSERT (buf[5] == (char32_t) 0xBADFACE);
-                  }
-                else
-                  ASSERT (buf[2] == (char32_t) 0xBADFACE);
-                ASSERT (mbsinit (&state));
-              }
-              break;
-
-            case '5':
               /* C or POSIX locale.  */
               {
                 char input[] = "n/a";
@@ -360,6 +166,200 @@ main (int argc, char *argv[])
                         ASSERT (buf[0] == (btoc32 (c) == 0xDF00 + c ? btoc32 (c) : c));
                       ASSERT (mbsinit (&state));
                     }
+              }
+              break;
+
+            case '2':
+              /* Locale encoding is ISO-8859-1 or ISO-8859-15.  */
+              {
+                char input[] = "B\374\337er"; /* "Büßer" */
+                memset (&state, '\0', sizeof (mbstate_t));
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input, 1, &state);
+                ASSERT (ret == 1);
+                ASSERT (wc == 'B');
+                ASSERT (mbsinit (&state));
+                input[0] = '\0';
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input + 1, 1, &state);
+                ASSERT (ret == 1);
+                ASSERT (c32tob (wc) == (unsigned char) '\374');
+                ASSERT (mbsinit (&state));
+                input[1] = '\0';
+
+                src = input + 2;
+                temp_state = state;
+                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 1, &temp_state);
+                ASSERT (ret == 3);
+                ASSERT (src == input + 2);
+                ASSERT (mbsinit (&state));
+
+                src = input + 2;
+                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 1, &state);
+                ASSERT (ret == (unlimited ? 3 : 1));
+                ASSERT (src == (unlimited ? NULL : input + 3));
+                ASSERT (c32tob (buf[0]) == (unsigned char) '\337');
+                if (unlimited)
+                  {
+                    ASSERT (buf[1] == 'e');
+                    ASSERT (buf[2] == 'r');
+                    ASSERT (buf[3] == 0);
+                    ASSERT (buf[4] == (char32_t) 0xBADFACE);
+                  }
+                else
+                  ASSERT (buf[1] == (char32_t) 0xBADFACE);
+                ASSERT (mbsinit (&state));
+              }
+              break;
+
+            case '3':
+              /* Locale encoding is UTF-8.  */
+              {
+                char input[] = "s\303\274\303\237\360\237\230\213!"; /* "süß😋!" */
+                memset (&state, '\0', sizeof (mbstate_t));
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input, 1, &state);
+                ASSERT (ret == 1);
+                ASSERT (wc == 's');
+                ASSERT (mbsinit (&state));
+                input[0] = '\0';
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input + 1, 1, &state);
+                ASSERT (ret == (size_t)(-2));
+                ASSERT (wc == (char32_t) 0xBADFACE);
+                ASSERT (!mbsinit (&state));
+                input[1] = '\0';
+
+                src = input + 2;
+                temp_state = state;
+                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 2, &temp_state);
+                ASSERT (ret == 4);
+                ASSERT (src == input + 2);
+                ASSERT (!mbsinit (&state));
+
+                src = input + 2;
+                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 2, &state);
+                ASSERT (ret == (unlimited ? 4 : 2));
+                ASSERT (src == (unlimited ? NULL : input + 5));
+                ASSERT (c32tob (buf[0]) == EOF);
+                ASSERT (c32tob (buf[1]) == EOF);
+                if (unlimited)
+                  {
+                    ASSERT (buf[2] == 0x1F60B); /* expect Unicode encoding */
+                    ASSERT (buf[3] == '!');
+                    ASSERT (buf[4] == 0);
+                    ASSERT (buf[5] == (char32_t) 0xBADFACE);
+                  }
+                else
+                  ASSERT (buf[2] == (char32_t) 0xBADFACE);
+                ASSERT (mbsinit (&state));
+              }
+              break;
+
+            case '4':
+              /* Locale encoding is EUC-JP.  */
+              {
+                char input[] = "<\306\374\313\334\270\354>"; /* "<日本語>" */
+                memset (&state, '\0', sizeof (mbstate_t));
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input, 1, &state);
+                ASSERT (ret == 1);
+                ASSERT (wc == '<');
+                ASSERT (mbsinit (&state));
+                input[0] = '\0';
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input + 1, 2, &state);
+                ASSERT (ret == 2);
+                ASSERT (c32tob (wc) == EOF);
+                ASSERT (mbsinit (&state));
+                input[1] = '\0';
+                input[2] = '\0';
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input + 3, 1, &state);
+                ASSERT (ret == (size_t)(-2));
+                ASSERT (wc == (char32_t) 0xBADFACE);
+                ASSERT (!mbsinit (&state));
+                input[3] = '\0';
+
+                src = input + 4;
+                temp_state = state;
+                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 2, &temp_state);
+                ASSERT (ret == 3);
+                ASSERT (src == input + 4);
+                ASSERT (!mbsinit (&state));
+
+                src = input + 4;
+                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 2, &state);
+                ASSERT (ret == (unlimited ? 3 : 2));
+                ASSERT (src == (unlimited ? NULL : input + 7));
+                ASSERT (c32tob (buf[0]) == EOF);
+                ASSERT (c32tob (buf[1]) == EOF);
+                if (unlimited)
+                  {
+                    ASSERT (buf[2] == '>');
+                    ASSERT (buf[3] == 0);
+                    ASSERT (buf[4] == (char32_t) 0xBADFACE);
+                  }
+                else
+                  ASSERT (buf[2] == (char32_t) 0xBADFACE);
+                ASSERT (mbsinit (&state));
+              }
+              break;
+
+            case '5':
+              /* Locale encoding is GB18030.  */
+              #if GL_CHAR32_T_IS_UNICODE && (defined __NetBSD__ || defined __sun)
+              fputs ("Skipping test: The GB18030 converter in this system's iconv is broken.\n", stderr);
+              return 77;
+              #endif
+              {
+                char input[] = "s\250\271\201\060\211\070\224\071\375\067!"; /* "süß😋!" */
+                memset (&state, '\0', sizeof (mbstate_t));
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input, 1, &state);
+                ASSERT (ret == 1);
+                ASSERT (wc == 's');
+                ASSERT (mbsinit (&state));
+                input[0] = '\0';
+
+                wc = (char32_t) 0xBADFACE;
+                ret = mbrtoc32 (&wc, input + 1, 1, &state);
+                ASSERT (ret == (size_t)(-2));
+                ASSERT (wc == (char32_t) 0xBADFACE);
+                ASSERT (!mbsinit (&state));
+                input[1] = '\0';
+
+                src = input + 2;
+                temp_state = state;
+                ret = mbsrtoc32s (NULL, &src, unlimited ? BUFSIZE : 2, &temp_state);
+                ASSERT (ret == 4);
+                ASSERT (src == input + 2);
+                ASSERT (!mbsinit (&state));
+
+                src = input + 2;
+                ret = mbsrtoc32s (buf, &src, unlimited ? BUFSIZE : 2, &state);
+                ASSERT (ret == (unlimited ? 4 : 2));
+                ASSERT (src == (unlimited ? NULL : input + 7));
+                ASSERT (c32tob (buf[0]) == EOF);
+                ASSERT (c32tob (buf[1]) == EOF);
+                if (unlimited)
+                  {
+                    ASSERT (c32tob (buf[2]) == EOF);
+                    ASSERT (buf[3] == '!');
+                    ASSERT (buf[4] == 0);
+                    ASSERT (buf[5] == (char32_t) 0xBADFACE);
+                  }
+                else
+                  ASSERT (buf[2] == (char32_t) 0xBADFACE);
+                ASSERT (mbsinit (&state));
               }
               break;
 
