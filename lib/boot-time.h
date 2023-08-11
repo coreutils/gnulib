5cd9573bc638b433eae64331dc61188eae6e434e
Bruno Haible <bruno@clisp.org>
2023-08-11 23:31:37 +0200

    boot-time: Add comment about multithread-safety.
    * lib/boot-time.h (get_boot_time): Add comment, same as in readutmp.h.

--- lib/boot-time.h
+++ lib/boot-time.h
@@ -27,7 +27,13 @@ extern "C" {
 
 
 /* Store the approximate time when the machine last booted in *P_BOOT_TIME,
-   and return 0.  If it cannot be determined, return -1.  */
+   and return 0.  If it cannot be determined, return -1.
+
+   This function is not multithread-safe, since on many platforms it
+   invokes the functions setutxent, getutxent, endutxent.  These
+   functions are needed because they may lock FILE (so that we don't
+   read garbage when a concurrent process writes to FILE), but their
+   drawback is that they have a common global state.  */
 extern int get_boot_time (struct timespec *p_boot_time);
 
 
