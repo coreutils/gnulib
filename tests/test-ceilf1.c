a6dab67faf765089c8afae91a40ccce7cc816ef1
Bruno Haible <bruno@clisp.org>
2023-11-24 20:02:37 +0100

    floorf, ceilf tests: Strengthen against compiler optimizations.
    Reported by René Rebe <rene@t2-project.org>.
    
    * tests/test-floorf1.c (main): Mark my_floorf as 'volatile'.
    * tests/test-floorf-ieee.c (main): Likewise.
    * tests/test-ceilf1.c (main): Mark my_ceilf as 'volatile'.
    * tests/test-ceilf-ieee.c (main): Likewise.

--- tests/test-ceilf1.c
+++ tests/test-ceilf1.c
@@ -43,7 +43,7 @@ dummy (float f)
 int
 main (int argc, _GL_UNUSED char **argv)
 {
-  float (*my_ceilf) (float) = argc ? ceilf : dummy;
+  float (* volatile my_ceilf) (float) = argc ? ceilf : dummy;
 
   /* Zero.  */
   ASSERT (my_ceilf (0.0f) == 0.0f);
