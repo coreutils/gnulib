8f4b4e52c991de2233b471f8e35a068866b31f01
Bruno Haible <bruno@clisp.org>
2024-02-06 17:08:16 +0100

    nstrftime, fprintftime: Simplify.
    * lib/strftime.c: Renamed from lib/nstrftime.c.
    (my_strftime): Don't define here.
    * lib/nstrftime.c: New file.
    * lib/fprintftime.c (my_strftime): New macro.
    Include strftime.c, not nstrftime.c.
    * m4/nstrftime.m4 (gl_FUNC_GNU_STRFTIME): Don't define my_strftime in
    config.h.
    * modules/nstrftime (Files): Add lib/strftime.c.
    * modules/fprintftime (Files): Likewise.
    * config/srclist.txt: Update comment regarding glibc/time/strftime.c.

--- lib/fprintftime.c
+++ lib/fprintftime.c
@@ -15,5 +15,6 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
+#define my_strftime fprintftime
 #define FPRINTFTIME 1
-#include "nstrftime.c"
+#include "strftime.c"
