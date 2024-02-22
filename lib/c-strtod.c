1727cfbbca1765e4a64ab75d31d57dca382abb2e
Bruno Haible <bruno@clisp.org>
2024-02-22 01:49:44 +0100

    c-strtold: Use strtold_l when available (regr. 2019-01-31).
    * lib/c-strtod.c: Fix typo: HAVE_GOOD_STRTOLD_L was never defined.

--- lib/c-strtod.c
+++ lib/c-strtod.c
@@ -1,6 +1,6 @@
 /* Convert string to double, using the C locale.
 
-   Copyright (C) 2003-2004, 2006, 2009-2023 Free Software Foundation, Inc.
+   Copyright (C) 2003-2004, 2006, 2009-2024 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -40,9 +40,7 @@
 # define STRTOD strtod
 #endif
 
-#if defined LC_ALL_MASK \
-    && ((LONG ? HAVE_GOOD_STRTOLD_L : HAVE_GOOD_STRTOD_L) \
-        || HAVE_WORKING_USELOCALE)
+#if defined LC_ALL_MASK && (HAVE_GOOD_STRTOD_L || HAVE_WORKING_USELOCALE)
 
 /* Cache for the C locale object.
    Marked volatile so that different threads see the same value
@@ -66,9 +64,7 @@ C_STRTOD (char const *nptr, char **endptr)
 {
   DOUBLE r;
 
-#if defined LC_ALL_MASK \
-    && ((LONG ? HAVE_GOOD_STRTOLD_L : HAVE_GOOD_STRTOD_L) \
-        || HAVE_WORKING_USELOCALE)
+#if defined LC_ALL_MASK && (HAVE_GOOD_STRTOD_L || HAVE_WORKING_USELOCALE)
 
   locale_t locale = c_locale ();
   if (!locale)
@@ -78,7 +74,7 @@ C_STRTOD (char const *nptr, char **endptr)
       return 0; /* errno is set here */
     }
 
-# if (LONG ? HAVE_GOOD_STRTOLD_L : HAVE_GOOD_STRTOD_L)
+# if HAVE_GOOD_STRTOD_L
 
   r = STRTOD_L (nptr, endptr, locale);
 
