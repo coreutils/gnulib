2bb63c10dda2024a6176132869e2d5ac2f37d344
Bruno Haible <bruno@clisp.org>
2024-03-05 12:19:29 +0100

    c-strtod: Add tests.
    * tests/test-strtod.h: New file, extracted from tests/test-strtod.c.
    * tests/test-strtod.c: Include test-strtod.h.
    (FABS): Remove unused macro.
    (main): Just invoke test_function.
    * tests/test-strtod1.c: Add comment.
    * modules/strtod-tests (Files): Add tests/test-strtod.h.
    * tests/test-c-strtod.c: New file, based on tests/test-strtod.c.
    * tests/test-c-strtod1.c: New file, based on tests/test-strtod1.c.
    * tests/test-c-strtod1.sh: New file, based on tests/test-strtod1.sh.
    * modules/c-strtod-tests: New file, based on modules/strtod-tests.

---
+++ tests/test-c-strtod.c
@@ -0,0 +1,40 @@
+/* Test of c_strtod() in the "C" locale.
+   Copyright (C) 2008-2024 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include "c-strtod.h"
+
+#include <errno.h>
+#include <float.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "isnand-nolibm.h"
+#include "minus-zero.h"
+#include "macros.h"
+
+#include "test-strtod.h"
+
+int
+main (void)
+{
+  test_function (c_strtod);
+
+  return 0;
+}
---
+++ tests/test-c-strtod1.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+: "${LOCALE_FR=fr_FR}"
+: "${LOCALE_FR_UTF8=fr_FR.UTF-8}"
+
+if test $LOCALE_FR = none && test $LOCALE_FR_UTF8 = none; then
+  if test -f /usr/bin/localedef; then
+    echo "Skipping test: no locale for testing is installed"
+  else
+    echo "Skipping test: no locale for testing is supported"
+  fi
+  exit 77
+fi
+
+if $LC_NUMERIC_IMPLEMENTED; then
+  :
+else
+  echo "Skipping test: LC_NUMERIC category of locales is not implemented"
+  exit 77
+fi
+
+if test $LOCALE_FR != none; then
+  LC_ALL=$LOCALE_FR      ${CHECKER} ./test-c-strtod1${EXEEXT} || exit 1
+fi
+
+if test $LOCALE_FR_UTF8 != none; then
+  LC_ALL=$LOCALE_FR_UTF8 ${CHECKER} ./test-c-strtod1${EXEEXT} || exit 1
+fi
+
+exit 0
