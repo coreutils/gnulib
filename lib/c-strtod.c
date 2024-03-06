5e9bb2df01f85690688c707542d9f5bafaeb7bcf
Bruno Haible <bruno@clisp.org>
2024-03-06 13:45:35 +0100

    c-strtof, c-strtod, c-strtold: Comment.
    * lib/c-strtod.c: Mention me as co-author.

--- lib/c-strtod.c
+++ lib/c-strtod.c
@@ -15,7 +15,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
-/* Written by Paul Eggert.  */
+/* Written by Paul Eggert and Bruno Haible.  */
 
 #include <config.h>
 
