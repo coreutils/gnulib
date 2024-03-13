aa7548454bed4cf5bf1041ce23176479c4415865
Collin Funk <collin.funk1@gmail.com>
2024-03-13 12:17:41 -0700

    sig2str: Add tests.
    * tests/test-sig2str.c: New file.
    * modules/sig2str-tests: New file.

---
+++ tests/test-sig2str.c
@@ -0,0 +1,82 @@
+/* Test the sig2str and str2sig functions.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+
+   This file is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+
+   This file is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Collin Funk <collin.funk1@gmail.com>, 2024.  */
+
+#include <config.h>
+
+#include "sig2str.h"
+
+#include <string.h>
+
+#include "macros.h"
+
+int
+main (void)
+{
+  char buffer[SIG2STR_MAX];
+  int signo;
+
+  /* Test sig2str on signals specified by ISO C.  */
+
+  ASSERT (sig2str (SIGABRT, buffer) == 0);
+  ASSERT (STREQ (buffer, "ABRT"));
+
+  ASSERT (sig2str (SIGFPE, buffer) == 0);
+  ASSERT (STREQ (buffer, "FPE"));
+
+  ASSERT (sig2str (SIGILL, buffer) == 0);
+  ASSERT (STREQ (buffer, "ILL"));
+
+  ASSERT (sig2str (SIGINT, buffer) == 0);
+  ASSERT (STREQ (buffer, "INT"));
+
+  ASSERT (sig2str (SIGSEGV, buffer) == 0);
+  ASSERT (STREQ (buffer, "SEGV"));
+
+  ASSERT (sig2str (SIGTERM, buffer) == 0);
+  ASSERT (STREQ (buffer, "TERM"));
+
+  /* Test str2sig on signals specified by ISO C.  */
+
+  ASSERT (str2sig ("ABRT", &signo) == 0);
+  ASSERT (signo == SIGABRT);
+
+  ASSERT (str2sig ("FPE", &signo) == 0);
+  ASSERT (signo == SIGFPE);
+
+  ASSERT (str2sig ("ILL", &signo) == 0);
+  ASSERT (signo == SIGILL);
+
+  ASSERT (str2sig ("INT", &signo) == 0);
+  ASSERT (signo == SIGINT);
+
+  ASSERT (str2sig ("SEGV", &signo) == 0);
+  ASSERT (signo == SIGSEGV);
+
+  ASSERT (str2sig ("TERM", &signo) == 0);
+  ASSERT (signo == SIGTERM);
+
+  /* Check behavior of sig2str on invalid signals.  */
+
+  ASSERT (sig2str (-714, buffer) == -1);
+
+  /* Check behavior of str2sig on invalid signals.  */
+
+  ASSERT (str2sig ("Not a signal", &signo) == -1);
+
+  return 0;
+}
