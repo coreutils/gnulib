068189126c0ced46817cb22f5faf4bfab3ca51c6
Bruno Haible <bruno@clisp.org>
2024-04-27 02:22:05 +0200

    login_tty tests: Avoid gcc warnings.
    * tests/test-login_tty.c: Include ignore-value.h.
    (main): Ignore the results of the freopen calls.
    * modules/login_tty-tests (Depends-on): Add ignore-value.

--- tests/test-login_tty.c
+++ tests/test-login_tty.c
@@ -1,5 +1,5 @@
 /* Test of login_tty() function.
-   Copyright (C) 2010-2023 Free Software Foundation, Inc.
+   Copyright (C) 2010-2024 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -25,6 +25,8 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+
+#include "ignore-value.h"
 
 int
 main ()
@@ -63,7 +65,7 @@ main ()
     for (fd = 0; fd < 3; fd++)
       if (!(tcgetpgrp (fd) == getpid ()))
         {
-          freopen ("err", "w+", stderr);
+          ignore_value (freopen ("err", "w+", stderr));
           fprintf (stderr, "tcgetpgrp(%d) = %ld whereas getpid() = %ld\n",
                    fd, (long) tcgetpgrp (fd), (long) getpid ());
           fflush (stderr);
@@ -76,7 +78,7 @@ main ()
           {
             if (!(errno == ENOSYS))
               {
-                freopen ("err", "w+", stderr);
+                ignore_value (freopen ("err", "w+", stderr));
                 fprintf (stderr, "tcgetsid(%d) = -1 and errno = %d\n",
                          fd, errno);
                 fflush (stderr);
@@ -87,7 +89,7 @@ main ()
           {
             if (!(sid == getpid ()))
               {
-                freopen ("err", "w+", stderr);
+                ignore_value (freopen ("err", "w+", stderr));
                 fprintf (stderr, "tcgetsid(%d) = %ld whereas getpid() = %ld\n",
                          fd, (long) tcgetsid (fd), (long) getpid ());
                 fflush (stderr);
