6cb75b5ee654ccafc605cb73087aa8e23bd2e457
Bruno Haible <bruno@clisp.org>
2024-04-27 20:33:59 +0200

    gnulib-tool: Simplify the Python version test.
    Suggested by Pádraig Brady in
    <https://lists.gnu.org/archive/html/bug-gnulib/2024-04/msg00469.html>.
    
    * gnulib-tool: Use Python's sys.version_info, not --version.
    * gnulib-tool.py: Use the same version test here. Use --version only to
    produce a specific error message.

--- gnulib-tool.py
+++ gnulib-tool.py
@@ -131,17 +131,21 @@ func_gnulib_dir ()
 func_gnulib_dir
 
 # Check the Python version.
-if (python3 --version) >/dev/null 2>/dev/null; then
-  case `python3 --version 2>&1` in
-    Python\ 3.[0-6] | Python\ 3.[0-6].*)
-      func_fatal_error "python3 is too old (minimum required version is 3.7); try setting GNULIB_TOOL_IMPL=sh" ;;
-    Python\ 3.*)
-      ;;
-    *)
-      func_fatal_error "python3 version is unsupported" ;;
-  esac
+if (python3 -c 'import sys; sys.exit(not sys.version_info >= (3,7))') 2>/dev/null; then
+  :
 else
-  func_fatal_error "python3 not found; try setting GNULIB_TOOL_IMPL=sh"
+  if (python3 --version) >/dev/null 2>/dev/null; then
+    case `python3 --version 2>&1` in
+      Python\ 3.[0-6] | Python\ 3.[0-6].*)
+        func_fatal_error "python3 is too old (minimum required version is 3.7); try setting GNULIB_TOOL_IMPL=sh" ;;
+      Python\ 3.*)
+        ;;
+      *)
+        func_fatal_error "python3 version is unsupported" ;;
+    esac
+  else
+    func_fatal_error "python3 not found; try setting GNULIB_TOOL_IMPL=sh"
+  fi
 fi
 
 # Tell Python to store the compiled bytecode outside the gnulib directory.
