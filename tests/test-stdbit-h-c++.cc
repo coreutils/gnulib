1a016b72a9ea5d48c6eab35494282ec657db6e18
Bruno Haible <bruno@clisp.org>
2024-05-16 01:23:39 +0200

    stdbit-h: Add tests.
    * tests/test-stdbit-h.c: New file.
    * tests/test-stdbit-h-c++.cc: New file.
    * modules/stdbit-h-tests: New file.
    * modules/stdbit-h-c++-tests: New file.

---
+++ tests/test-stdbit-h-c++.cc
@@ -0,0 +1,32 @@
+/* Test of <stdbit.h> substitute in C++ mode.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2024.  */
+
+#define GNULIB_NAMESPACE gnulib
+#include <config.h>
+
+#include <stdbit.h>
+
+/* Check against conflicts between <stdbit.h> and the C++ header files.  */
+#include <stddef.h>
+#include <iostream>
+
+
+int
+main ()
+{
+}
