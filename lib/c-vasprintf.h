70d42c559229eb99ae41914ccca4f02c75d5a0bf
Bruno Haible <bruno@clisp.org>
2024-06-22 12:21:46 +0200

    c-vazsprintf: New module.
    * lib/c-vasprintf.h: Change license to LGPLv2+.
    Include <stddef.h>.
    (c_azsprintf, c_vazsprintf): New declarations.
    * lib/c-azsprintf.c: New file, based on lib/azsprintf.c.
    * lib/c-vazsprintf.c: New file, based on lib/vazsprintf.c.
    * modules/c-vazsprintf: New file.

--- lib/c-vasprintf.h
+++ lib/c-vasprintf.h
@@ -1,17 +1,17 @@
 /* vasprintf and asprintf, in C locale.
    Copyright (C) 2002-2004, 2006-2024 Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   This file is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
+   This file is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #ifndef _C_VASPRINTF_H
@@ -25,12 +25,32 @@
 /* Get va_list.  */
 #include <stdarg.h>
 
+/* Get ptrdiff_t.  */
+#include <stddef.h>
+
 /* Get _GL_ATTRIBUTE_SPEC_PRINTF_STANDARD.  */
 #include <stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/* Prints formatted output to a string dynamically allocated with malloc().
+   If the memory allocation succeeds, it stores the address of the string in
+   *RESULT and returns the number of resulting bytes, excluding the trailing
+   NUL.  Upon memory allocation error, or some other error, it returns -1
+   with errno set.
+
+   Failure code EOVERFLOW can only occur when a width > INT_MAX is used.
+   Therefore, if the format string is valid and does not use %ls/%lc
+   directives nor widths, the only possible failure code is ENOMEM.
+
+   Formatting takes place in the C locale, that is, the decimal point
+   used in floating-point formatting directives is always '.'. */
+ptrdiff_t c_azsprintf (char **resultp, const char *format, ...)
+       _GL_ATTRIBUTE_FORMAT ((_GL_ATTRIBUTE_SPEC_PRINTF_STANDARD, 2, 3));
+ptrdiff_t c_vazsprintf (char **resultp, const char *format, va_list args)
+       _GL_ATTRIBUTE_FORMAT ((_GL_ATTRIBUTE_SPEC_PRINTF_STANDARD, 2, 0));
 
 /* asprintf() and vasprintf(), but formatting takes place in the C locale, that
    is, the decimal point used in floating-point formatting directives is always
