6c4c5f3de377aa12aa57952d1cca5ac8b027ccb4
Bruno Haible <bruno@clisp.org>
2024-06-24 20:11:48 +0200

    c-vasnprintf: Relicense under LGPLv2+.
    * lib/c-vasnprintf.h: Rebase on lib/vasnprintf.h.
    * lib/c-vasnprintf.c: Rebase on lib/unistdio/ulc-vasnprintf.c.
    * modules/c-vasnprintf (License): Change to LGPLv2+.

--- lib/c-vasnprintf.c
+++ lib/c-vasnprintf.c
@@ -1,17 +1,17 @@
 /* Formatted output to strings in C locale.
    Copyright (C) 2009-2024 Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   This file is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of the
+   License, or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
+   This file is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
+   You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include <config.h>
@@ -25,14 +25,14 @@
 
 #define VASNPRINTF c_vasnprintf
 #define FCHAR_T char
-#define DCHAR_T char
 #define DIRECTIVE char_directive
 #define DIRECTIVES char_directives
 #define PRINTF_PARSE printf_parse
+#define DCHAR_T char
 #define DCHAR_CPY memcpy
 #define DCHAR_SET memset
-#define DCHAR_IS_TCHAR 1
 #define TCHAR_T char
+#define DCHAR_IS_TCHAR 1
 
 #define NEED_PRINTF_DOUBLE 1
 #define NEED_PRINTF_LONG_DOUBLE 1
