281bc6d553fa27e2d4d80c1f4106dbf6b1b24b69
Bruno Haible <bruno@clisp.org>
2024-07-24 23:53:53 +0200

    Avoid false gethostname_used_without_requesting_gnulib_module_gethostname.
    * modules/gethostname (configure.ac): Invoke gl_MODULE_INDICATOR.
    * lib/unistd.in.h (gethostname): Don't define to
    gethostname_used_without_requesting_gnulib_module_gethostname if the
    gnulib module 'gethostname' is in use under the same configure.ac.

--- lib/unistd.in.h
+++ lib/unistd.in.h
@@ -1318,8 +1318,10 @@ _GL_CXXALIAS_SYS_CAST (gethostname, int, (char *name, size_t len));
 # endif
 _GL_CXXALIASWARN (gethostname);
 #elif @UNISTD_H_HAVE_WINSOCK2_H@
-# undef gethostname
-# define gethostname gethostname_used_without_requesting_gnulib_module_gethostname
+# if !GNULIB_GETHOSTNAME
+#  undef gethostname
+#  define gethostname gethostname_used_without_requesting_gnulib_module_gethostname
+# endif
 #elif defined GNULIB_POSIXCHECK
 # undef gethostname
 # if HAVE_RAW_DECL_GETHOSTNAME
