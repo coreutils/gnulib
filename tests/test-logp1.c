76531f80e62fcf97662b3ab570cecad64c0aed5e
Bruno Haible <bruno@clisp.org>
2024-07-29 03:56:28 +0200

    logp1: Add tests.
    * tests/test-logp1.c: New file, based on tests/test-log1p.c.
    * modules/logp1-tests: New file.

---
+++ tests/test-logp1.c
@@ -0,0 +1,49 @@
+/* Test of logp1() function.
+   Copyright (C) 2010-2024 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2010.  */
+
+#include <config.h>
+
+#include <math.h>
+
+#include "signature.h"
+SIGNATURE_CHECK (logp1, double, (double));
+
+#include <float.h>
+
+#include "macros.h"
+
+#define DOUBLE double
+#define HUGEVAL HUGE_VAL
+#define L_(literal) literal
+#define MANT_DIG DBL_MANT_DIG
+#define LOG1P logp1
+#define RANDOM randomd
+#include "test-log1p.h"
+
+int
+main ()
+{
+  /* A particular value.  */
+  x = 0.6;
+  y = logp1 (x);
+  ASSERT (y >= 0.4700036292 && y <= 0.4700036293);
+
+  test_function ();
+
+  return test_exit_status;
+}
