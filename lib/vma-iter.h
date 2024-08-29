c2aa5ba94a45411ec63a43d762984029797a6254
Bruno Haible <bruno@clisp.org>
2024-08-29 18:58:55 +0200

    vma-iter: Relicense under GPLv2+.
    * modules/vma-iter (License): Change to GPLv2+.
    * lib/vma-iter.h: Update license notice.
    * lib/vma-iter.c: Likewise.

--- lib/vma-iter.h
+++ lib/vma-iter.h
@@ -2,12 +2,12 @@
    Copyright (C) 2011-2024 Free Software Foundation, Inc.
    Written by Bruno Haible <bruno@clisp.org>, 2011.
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   This file is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2 of the License,
+   or (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful,
+   This file is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
