ff5bc263b0bf534671e33b49582dbcd57cc2c6a5
Bruno Haible <bruno@clisp.org>
2024-09-01 18:09:28 +0200

    math: Remove workaround for an older Haiku bug.
    * lib/math.in.h (FP_ILOGB0, FP_ILOGBNAN): Don't override on Haiku.

--- lib/math.in.h
+++ lib/math.in.h
@@ -227,16 +227,7 @@ _NaN ()
 #endif
 
 
-#if defined FP_ILOGB0 && defined FP_ILOGBNAN
- /* Ensure FP_ILOGB0 and FP_ILOGBNAN are correct.  */
-# if defined __HAIKU__
-  /* Haiku: match what ilogb() does */
-#  undef FP_ILOGB0
-#  undef FP_ILOGBNAN
-#  define FP_ILOGB0   (- 2147483647 - 1) /* INT_MIN */
-#  define FP_ILOGBNAN (- 2147483647 - 1) /* INT_MIN */
-# endif
-#else
+#if !(defined FP_ILOGB0 && defined FP_ILOGBNAN)
  /* Ensure FP_ILOGB0 and FP_ILOGBNAN are defined.  */
 # if defined __NetBSD__ || defined __sgi
   /* NetBSD, IRIX 6.5: match what ilogb() does */
