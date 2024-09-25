20316f78bffd374f7034cf37b2b1f1ec8d0c3f21
Bruno Haible <bruno@clisp.org>
2024-09-25 18:26:18 +0200

    xstring-buffer: New module.
    * lib/string-buffer.h (sb_xappend1, sb_xappend_desc, sb_xappend_c,
    sb_xappendvf, sb_xappendf, sb_xcontents_c, sb_xdupfree, sb_xdupfree_c):
    New declarations.
    * lib/xstring-buffer.c: New file.
    * lib/xstring-buffer-printf.c: New file.
    * modules/xstring-buffer: New file.

--- lib/string-buffer.h
+++ lib/string-buffer.h
@@ -19,8 +19,9 @@
 #ifndef _STRING_BUFFER_H
 #define _STRING_BUFFER_H
 
-/* This file uses _GL_ATTRIBUTE_MALLOC, _GL_ATTRIBUTE_CAPABILITY_TYPE,
-   _GL_ATTRIBUTE_ACQUIRE_CAPABILITY, _GL_ATTRIBUTE_RELEASE_CAPABILITY.  */
+/* This file uses _GL_ATTRIBUTE_MALLOC, _GL_ATTRIBUTE_RETURNS_NONNULL,
+   _GL_ATTRIBUTE_CAPABILITY_TYPE, _GL_ATTRIBUTE_ACQUIRE_CAPABILITY,
+   _GL_ATTRIBUTE_RELEASE_CAPABILITY.  */
 #if !_GL_CONFIG_H_INCLUDED
  #error "Please include config.h first."
 #endif
@@ -48,6 +49,8 @@ struct string_buffer
 extern "C" {
 #endif
 
+/* ================== Functions in module 'string-buffer' ================== */
+
 /* Initializes BUFFER to the empty string.  */
 extern void sb_init (struct string_buffer *buffer)
   _GL_ATTRIBUTE_ACQUIRE_CAPABILITY (buffer->data);
@@ -56,7 +59,7 @@ extern void sb_init (struct string_buffer *buffer)
    Returns 0, or -1 in case of out-of-memory error.  */
 extern int sb_append1 (struct string_buffer *buffer, char c);
 
-/* Appends the contents of the memory area STR to BUFFER.
+/* Appends the contents of the memory area S to BUFFER.
    Returns 0, or -1 in case of out-of-memory error.  */
 extern int sb_append_desc (struct string_buffer *buffer, string_desc_t s);
 
@@ -122,6 +125,78 @@ extern string_desc_t sb_dupfree (struct string_buffer *buffer)
    It is the responsibility of the caller to free() the result.  */
 extern char * sb_dupfree_c (struct string_buffer *buffer)
   _GL_ATTRIBUTE_RELEASE_CAPABILITY (buffer->data);
+
+/* ================== Functions in module 'xstring-buffer' ================== */
+
+#if GNULIB_XSTRING_BUFFER
+
+/* The following functions invoke xalloc_die () in case of out-of-memory
+   error.  */
+
+/* Appends the character C to BUFFER.  */
+extern void sb_xappend1 (struct string_buffer *buffer, char c);
+
+/* Appends the contents of the memory area S to BUFFER.  */
+extern void sb_xappend_desc (struct string_buffer *buffer, string_desc_t s);
+
+/* Appends the contents of the C string STR to BUFFER.  */
+extern void sb_xappend_c (struct string_buffer *buffer, const char *str);
+
+/* Appends the result of the printf-compatible FORMATSTRING with the argument
+   list LIST to BUFFER.
+   Returns 0, or -1 in case of error other than out-of-memory error.
+   Error code EOVERFLOW can only occur when a width > INT_MAX is used.
+   Therefore, if the format string is valid and does not use %ls/%lc
+   directives nor widths, no error is possible.  */
+extern int sb_xappendvf (struct string_buffer *buffer,
+                         const char *formatstring, va_list list)
+  #if (__GNUC__ + (__GNUC_MINOR__ >= 4) > 4) && !defined __clang__
+  ATTRIBUTE_FORMAT ((__gnu_printf__, 2, 0))
+  #else
+  ATTRIBUTE_FORMAT ((__printf__, 2, 0))
+  #endif
+  ;
+
+/* Appends the result of the printf-compatible FORMATSTRING with the following
+   arguments to BUFFER.
+   Returns 0, or -1 in case of error other than out-of-memory error.
+   Error code EOVERFLOW can only occur when a width > INT_MAX is used.
+   Therefore, if the format string is valid and does not use %ls/%lc
+   directives nor widths, no error is possible.  */
+extern int sb_xappendf (struct string_buffer *buffer,
+                        const char *formatstring, ...)
+  #if (__GNUC__ + (__GNUC_MINOR__ >= 4) > 4) && !defined __clang__
+  ATTRIBUTE_FORMAT ((__gnu_printf__, 2, 3))
+  #else
+  ATTRIBUTE_FORMAT ((__printf__, 2, 3))
+  #endif
+  ;
+
+/* Ensures the contents of BUFFER is followed by a NUL byte (without
+   incrementing the length of the contents).
+   Then returns a read-only view of the current contents of BUFFER,
+   that is, the current contents of BUFFER as a C string.
+   The result is only valid until the next operation on BUFFER.  */
+extern const char * sb_xcontents_c (struct string_buffer *buffer)
+  _GL_ATTRIBUTE_RETURNS_NONNULL;
+
+/* Returns the contents of BUFFER and frees all other memory held by BUFFER.
+   Returns (0, NULL) if there was an error earlier.
+   It is the responsibility of the caller to string_desc_free() the result.  */
+extern string_desc_t sb_xdupfree (struct string_buffer *buffer)
+  _GL_ATTRIBUTE_RELEASE_CAPABILITY (buffer->data);
+
+/* Returns the contents of BUFFER (with an added trailing NUL, that is,
+   as a C string), and frees all other memory held by BUFFER.
+   Returns NULL if there was an error earlier.
+   It is the responsibility of the caller to free() the result.  */
+extern char * sb_xdupfree_c (struct string_buffer *buffer)
+  _GL_ATTRIBUTE_MALLOC _GL_ATTRIBUTE_DEALLOC_FREE
+  _GL_ATTRIBUTE_RELEASE_CAPABILITY (buffer->data);
+
+#endif /* GNULIB_XSTRING_BUFFER */
+
+/* ========================================================================== */
 
 #ifdef __cplusplus
 }
