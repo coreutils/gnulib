f9da9cbb88036d5dae18df9394c1e2fe6a6bc3a8
Bruno Haible <bruno@clisp.org>
2024-11-05 21:10:23 +0100

    strndup tests: Verify N3322 functionality.
    * tests/test-strndup.c: New file.
    * modules/strndup-tests: New file.

---
+++ tests/test-strndup.c
@@ -0,0 +1,38 @@
+/* Test of strndup() function.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include <string.h>
+
+#include <stddef.h>
+
+#include "macros.h"
+
+int
+main (void)
+{
+  int volatile value;
+
+  /* Test zero-length operations on NULL pointers, allowed by
+     <https://www.open-std.org/jtc1/sc22/wg14/www/docs/n3322.pdf>.  */
+
+  value = (strndup (NULL, 0) != NULL);
+  ASSERT (value);
+
+  return test_exit_status;
+}
