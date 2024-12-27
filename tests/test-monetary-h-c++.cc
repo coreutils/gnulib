c08ded39555f5b6e39f20ff51fbee4145cfd76b2
Bruno Haible <bruno@clisp.org>
2024-12-27 23:14:27 +0100

    Rename module monetary to monetary-h.
    * modules/monetary-h: Renamed from modules/monetary.
    * modules/monetary-h-tests: Renamed from modules/monetary-tests.
    * modules/monetary-h-c++-tests: Renamed from modules/monetary-c++-tests.
    * tests/test-monetary-h.c: Renamed from tests/test-monetary.c.
    * tests/test-monetary-h-c++.cc: Renamed from tests/test-monetary-c++.cc.
    * doc/posix-headers/monetary.texi: Update.
    * modules/* (Depends-on): Update.
    * modules/monetary: New file.

---
+++ tests/test-monetary-h-c++.cc
@@ -0,0 +1,39 @@
+/* Test of <monetary.h> substitute in C++ mode.
+   Copyright (C) 2017-2024 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2017.  */
+
+#define GNULIB_NAMESPACE gnulib
+#include <config.h>
+
+#if HAVE_MONETARY_H
+# include <monetary.h>
+#endif
+
+#include "signature.h"
+
+
+#if GNULIB_TEST_STRFMON_L
+SIGNATURE_CHECK (GNULIB_NAMESPACE::strfmon_l, ssize_t,
+                 (char *s, size_t maxsize, locale_t locale,
+                  const char *format, ...));
+#endif
+
+
+int
+main ()
+{
+}
