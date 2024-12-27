2b43bd904dda49b18bc7603988ffba6a08c44267
Bruno Haible <bruno@clisp.org>
2024-12-27 23:41:42 +0100

    Rename module sys_un to sys_un-h.
    * modules/sys_un-h: Renamed from modules/sys_un.
    * modules/sys_un-h-tests: Renamed from modules/sys_un-tests.
    * modules/sys_un-h-c++-tests: Renamed from modules/sys_un-c++-tests.
    * tests/test-sys_un-h.c: Renamed from tests/test-sys_un.c.
    * tests/test-sys_un-h-c++.cc: Renamed from tests/test-sys_un-c++.cc.
    * doc/posix-headers/sys_un.texi: Update.
    * modules/sys_un: New file.

---
+++ tests/test-sys_un-h-c++.cc
@@ -0,0 +1,44 @@
+/* Test of the <sys/un.h> substitute in C++ mode.
+   Copyright (C) 2024 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Collin Funk <collin.funk1@gmail.com>, 2024.  */
+
+#define GNULIB_NAMESPACE gnulib
+#include <config.h>
+
+#if HAVE_UNIXSOCKET
+
+# include <sys/un.h>
+
+int
+main ()
+{
+}
+
+#else
+
+/* UNIX domain sockets unsupported.  */
+
+# include <iostream>
+
+int
+main ()
+{
+  std::cout << "Skipping test: UNIX domain sockets not supported" << std::endl;
+  return 77;
+}
+
+#endif
