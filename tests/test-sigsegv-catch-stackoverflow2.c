4b3bf87eecba10805ce10bbd74ad04cf9fdaa629
Bruno Haible <bruno@clisp.org>
2025-01-06 01:16:11 +0100

    sigsegv tests: Work around a longjmp bug on GNU/Hurd.
    * tests/test-sigsegv-catch-stackoverflow1.c (_FORTIFY_SOURCE,
    __USE_FORTIFY_LEVEL): Undefine, as a workaround to the Hurd longjmp bug.
    * tests/test-sigsegv-catch-stackoverflow2.c (_FORTIFY_SOURCE,
    __USE_FORTIFY_LEVEL): Likewise.
    * doc/posix-functions/longjmp.texi: Document the Hurd bug.

--- tests/test-sigsegv-catch-stackoverflow2.c
+++ tests/test-sigsegv-catch-stackoverflow2.c
@@ -18,6 +18,14 @@
 
 #include <config.h>
 
+/* On GNU/Hurd, when compiling with -D_FORTIFY_SOURCE=2, avoid an error
+   "*** longjmp causes uninitialized stack frame ***: terminated".
+   Cf. <https://sourceware.org/bugzilla/show_bug.cgi?id=32522>  */
+#ifdef __GNU__
+# undef _FORTIFY_SOURCE
+# undef __USE_FORTIFY_LEVEL
+#endif
+
 /* Specification.  */
 #include "sigsegv.h"
 
