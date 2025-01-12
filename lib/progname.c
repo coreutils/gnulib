6252c111bafc4b781637c621740af5b854c0436a
Pádraig Brady <P@draigBrady.com>
2025-01-12 16:47:57 +0000

    progname: also set program_invocation_short_name
    * lib/progname.c (set_program_name): Keep program_invocation_name
    and program_invocation_short_name consistent.

--- lib/progname.c
+++ lib/progname.c
@@ -1,5 +1,5 @@
 /* Program name management.
-   Copyright (C) 2001-2003, 2005-2024 Free Software Foundation, Inc.
+   Copyright (C) 2001-2003, 2005-2025 Free Software Foundation, Inc.
    Written by Bruno Haible <bruno@clisp.org>, 2001.
 
    This program is free software: you can redistribute it and/or modify
@@ -63,12 +63,8 @@ set_program_name (const char *argv0)
       argv0 = base;
       if (strncmp (base, "lt-", 3) == 0)
         {
-          argv0 = base + 3;
-          /* On glibc systems, remove the "lt-" prefix from the variable
-             program_invocation_short_name.  */
-#if HAVE_DECL_PROGRAM_INVOCATION_SHORT_NAME
-          program_invocation_short_name = (char *) argv0;
-#endif
+          base += 3;
+          argv0 = base;
         }
     }
 
@@ -88,5 +84,8 @@ set_program_name (const char *argv0)
      as well.  */
 #if HAVE_DECL_PROGRAM_INVOCATION_NAME
   program_invocation_name = (char *) argv0;
+#endif
+#if HAVE_DECL_PROGRAM_INVOCATION_SHORT_NAME
+  program_invocation_short_name = (char *) base;
 #endif
 }
