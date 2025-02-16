6b6a5ffe15fc27f43636ff3ad2efee32b77b0cff
Bruno Haible <bruno@clisp.org>
2025-02-16 11:34:23 +0100

    ctype-h: Make ctype.h self-contained again (regr. 2025-02-14).
    * lib/ctype.in.h: Add comments re _GL_ARG_NONNULL.
    * modules/ctype-h (Depends-on): Add snippet/arg-nonnull.
    (Makefile.am): Substitute $(ARG_NONNULL_H) into ctype.h.

--- lib/ctype.in.h
+++ lib/ctype.in.h
@@ -49,7 +49,7 @@
 #ifndef _@GUARD_PREFIX@_CTYPE_H
 #define _@GUARD_PREFIX@_CTYPE_H
 
-/* This file uses GNULIB_POSIXCHECK, HAVE_RAW_DECL_*.  */
+/* This file uses _GL_ARG_NONNULL, GNULIB_POSIXCHECK, HAVE_RAW_DECL_*.  */
 #if !_GL_CONFIG_H_INCLUDED
  #error "Please include config.h first."
 #endif
@@ -64,6 +64,8 @@
 #endif
 
 /* The definitions of _GL_FUNCDECL_RPL etc. are copied here.  */
+
+/* The definition of _GL_ARG_NONNULL is copied here.  */
 
 /* The definition of _GL_WARN_ON_USE is copied here.  */
 
