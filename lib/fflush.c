ebbd322ff029a9212274df18f1aeac27cc6df6b6
Collin Funk <collin.funk1@gmail.com>
2025-04-06 19:34:54 -0700

    fflush: Port to recent Haiku snapshots.
    * lib/stdio-impl.h (_IO_IN_BACKUP) [__HAIKU__]: Define macros.
    * lib/fflush.c (fp_) [!__HAIKU__]: Define to fp.
    (fflush): Use fp_ instead of fp.

--- lib/fflush.c
+++ lib/fflush.c
@@ -33,12 +33,15 @@
 
 #if defined _IO_EOF_SEEN || defined _IO_ftrylockfile || __GNU_LIBRARY__ == 1
 /* GNU libc, BeOS, Haiku, Linux libc5 */
+# if !defined __HAIKU__
+#  define fp_ fp
+# endif
 
 /* Clear the stream's ungetc buffer, preserving the value of ftello (fp).  */
 static void
 clear_ungetc_buffer_preserving_position (FILE *fp)
 {
-  if (fp->_flags & _IO_IN_BACKUP)
+  if (fp_->_flags & _IO_IN_BACKUP)
     /* _IO_free_backup_area is a bit complicated.  Simply call fseek.  */
     fseeko (fp, 0, SEEK_CUR);
 }
