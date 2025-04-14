959fe24f51033374ce8986c5b1199fb8ddf2f816
Bruno Haible <bruno@clisp.org>
2025-04-14 15:15:12 +0200

    select tests: Work around a Cygwin bug.
    * tests/test-select-in.sh: Skip the /dev/null test on Cygwin 3.6.x.

--- tests/test-select-in.sh
+++ tests/test-select-in.sh
@@ -28,7 +28,18 @@ test `cat t-select-in.tmp` = "1" || exit 1
 
 # Special files.
 # This part of the test is known to fail on Solaris 2.6 and older.
-
+# Also it fails in Cygwin 3.6.1, due to a Cygwin regression.
+# <https://sourceware.org/pipermail/cygwin/2025-April/257940.html>
+# <https://sourceware.org/pipermail/cygwin/2025-April/257952.html>
+case `uname -s` in
+  CYGWIN*)
+    case `uname -r` in
+      3.6.*)
+        echo "Skipping test: known Cygwin 3.6.x bug"
+        exit 77;;
+    esac
+    ;;
+esac
 rm -f t-select-in.tmp
 ${CHECKER} ./test-select-fd${EXEEXT} r 0 t-select-in.tmp < /dev/null
 test `cat t-select-in.tmp` = "1" || exit 1
