630f2f65887cb01eb01252b9c38ad8dbefc8ee98
Bruno Haible <bruno@clisp.org>
2025-05-01 17:28:06 +0200

    javacomp: Fix resource leak.
    * lib/javacomp.c (execute_and_read_line): Upon failure, close the stream
    and wait for the child process to terminate.

--- lib/javacomp.c
+++ lib/javacomp.c
@@ -359,6 +359,8 @@ execute_and_read_line (const char *progname,
   if (linelen == (size_t)(-1))
     {
       error (0, 0, _("%s subprocess I/O error"), progname);
+      fclose (fp);
+      wait_subprocess (child, progname, true, false, true, false, NULL);
       return NULL;
     }
   if (linelen > 0 && line[linelen - 1] == '\n')
