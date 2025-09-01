a299d041f50d16cc945e05b67bf4b8f96efd14e7
Collin Funk <collin.funk1@gmail.com>
2025-09-01 14:01:36 -0700

    sys_types-h tests: Fix a static_assert failure on glibc/alpha.
    * tests/test-sys_types-h.c [__GLIBC__ && __alpha]: Don't check that
    blksize_t and blkcnt_t are signed.
    * doc/posix-headers/sys_types.texi: Mention that these types are
    unsigned on this platform.

--- tests/test-sys_types-h.c
+++ tests/test-sys_types-h.c
@@ -46,12 +46,14 @@ static_assert (TYPE_SIGNED (off_t));
 static_assert (TYPE_SIGNED (off64_t));
 
 /* POSIX requires that blksize_t is a signed integer type.  */
-#if !(defined __ANDROID__ || (defined __FreeBSD_kernel__ && !defined __FreeBSD__))
+#if !(defined __ANDROID__ \
+      || (defined __FreeBSD_kernel__ && !defined __FreeBSD__) \
+      || defined __GLIBC__ && defined __alpha)
 static_assert (TYPE_SIGNED (blksize_t));
 #endif
 
 /* POSIX requires that blkcnt_t is a signed integer type.  */
-#if !defined __ANDROID__
+#if !(defined __ANDROID__ || defined __GLIBC__ && defined __alpha)
 static_assert (TYPE_SIGNED (blkcnt_t));
 #endif
 
