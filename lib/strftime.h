79cc4b69d6ac74766e04b12966960051ce2e75f3
Paul Eggert <eggert@cs.ucla.edu>
2025-11-01 11:02:53 -0600

    nstrftime: don’t assume <time.h> defines ptrdiff_t
    * lib/strftime.h: Include <stddef.h>

--- lib/strftime.h
+++ lib/strftime.h
@@ -15,6 +15,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
+#include <stddef.h>
 #include <time.h>
 
 #ifdef __cplusplus
