1141f12011ff79542eadb84d15502272c52584d5
Collin Funk <collin.funk1@gmail.com>
2025-12-31 17:59:14 -0800

    sys_stat-h tests: Fix a static_assert failure on QNX.
    * tests/test-sys_stat-h.c [__QNX__]: Don't check that blksize_t and
    blkcnt_t are signed. Add some parentheses for readability.
    * doc/posix-headers/sys_stat.texi: Mention that these types are unsigned
    on this platform.

--- tests/test-sys_stat-h.c
+++ tests/test-sys_stat-h.c
@@ -339,14 +339,15 @@ struct timespec st;
 static_assert (TYPE_SIGNED (off_t));
 
 /* POSIX requires that blksize_t is a signed integer type.  */
-#if !(defined __ANDROID__ \
+#if !(defined __ANDROID__ || defined __QNX__                  \
       || (defined __FreeBSD_kernel__ && !defined __FreeBSD__) \
-      || defined __GLIBC__ && defined __alpha)
+      || (defined __GLIBC__ && defined __alpha))
 static_assert (TYPE_SIGNED (blksize_t));
 #endif
 
 /* POSIX requires that blkcnt_t is a signed integer type.  */
-#if !(defined __ANDROID__ || defined __GLIBC__ && defined __alpha)
+#if !(defined __ANDROID__ || defined __QNX__            \
+      || (defined __GLIBC__ && defined __alpha))
 static_assert (TYPE_SIGNED (blkcnt_t));
 #endif
 
