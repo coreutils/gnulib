8ebbf13477415e48732a0ad33efc07c4678e7d29
Bruno Haible <bruno@clisp.org>
2026-01-09 00:46:34 +0100

    get-rusage-data tests: Avoid test failure on Haiku.
    * tests/test-get-rusage-data.c (main): Disable a failing assertion on
    Haiku.

--- tests/test-get-rusage-data.c
+++ tests/test-get-rusage-data.c
@@ -61,7 +61,7 @@ main ()
       ASSERT (value2 >= value1);
       ASSERT (value3 >= value2);
 
-#if !(__GLIBC__ == 2 || MUSL_LIBC || (defined __APPLE__ && defined __MACH__) || defined __FreeBSD__ || defined __DragonFly__ || defined __NetBSD__ || defined __OpenBSD__ || defined _WIN32 || defined __CYGWIN__)
+#if !(__GLIBC__ == 2 || MUSL_LIBC || (defined __APPLE__ && defined __MACH__) || defined __FreeBSD__ || defined __DragonFly__ || defined __NetBSD__ || defined __OpenBSD__ || defined _WIN32 || defined __CYGWIN__ || defined __HAIKU__)
       /* Allocating 2.5 MB of memory should increase the data segment size.  */
       ASSERT (value3 > value1);
 #endif
