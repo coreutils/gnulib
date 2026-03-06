83a7ecb385195d59009000a9f01c48dd40e05d07
Collin Funk <collin.funk1@gmail.com>
2026-03-05 22:22:41 -0800

    gnulib-tool.py: Use --symlink or --hardlink for local modules.
    Reported by Pádraig Brady in
    <https://lists.gnu.org/r/coreutils/2026-03/msg00015.html>.
    
    * pygnulib/GLConfig.py (__init__): If --local-symlink or
    --local-hardlink are not specified, use the value specified by --symlink
    or --hardlink for local modules instead of copying.

--- pygnulib/GLConfig.py
+++ pygnulib/GLConfig.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2002-2025 Free Software Foundation, Inc.
+# Copyright (C) 2002-2026 Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -209,8 +209,8 @@ class GLConfig:
         # lcopymode (--local-symlink and --local-hardlink)
         self.resetLCopyMode()
         if lcopymode == None:
-            # Default to copying.
-            lcopymode = CopyAction.Copy
+            # Default to the mode used for non-local modules.
+            lcopymode = copymode
         self.setLCopyMode(lcopymode)
         # configure_ac
         self.resetAutoconfFile()
