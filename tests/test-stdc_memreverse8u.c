ee8c16407d902cae5aa150742a0d5aba1d54a1a5
Bruno Haible <bruno@clisp.org>
2026-03-15 13:05:20 +0100

    stdc_memreverse8u: Add tests.
    * tests/test-stdc_memreverse8u.c: New file, based on
    tests/test-byteswap.c.
    * modules/stdc_memreverse8u-tests: New file.

---
+++ tests/test-stdc_memreverse8u.c
@@ -0,0 +1,79 @@
+/* Test of stdc_memreverse8u* functions.
+   Copyright (C) 2007-2026 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* Written by Bruno Haible <bruno@clisp.org>, 2007.  */
+
+#include <config.h>
+
+/* Specification.  */
+#include <stdbit.h>
+
+#include <stdint.h>
+
+#include "macros.h"
+
+/* Test stdc_memreverse8u* functions with constant values.  */
+static void
+test_memreverse8u_constant (void)
+{
+  ASSERT (stdc_memreverse8u8 (UINT8_C (0x42)) == UINT8_C (0x42));
+  ASSERT (stdc_memreverse8u16 (UINT16_C (0x1234)) == UINT16_C (0x3412));
+  ASSERT (stdc_memreverse8u32 (UINT32_C (0x12345678)) == UINT32_C (0x78563412));
+  ASSERT (stdc_memreverse8u64 (UINT64_C (0x1234567890ABCDEF))
+          == UINT64_C (0xEFCDAB9078563412));
+}
+
+/* Test that the stdc_memreverse8u* functions evaluate their arguments once.  */
+static void
+test_memreverse8u_eval_once (void)
+{
+  uint8_t value_0 = 0;
+  ASSERT (stdc_memreverse8u8 (value_0++) == 0);
+  ASSERT (value_0 == 1);
+
+  uint16_t value_1 = 0;
+  ASSERT (stdc_memreverse8u16 (value_1++) == 0);
+  ASSERT (value_1 == 1);
+
+  uint32_t value_2 = 0;
+  ASSERT (stdc_memreverse8u32 (value_2++) == 0);
+  ASSERT (value_2 == 1);
+
+  uint64_t value_3 = 0;
+  ASSERT (stdc_memreverse8u64 (value_3++) == 0);
+  ASSERT (value_3 == 1);
+}
+
+/* Test that the stdc_memreverse8u* functions accept floating-point
+   arguments.  */
+static void
+test_memreverse8u_double (void)
+{
+  ASSERT (stdc_memreverse8u8 (0.0) == 0);
+  ASSERT (stdc_memreverse8u16 (0.0) == 0);
+  ASSERT (stdc_memreverse8u32 (0.0) == 0);
+  ASSERT (stdc_memreverse8u64 (0.0) == 0);
+}
+
+int
+main (void)
+{
+  test_memreverse8u_constant ();
+  test_memreverse8u_eval_once ();
+  test_memreverse8u_double ();
+
+  return test_exit_status;
+}
