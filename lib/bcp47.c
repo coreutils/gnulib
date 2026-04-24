ea40d7d7013d684d23ec16ae94f2e708eb1f1cfa
Bruno Haible <bruno@clisp.org>
2026-04-24 15:57:52 +0200

    bcp47: Fix handling of variants of length 4.
    Reported by Patrice Dumas <pertusus@gnu.org> in
    <https://lists.gnu.org/archive/html/bug-gnulib/2026-04/msg00154.html>.
    
    * lib/bcp47.c (bcp47_to_xpg): Don't parse a variant of length 4 that
    contains some digits as a script.
    * tests/test-bcp47.c (main): Add a test case with a variant of length 4.

--- lib/bcp47.c
+++ lib/bcp47.c
@@ -405,7 +405,14 @@ bcp47_to_xpg (char *xpg, const char *bcp47, const char *codeset)
               p++;
             size_t subtag_len = p - subtag_start;
 
-            if (!past_script && subtag_len == 4)
+            if (!past_script
+                && subtag_len == 4
+                /* Subtags of length 4 that contain digits are variants,
+                   not scripts.  */
+                && !(subtag_start[0] >= '0' && subtag_start[0] <= '9')
+                && !(subtag_start[1] >= '0' && subtag_start[1] <= '9')
+                && !(subtag_start[2] >= '0' && subtag_start[2] <= '9')
+                && !(subtag_start[3] >= '0' && subtag_start[3] <= '9'))
               {
                 /* Parsed -script.  */
                 script_start = subtag_start;
