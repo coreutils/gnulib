630c3df05694698ddb9efc7bd848281eb7cab96d
Bruno Haible <bruno@clisp.org>
2026-05-10 17:04:19 +0200

    signal.h: Fix compilation error on Solaris 10 in C++ mode.
    * lib/signal.in.h: On Solaris in C++ mode, don't include <pthread.h>.
    The include <pthread.h> is only needed for Solaris versions < 9.

--- lib/signal.in.h
+++ lib/signal.in.h
@@ -68,12 +68,13 @@
 
 /* Mac OS X 10.3, FreeBSD < 8.0, OpenBSD < 5.1, Solaris 2.6, Android,
    OS/2 kLIBC declare pthread_sigmask in <pthread.h>, not in <signal.h>.
-   But avoid namespace pollution on glibc systems.*/
+   But avoid namespace pollution on glibc systems.  */
 #if (@GNULIB_PTHREAD_SIGMASK@ || defined GNULIB_POSIXCHECK) \
     && ((defined __APPLE__ && defined __MACH__) \
         || (defined __FreeBSD__ && __FreeBSD__ < 8) \
         || (defined __OpenBSD__ && OpenBSD < 201205) \
-        || defined __sun || defined __ANDROID__ \
+        || (defined __sun && !defined __cplusplus) \
+        || defined __ANDROID__ \
         || defined __KLIBC__) \
     && ! defined __GLIBC__
 # include <pthread.h>
