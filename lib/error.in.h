ddb55b0d7d80aee19ee9add9d22372b6f4a4cda7
Bruno Haible <bruno@clisp.org>
2026-05-12 11:39:58 +0200

    error: Restore library name-spacing of verror_at_line.
    * lib/error.in.h (verror_at_line): Don't define as a macro if
    _GL_NO_INLINE_ERROR is defined.

--- lib/error.in.h
+++ lib/error.in.h
@@ -239,7 +239,7 @@ extern void verror_at_line (int __status, int __errnum, const char *__fname,
                             va_list __args)
      _GL_ATTRIBUTE_COLD
      _GL_ATTRIBUTE_FORMAT ((_GL_ATTRIBUTE_SPEC_PRINTF_STANDARD, 5, 0));
-#ifdef _GL_NO_INLINE_ERROR
+#ifndef _GL_NO_INLINE_ERROR
 # if !GNULIB_defined_verror_at_line
 #  define verror_at_line(status, ...) \
      _gl_error_call (verror_at_line, status, __VA_ARGS__)
