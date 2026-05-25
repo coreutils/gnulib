1d411d24777b1defd3a065300da16b31586ef85f
Bruno Haible <bruno@clisp.org>
2026-05-25 18:27:18 +0200

    mbsncasecmp tests: Enhance tests.
    * tests/test-mbsncasecmp.c (test_ascii): New function, extracted from
    main.
    (test_utf_8): Likewise. Add test cases with incomplete characters.
    (main): Invoke them. Accept a numeric argument.
    * tests/test-mbsncasecmp-4.sh: Renamed from tests/test-mbsncasecmp.sh.
    * tests/test-mbsncasecmp-3.sh: New file, based on
    tests/test-mbmemcasecmp-3.sh.
    * modules/mbsncasecmp-tests (Files): Update after rename. Add
    locale-en.m4, locale-fr.m4.
    (configure.ac): Invoke gt_LOCALE_EN_UTF8, gt_LOCALE_FR_UTF8.
    (Makefile.am): Arrange to run test-mbsncasecmp-3.sh,
    test-mbsncasecmp-4.sh, instead of test-mbsncasecmp.sh.

--- tests/test-mbsncasecmp.c
+++ tests/test-mbsncasecmp.c
@@ -24,13 +24,9 @@
 
 #include "macros.h"
 
-int
-main ()
+static void
+test_ascii (void)
 {
-  /* configure should already have checked that the locale is supported.  */
-  if (setlocale (LC_ALL, "") == NULL)
-    return 1;
-
   ASSERT (mbsncasecmp ("paragraph", "Paragraph", 1000000) == 0);
   ASSERT (mbsncasecmp ("paragraph", "Paragraph", 9) == 0);
 
@@ -54,16 +50,64 @@ main ()
   ASSERT (mbsncasecmp ("paragraph", "para", 9) > 0);
   ASSERT (mbsncasecmp ("paragraph", "para", 5) > 0);
   ASSERT (mbsncasecmp ("paragraph", "para", 4) == 0);
+}
 
+static void
+test_utf_8 (bool turkish)
+{
   /* The following tests shows how mbsncasecmp() is different from
      strncasecmp().  */
 
-  ASSERT (mbsncasecmp ("\303\266zg\303\274r", "\303\226ZG\303\234R", 99) == 0); /* özgür */
-  ASSERT (mbsncasecmp ("\303\226ZG\303\234R", "\303\266zg\303\274r", 99) == 0); /* özgür */
+  if (turkish)
+    {
+      ASSERT (mbsncasecmp ("\303\266zg\303\274r", "\303\226ZG\303\234R", 99) == 0); /* özgür */
+      ASSERT (mbsncasecmp ("\303\226ZG\303\234R", "\303\266zg\303\274r", 99) == 0); /* özgür */
 
-  /* This test shows how strings of different size can compare equal.  */
-  ASSERT (mbsncasecmp ("turkish", "TURK\304\260SH", 7) == 0);
-  ASSERT (mbsncasecmp ("TURK\304\260SH", "turkish", 7) == 0);
+      /* This test shows how strings of different size can compare equal.  */
+      ASSERT (mbsncasecmp ("turkish", "TURK\304\260SH", 7) == 0);
+      ASSERT (mbsncasecmp ("TURK\304\260SH", "turkish", 7) == 0);
+    }
 
-  return test_exit_status;
+  /* Incomplete characters.  See
+     https://www.unicode.org/versions/Unicode15.0.0/ch03.pdf
+     page 128 table 3-11.  */
+
+  /* "\341\200\240" = 0xE1 0x80 0xA0 = U+1020.  */
+  ASSERT (mbsncasecmp ("\341\200", "\341\200", 99) == 0);
+  ASSERT (mbsncasecmp ("\341\200X", "\341\200x", 99) == 0);
+  ASSERT (mbsncasecmp ("\341", "\341", 99) == 0);
+  ASSERT (mbsncasecmp ("\341X", "\341x", 99) == 0);
+  /* "\360\221\222\240" = 0xF0 0x91 0x92 0xA0 = U+114A0.  */
+  ASSERT (mbsncasecmp ("\360\221\222", "\360\221\222", 99) == 0);
+  ASSERT (mbsncasecmp ("\360\221\222X", "\360\221\222x", 99) == 0);
+  ASSERT (mbsncasecmp ("\360\221", "\360\221", 99) == 0);
+  ASSERT (mbsncasecmp ("\360\221X", "\360\221x", 99) == 0);
+  ASSERT (mbsncasecmp ("\360", "\360", 99) == 0);
+  ASSERT (mbsncasecmp ("\360X", "\360x", 99) == 0);
+}
+
+int
+main (int argc, char *argv[])
+{
+  /* configure should already have checked that the locale is supported.  */
+  if (setlocale (LC_ALL, "") == NULL)
+    return 1;
+
+  test_ascii ();
+
+  if (argc > 1)
+    switch (argv[1][0])
+      {
+      case '3':
+        /* Locale encoding is UTF-8, locale is not Turkish.  */
+        test_utf_8 (false);
+        return test_exit_status;
+
+      case '4':
+        /* Locale encoding is UTF-8, locale is Turkish.  */
+        test_utf_8 (true);
+        return test_exit_status;
+      }
+
+  return 1;
 }
