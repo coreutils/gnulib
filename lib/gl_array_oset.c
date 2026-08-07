8b8d7319f8136deaade3f2fc5882b250f4e49f1e
Bruno Haible <bruno@clisp.org>
2026-08-08 00:09:48 +0200

    Fix incorrect use of _GL_ATTRIBUTE_PURE.
    Reported by Arsen Arsenović <arsen@aarsen.me> in
    <https://lists.gnu.org/archive/html/bug-gettext/2026-08/msg00000.html>.
    
    * lib/gl_array_omap.c (gl_array_search, gl_array_search_atleast): Use
    attribute _GL_ATTRIBUTE_REPRODUCIBLE instead of attribute
    _GL_ATTRIBUTE_PURE.
    * lib/gl_array_oset.c (gl_array_search_atleast): Likewise.
    * lib/gl_hash_map.c (gl_hash_search): Likewise.
    * lib/gl_linkedhash_map.c (gl_linkedhash_search): Likewise.
    * lib/modechange.h (mode_adjust): Likewise.

--- lib/gl_array_oset.c
+++ lib/gl_array_oset.c
@@ -160,7 +160,7 @@ gl_array_indexof_atleast (gl_oset_t set,
   return count;
 }
 
-static bool _GL_ATTRIBUTE_PURE
+_GL_ATTRIBUTE_REPRODUCIBLE static bool
 gl_array_search_atleast (gl_oset_t set,
                          gl_setelement_threshold_fn threshold_fn,
                          const void *threshold,
--- lib/gl_linkedhash_map.c
+++ lib/gl_linkedhash_map.c
@@ -119,7 +119,7 @@ gl_linkedhash_size (gl_map_t map)
   return map->count;
 }
 
-static bool _GL_ATTRIBUTE_PURE
+_GL_ATTRIBUTE_REPRODUCIBLE static bool
 gl_linkedhash_search (gl_map_t map, const void *key, const void **valuep)
 {
   size_t hashcode =
