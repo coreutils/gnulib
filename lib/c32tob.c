f8e2e1ce277cc2faa1e7fa0974aec2b631b1ea41
Bruno Haible <bruno@clisp.org>
2026-09-20 21:54:23 +0200

    c32tob: Make consistent with btoc32.
    * lib/c32tob.c: Include hard-locale.h, <locale.h>.
    (c32tob): On Cygwin and native Windows and when uchar-h-c23 is in use,
    map the code points U+DF80..U+DFFF back to 0x80..0xFF.
    * modules/c32tob (Depends-on): Add hard-locale.
    (configure.ac): Require gl_MBRTOC32_C_LOCALE,
    gl_MBRTOC32_C_LOCALE_LIKE_ISO_8859.

--- lib/c32tob.c
+++ lib/c32tob.c
@@ -30,6 +30,12 @@
 # include "lc-charset-unicode.h"
 #endif
 
+#if _GL_SMALL_WCHAR_T \
+    || (GL_CHAR32_T_IS_UNICODE && GL_CHAR32_T_VS_WCHAR_T_NEEDS_CONVERSION)
+# include "hard-locale.h"
+# include <locale.h>
+#endif
+
 #if _GL_WCHAR_T_IS_UCS4
 _GL_EXTERN_INLINE
 #endif
@@ -49,15 +55,35 @@ c32tob (wint_t wc)
         return (unsigned char) buf[0];
     }
   return EOF;
-#elif _GL_SMALL_WCHAR_T
+#elif !GNULIB_defined_mbstate_t && _GL_SMALL_WCHAR_T /* Cygwin, mingw, MSVC */
   /* In all known encodings, unibyte characters correspond only to
      characters in the BMP.  */
   if (wc != WEOF && (wchar_t) wc == wc)
-    return wctob ((wchar_t) wc);
+    {
+      if (!hard_locale (LC_CTYPE))
+        {
+          /* In the "C" locale, map the code points U+DF80..U+DFFF back
+             to the bytes 0x80..0xFF, for consistency with the mbrtoc32 and
+             btoc32 functions.  */
+          if (wc >= 0x00 && wc <= 0x7F)
+            return (unsigned char) wc;
+          else if (wc >= 0xDF80 && wc <= 0xDFFF)
+            return (unsigned char) (wc - 0xDF00);
+          else
+            return EOF;
+        }
+      return wctob ((wchar_t) wc);
+    }
   else
     return EOF;
 #else
 # if GL_CHAR32_T_IS_UNICODE && GL_CHAR32_T_VS_WCHAR_T_NEEDS_CONVERSION
+  if ((wc >= 0xDF80 && wc <= 0xDFFF) && !hard_locale (LC_CTYPE))
+    {
+      /* In the "C" locale, map the code points U+DF80..U+DFFF back to the bytes
+         0x80..0xFF, for consistency with the mbrtoc32 and btoc32 functions.  */
+      return (unsigned char) (wc - 0xDF00);
+    }
   if (wc != 0)
     {
       wc = unicode_to_locale_encoding (wc);
